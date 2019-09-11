4d2f557b51e544ea8d32cd937195e77b946bb615
Janusz Kornaga <janusz.kornaga@rumblefishdev.com>
2019-09-11 14:14:52 +0000

    Merge branch 'fix/remove-lastSeen-on-focus' into 'develop'
    remove update lastSeen on focus
    
    See merge request holmes/zbay!85

--- packages/ZbayLite/src/renderer/index.js
+++ packages/ZbayLite/src/renderer/index.js
@@ -1,13 +1,13 @@
 import { Web } from './vendor/buttercup'
 import React from 'react'
 import { render } from 'react-dom'
-import { ipcRenderer, remote } from 'electron'
+import { ipcRenderer } from 'electron'
 
 import Root from './Root'
 import store from './store'
 import nodeHandlers from './store/handlers/node'
-import channelHandlers from './store/handlers/channel'
-import vaultSelectors from './store/selectors/vault'
+// import channelHandlers from './store/handlers/channel'
+// import vaultSelectors from './store/selectors/vault'
 
 Web.HashingTools.patchCorePBKDF()
 
@@ -16,13 +16,13 @@ ipcRenderer.on('bootstrappingNode', (event, { bootstrapping, message }) => {
   store.dispatch(nodeHandlers.actions.setBootstrappingMessage(message))
 })
 
-remote.app.on('browser-window-focus', () => {
-  const vaultUnlocked = !vaultSelectors.locked(store.getState())
-  if (vaultUnlocked) {
-    store.dispatch(channelHandlers.epics.updateLastSeen())
-    store.dispatch(channelHandlers.epics.clearNewMessages())
-  }
-})
+// remote.app.on('browser-window-focus', () => {
+//   const vaultUnlocked = !vaultSelectors.locked(store.getState())
+//   if (vaultUnlocked) {
+//     store.dispatch(channelHandlers.epics.updateLastSeen())
+//     store.dispatch(channelHandlers.epics.clearNewMessages())
+//   }
+// })
 
 render(
   <Root />,
