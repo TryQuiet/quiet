6fdf694411e0866c1ffd25dff526bac68bd862a1
Norbert Bodziony <norbert.bodziony@rumblefish.pl>
2019-10-24 12:02:20 +0000

    Merge branch 'StylesFixes' into 'develop'
    fix css andshow register nickname
    
    See merge request holmes/z-bay!40

--- packages/ZbayLite/src/renderer/components/ui/IdentityPanel.test.js
+++ packages/ZbayLite/src/renderer/components/ui/IdentityPanel.test.js
@@ -7,7 +7,22 @@ import { Identity } from '../../store/handlers/identity'
 import { IdentityPanel } from './IdentityPanel'
 
 describe('IdentityPanel', () => {
-  it('renders component', () => {
+  it('renders component with username', () => {
+    const identity = Identity({
+      address: 'zs1z7rejlpsa98s2rrrfkwmaxu53e4ue0ulcrw0h4x5g8jl04tak0d3mm47vdtahatqrlkngh9sly',
+      name: 'Saturn'
+    })
+    const result = shallow(
+      <IdentityPanel
+        classes={mockClasses}
+        identity={identity}
+        handleSettings={jest.fn()}
+        user={{ nickname: 'test' }}
+      />
+    )
+    expect(result).toMatchSnapshot()
+  })
+  it('renders component without username', () => {
     const identity = Identity({
       address: 'zs1z7rejlpsa98s2rrrfkwmaxu53e4ue0ulcrw0h4x5g8jl04tak0d3mm47vdtahatqrlkngh9sly',
       name: 'Saturn'
