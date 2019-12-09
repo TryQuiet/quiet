1bd343c54fc6c9dd7486c2cf16f1fb5a0b444379
Norbert Bodziony <norbert.bodziony@rumblefish.pl>
2019-12-09 11:52:03 +0000

    Merge branch 'userPopover' into 'develop'
    remove actions from user popover
    
    See merge request holmes/z-bay!142

--- packages/ZbayLite/src/renderer/components/widgets/channels/SendMessagePopover.js
+++ packages/ZbayLite/src/renderer/components/widgets/channels/SendMessagePopover.js
@@ -4,54 +4,10 @@ import * as R from 'ramda'
 import Popover from '@material-ui/core/Popover'
 import Jdenticon from 'react-jdenticon'
 import { withStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import ClearIcon from '@material-ui/icons/Clear'
 
-import IconButton from '../../ui/IconButton'
+import QuickActionLayout from '../../ui/QuickActionLayout'
 
-const styles = theme => ({
-  alignAvatarPopover: {
-    marginTop: theme.spacing(0)
-  },
-  button: {
-    width: 260,
-    height: 60,
-    marginTop: theme.spacing(0.5),
-    paddingBottom: theme.spacing(1.2),
-    paddingLeft: theme.spacing(1.6),
-    paddingRight: theme.spacing(1.6),
-    color: theme.palette.colors.white,
-    fontSize: '0.9rem',
-    backgroundColor: theme.palette.colors.purple,
-    textTransform: 'none'
-  },
-  container: {
-    height: 400,
-    width: 320
-  },
-  usernamePopover: {
-    marginTop: theme.spacing(0),
-    fontSize: '1.2rem',
-    fontWeight: '600'
-  },
-  closeIcon: {
-    margin: theme.spacing(0)
-  },
-  info: {
-    color: theme.palette.colors.zbayBlue
-  },
-  infoDiv: {
-    textAlign: 'center',
-    marginTop: theme.spacing(0),
-    marginLeft: theme.spacing(4),
-    marginRight: theme.spacing(4)
-  },
-  avatar: {
-    marginTop: theme.spacing(0)
-  }
-})
+const styles = theme => ({})
 
 export const SendMessagePopover = ({
   classes,
@@ -60,11 +16,8 @@ export const SendMessagePopover = ({
   anchorEl,
   handleClose,
   isUnregistered,
-  identityId,
   createContact,
-  history,
-  removeMessage,
-  banUser
+  history
 }) => {
   const open = Boolean(anchorEl)
   const id = open ? 'simple-popover' : undefined
@@ -84,73 +37,26 @@ export const SendMessagePopover = ({
         horizontal: 'center'
       }}
     >
-      <Grid
-        container
-        className={classes.container}
-        direction='column'
-        justify='flex-start'
-        alignItems='center'
+      <QuickActionLayout
+        main={username}
+        info={`${address.substring(0, 32)}...`}
+        buttonName='Send message'
+        handleClose={handleClose}
+        warrning={
+          isUnregistered ? 'Sending direct messages is only available for registered users' : null
+        }
+        onClick={() =>
+          createContact({
+            contact: {
+              replyTo: address,
+              username
+            },
+            history
+          })
+        }
       >
-        <Grid className={classes.icon} container item direction='row' justify='flex-start'>
-          <IconButton className={classes.closeIcon} onClick={handleClose}>
-            <ClearIcon />
-          </IconButton>
-        </Grid>
-        <Grid item className={classes.avatar}>
-          <span className={classes.alignAvatarPopover}>
-            <Jdenticon size='100' value={username} />
-          </span>
-        </Grid>
-        <Grid item>
-          <Typography color='textPrimary' className={classes.usernamePopover}>
-            {username}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography className={classes.info} variant='caption'>
-            {`${address.substring(0, 32)}...`}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Button
-            variant={'contained'}
-            onClick={() =>
-              createContact({
-                contact: {
-                  replyTo: address,
-                  username
-                },
-                history
-              })
-            }
-            disabled={
-              !!(isUnregistered
-                ? 'Sending direct messages is only available for registered users'
-                : null)
-            }
-            className={classes.button}
-          >
-            Send message
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button variant={'contained'} onClick={removeMessage} className={classes.button}>
-            Remove message
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button variant={'contained'} onClick={banUser} className={classes.button}>
-            Block user
-          </Button>
-        </Grid>
-        <Grid item className={classes.infoDiv}>
-          <Typography className={classes.info} variant='caption'>
-            {isUnregistered
-              ? 'Sending direct messages is only available for registered users'
-              : null}
-          </Typography>
-        </Grid>
-      </Grid>
+        <Jdenticon size='100' value={username} />
+      </QuickActionLayout>
     </Popover>
   )
 }
@@ -159,8 +65,6 @@ SendMessagePopover.propTypes = {
   classes: PropTypes.object.isRequired,
   username: PropTypes.string.isRequired,
   handleClose: PropTypes.func.isRequired,
-  banUser: PropTypes.func.isRequired,
-  removeMessage: PropTypes.func.isRequired,
   address: PropTypes.string.isRequired,
   anchorEl: PropTypes.bool,
   isUnregistered: PropTypes.bool
--- packages/ZbayLite/src/renderer/containers/widgets/channels/SendMessagePopover.js
+++ packages/ZbayLite/src/renderer/containers/widgets/channels/SendMessagePopover.js
@@ -7,28 +7,15 @@ import * as R from 'ramda'
 import SendMessagePopover from '../../../components/widgets/channels/SendMessagePopover'
 import identitySelectors from '../../../store/selectors/identity'
 import contactsHandlers from '../../../store/handlers/contacts'
-import moderationHandlers, {
-  moderationActionsType
-} from '../../../store/handlers/moderationActions'
 
 export const mapStateToProps = state => ({
   identityId: identitySelectors.id(state)
 })
 
-export const mapDispatchToProps = (dispatch, { publicKey, txid }) =>
+export const mapDispatchToProps = (dispatch) =>
   bindActionCreators(
     {
-      createContact: contactsHandlers.epics.createVaultContact,
-      banUser: () =>
-        moderationHandlers.epics.handleModerationAction({
-          moderationType: moderationActionsType.BLOCK_USER,
-          moderationTarget: publicKey
-        }),
-      removeMessage: () =>
-        moderationHandlers.epics.handleModerationAction({
-          moderationType: moderationActionsType.REMOVE_MESSAGE,
-          moderationTarget: txid
-        })
+      createContact: contactsHandlers.epics.createVaultContact
     },
     dispatch
   )
