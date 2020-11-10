e5ab568e2b87dd7998f38a652063d57c7f8c39dc
Janusz Kornaga <Janusz Kornaga>
2020-11-10 19:32:59 +0100

    hot fix

--- packages/ZbayLite/src/renderer/store/handlers/messages.ts
+++ packages/ZbayLite/src/renderer/store/handlers/messages.ts
@@ -585,7 +585,7 @@ const setUsersMessages = (address, messages: DisplayableMessage[]) => async (
       return new DisplayableMessage(message);
     })
   );
-  const itemMessages = messagesAll.filter((msg) => msg.message.itemId);
+  const itemMessages = messagesAll.filter((msg) => msg.message ? msg.message.itemId : null);
   const contacts = contactsSelectors.contacts(getState());
   const groupedItemMesssages = R.groupBy<DisplayableMessage>(
     (msg) => msg.message.itemId + msg.sender.username
@@ -639,7 +639,7 @@ const setUsersMessages = (address, messages: DisplayableMessage[]) => async (
       );
     }
   }
-  const normalMessages = messagesAll.filter((msg) => !msg.message.itemId);
+  const normalMessages = messagesAll.filter((msg) => msg.message ? !msg.message.itemId : null);
   const groupedMesssages = R.groupBy<DisplayableMessage>(
     (msg) => msg.publicKey
   )(normalMessages);
