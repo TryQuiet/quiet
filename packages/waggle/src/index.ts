4785983b29ed18dd80b73fb4b1ce3f337c4e5227
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2021-05-25 07:02:36 +0200

    merged export-waggle-version into master

--- packages/waggle/src/index.ts
+++ packages/waggle/src/index.ts
@@ -2,10 +2,12 @@ import { Tor } from './torManager'
 import { DataServer } from './socket/DataServer'
 import { ConnectionsManager } from './libp2p/connectionsManager'
 import initListeners from './socket/listeners'
+import { version } from './../package.json'
 
 export default {
   Tor,
   DataServer,
   ConnectionsManager,
-  initListeners
+  initListeners,
+  version
 }
