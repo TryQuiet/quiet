aaed7990c6db4ffb1af2d6e5a62856831f98b4b7
vinkabuki <vincent.kabacky@gmail.com>
2021-06-11 14:35:24 +0200

    Merge pull request #43 from ZbayApp/improvement/ci-tests
    Improve ci tests

--- packages/waggle/src/torManager/torManager.test.ts
+++ packages/waggle/src/torManager/torManager.test.ts
@@ -7,18 +7,15 @@ jest.setTimeout(30_000)
 
 let tmpDir: TmpDir
 let tmpAppDataPath: string
-let tor: Tor
 
 beforeEach(() => {
   jest.clearAllMocks()
   tmpDir = createTmpDir()
   tmpAppDataPath = tmpZbayDirPath(tmpDir.name)
-  tor = null
 })
 
 afterEach(async () => {
   tmpDir.removeCallback()
-  tor && await tor.kill()
 })
 
 test('start and close tor', async () => {
@@ -65,14 +62,15 @@ test('start tor, do not kill tor process and start again', async () => {
 })
 
 test('spawn new hidden service', async () => {
-  tor = await spawnTorProcess(tmpAppDataPath)
+  const tor = await spawnTorProcess(tmpAppDataPath)
   await tor.init()
   const hiddenService = await tor.createNewHiddenService(4343, 4343)
   expect(hiddenService.onionAddress).toHaveLength(56)
+  await tor.kill()
 })
 
 test('spawn hidden service using private key', async () => {
-  tor = await spawnTorProcess(tmpAppDataPath)
+  const tor = await spawnTorProcess(tmpAppDataPath)
   await tor.init()
   const hiddenServiceOnionAddress = await tor.spawnHiddenService({
     virtPort: 4343,
@@ -81,4 +79,5 @@ test('spawn hidden service using private key', async () => {
       'ED25519-V3:uCr5t3EcOCwig4cu7pWY6996whV+evrRlI0iIIsjV3uCz4rx46sB3CPq8lXEWhjGl2jlyreomORirKcz9mmcdQ=='
   })
   expect(hiddenServiceOnionAddress).toBe('u2rg2direy34dj77375h2fbhsc2tvxj752h4tlso64mjnlevcv54oaad')
+  await tor.kill()
 })
