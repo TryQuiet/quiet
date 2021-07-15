b5fafbe979bbdcc61c8c3a988e50297dc9fe9029
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2021-07-15 12:44:05 +0200

    Merge pull request #12 from ZbayApp/fix/add-import-for-cert-fields-types-enum
    Added import for CertFieldsTypes

--- packages/identity/src/index.ts
+++ packages/identity/src/index.ts
@@ -1,11 +1,24 @@
 import { createRootCA } from './generateRootCA'
-import { extractPubKey, parseCertificate, keyFromCertificate, keyObjectFromString } from './extractPubKey'
+import {
+  extractPubKey,
+  parseCertificate,
+  keyFromCertificate,
+  keyObjectFromString
+} from './extractPubKey'
 import { verifyUserCert } from './verifyUserCertificate'
 import { verifySignature } from './verification'
 import { sign } from './sign'
 import { createUserCsr } from './requestCertificate'
 import { createUserCert } from './generateUserCertificate'
-import { loadPrivateKey, loadCSR, loadCertificate, formatPEM, generateKeyPair, getCertFieldValue } from './common'
+import {
+  loadPrivateKey,
+  loadCSR,
+  loadCertificate,
+  formatPEM,
+  generateKeyPair,
+  getCertFieldValue,
+  CertFieldsTypes
+} from './common'
 
 export { createRootCA }
 export { extractPubKey, parseCertificate, keyFromCertificate, keyObjectFromString }
@@ -14,4 +27,12 @@ export { verifySignature }
 export { sign }
 export { createUserCsr }
 export { createUserCert }
-export { loadPrivateKey, loadCSR, loadCertificate, formatPEM, generateKeyPair, getCertFieldValue }
+export {
+  loadPrivateKey,
+  loadCSR,
+  loadCertificate,
+  formatPEM,
+  generateKeyPair,
+  getCertFieldValue,
+  CertFieldsTypes
+}
