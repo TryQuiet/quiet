a7e4ab0309d4986d33c6eb19a2c85e21b4d5ed46
siepra <wiktor.siepra@gmail.com>
2021-08-30 15:14:09 +0200

    Uploaded valid provision profile

--- packages/mobile/.github/secrets/decrypt_secrets.sh
+++ packages/mobile/.github/secrets/decrypt_secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/ci.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
