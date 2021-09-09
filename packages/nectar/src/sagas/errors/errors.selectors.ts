d6228a7dcde1a3776e63d6eb0213465125c49741
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2021-09-09 14:04:57 +0200

    Merge pull request #8 from ZbayApp/fix/remove-factory-from-error-selectors
    Removed factory from error selectors because it causes problems on mo…

--- packages/nectar/src/sagas/errors/errors.selectors.ts
+++ packages/nectar/src/sagas/errors/errors.selectors.ts
@@ -1,8 +1,15 @@
-import { StoreKeys } from '../store.keys';
-import { selectorsFactory } from '../store.utils';
+import { createSelector } from 'reselect';
 import { ErrorsState } from './errors.slice';
 
-export const errorsSelectors = selectorsFactory(
-  StoreKeys.Errors,
-  ErrorsState
+const selectSelf = (state) => state;
+
+export const certificateRegistration = createSelector(
+  selectSelf,
+  (reducerState: ErrorsState) => {
+    return reducerState.certificateRegistration;
+  }
 );
+
+export const errorsSelectors = {
+  certificateRegistration
+}
