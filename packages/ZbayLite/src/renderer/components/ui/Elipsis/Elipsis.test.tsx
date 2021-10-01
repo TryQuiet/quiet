3f62a0c876a7cf9741afd92b50c5b35471bd4bd5
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2021-10-01 11:02:52 +0200

    Fix/update components tests (#387)
    * WindowWrapper test updated
    
    * ZcashIcon test updated
    
    * Elipsis snapshots fixed
    
    * IdentityPanel test updated
    
    * Tooltip tests updated

--- packages/ZbayLite/src/renderer/components/ui/Elipsis/Elipsis.test.tsx
+++ packages/ZbayLite/src/renderer/components/ui/Elipsis/Elipsis.test.tsx
@@ -6,31 +6,44 @@ import { Elipsis } from './Elipsis'
 import { renderComponent } from '../../../testUtils/renderComponent'
 
 describe('Elipsis', () => {
-  each(['bottom-start', 'bottom', 'bottom-end']).test(
-    'renders with placement %s',
-    (placement) => {
-      const result = renderComponent(
-        <Elipsis
-          content={'this is a sample text'}
-          length={5}
-          tooltipPlacement={placement}
-        />
-      )
-      expect(result.baseElement).toMatchSnapshot()
-    }
-  )
-
-  it('renders with custom size', () => {
+  each(['bottom-start', 'bottom', 'bottom-end']).test('renders with placement %s', placement => {
     const result = renderComponent(
-      <Elipsis content={'this is a sample text'} length={5} />
+      <Elipsis content={'this is a sample text'} length={5} tooltipPlacement={placement} />
     )
     expect(result.baseElement).toMatchSnapshot()
   })
 
+  it('renders with custom size', () => {
+    const result = renderComponent(<Elipsis content={'this is a sample text'} length={5} />)
+    expect(result.baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div>
+          <span>
+            <span
+              class="MuiTypography-root makeStyles-content-142 MuiTypography-caption"
+            >
+              this ...
+            </span>
+          </span>
+        </div>
+      </body>
+    `)
+  })
+
   it('disables if shorter than limit', () => {
-    const result = renderComponent(
-      <Elipsis content={'this is a sample text'} length={50} />
-    )
-    expect(result.baseElement).toMatchSnapshot()
+    const result = renderComponent(<Elipsis content={'this is a sample text'} length={50} />)
+    expect(result.baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div>
+          <span>
+            <span
+              class="MuiTypography-root makeStyles-content-189 MuiTypography-caption"
+            >
+              this is a sample text
+            </span>
+          </span>
+        </div>
+      </body>
+    `)
   })
 })
