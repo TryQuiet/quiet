a15b50d6d60363d549ae0dda3309d9e66f031870
Dinek007 <61462655+Dinek007@users.noreply.github.com>
2021-10-27 14:47:57 +0200

    Merge pull request #128 from ZbayApp/fix/remove-dev-import-from-prod-code
    remove devDependencies import

--- packages/waggle/src/storage/storage.ts
+++ packages/waggle/src/storage/storage.ts
@@ -21,10 +21,14 @@ import { loadAllPublicChannels } from '../socket/events/channels'
 import {
   loadAllDirectMessages, loadAllMessages, message as socketMessage, sendIdsToZbay
 } from '../socket/events/messages'
-import { dataFromRootPems } from '../common/testUtils'
 import { createPaths } from '../common/utils'
 import validate from '../validation/validators'
 const log = logger('db')
+
+const dataFromRootPems: DataFromPems = {
+  certificate: 'MIIBNjCB3AIBATAKBggqhkjOPQQDAjASMRAwDgYDVQQDEwdaYmF5IENBMCYYEzIwMjEwNjIyMDkzMDEwLjAyNVoYDzIwMzAwMTMxMjMwMDAwWjASMRAwDgYDVQQDEwdaYmF5IENBMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEV5a3Czy+L7IfVX0FpJtSF5mi0GWGrtPqv5+CFSDPrHXijsxWdPTobR1wk8uCLP4sAgUbs/bIleCxQy41kSSyOaMgMB4wDwYDVR0TBAgwBgEB/wIBAzALBgNVHQ8EBAMCAAYwCgYIKoZIzj0EAwIDSQAwRgIhAPOzksuipKyBALt/o8O/XwsrVSzfSHXdAR4dOWThQ1lbAiEAmKqjhsmf50kxWX0ekhbAeCTjcRApXhjnslmJkIFGF2o=+lmBImw3BMNjA0FTlK5iRmVC+w/T6M04Es+yiYL608vOhx2slnoyAwHjAPBgNVHRMECDAGAQH/AgEDMAsGA1UdDwQEAwIABjAKBggqhkjOPQQDAgNIADBFAiEA+0kIz0ny/PLVERTcL0+KCpsztyA6Zuwzj05VW5NMdx0CICgdzf0lg0/2Ksl1AjSPYsy2w+Hn09PGlBnD7TiExBpx',
+  privKey: 'MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgTvNuJL0blaYq6zmFS53WmmOfHshlqn+8wNHDzo4df5WgCgYIKoZIzj0DAQehRANCAARXlrcLPL4vsh9VfQWkm1IXmaLQZYau0+q/n4IVIM+sdeKOzFZ09OhtHXCTy4Is/iwCBRuz9siV4LFDLjWRJLI5+lmBImw3BMNjA0FTlK5iRmVC+w/T6M04Es+yiYL608vOhx2sln'
+}
 
 const webcrypto = new Crypto()
 setEngine(
