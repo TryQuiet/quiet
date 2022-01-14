b576020663d105c1e7296f16e21f66bd965ba62f
siepra <wiktor.siepra@gmail.com>
2022-01-14 18:18:22 +0100

    Fix waggle tests

--- packages/waggle/src/communities/manager.ts
+++ packages/waggle/src/communities/manager.ts
@@ -140,7 +140,7 @@ export default class CommunitiesManager {
       address: params.onionAddress,
       addressPort: port,
       targetPort: params.targetPort,
-      bootstrapMultiaddrs: params.peers,
+      bootstrapMultiaddrs: peers,
       certs: params.certs
     }
 
