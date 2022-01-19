a8da361f9188a8d2324805ee5315ac128ba1e09a
siepra <wiktor.siepra@gmail.com>
2022-01-19 13:56:49 +0100

    Added funny things to MessagesAccessController

--- packages/waggle/src/storage/MessagesAccessController.ts
+++ packages/waggle/src/storage/MessagesAccessController.ts
@@ -3,9 +3,26 @@ import { verifySignature } from '@zbayapp/identity'
 import { ChannelMessage } from '@zbayapp/nectar'
 import AccessController from 'orbit-db-access-controllers/src/access-controller-interface'
 
+const type = 'messagesaccess'
+
 export class MessagesAccessController extends AccessController {
-  static get type() {
-    return 'messagesaccess'
+  _orbitdb
+  _db
+  _options
+
+  constructor (orbitdb, options) {
+    super()
+    this._orbitdb = orbitdb
+    this._db = null
+    this._options = options || {}
+  }
+
+  // Returns the type of the access controller
+  static get type () { return type }
+
+  // Returns the address of the OrbitDB used as the AC
+  get address () {
+    return this._db.address
   }
 
   async canAppend(entry) {
@@ -16,12 +33,34 @@ export class MessagesAccessController extends AccessController {
     return false
   }
 
-  static async create(_orbitdb, _options) {
-    return new MessagesAccessController()
+  async load (address) {
+    if (this._db) { await this._db.close() }
+
+    // Force '<address>/_access' naming for the database
+    this._db = await this._orbitdb.log(address)
+
+    // this._db.events.on('ready', this._onUpdate.bind(this))
+    // this._db.events.on('write', this._onUpdate.bind(this))
+    // this._db.events.on('replicated', this._onUpdate.bind(this))
+
+    await this._db.load()
   }
 
-  async save() {
+  async close () {
+    await this._db.close()
+  }
+
+  async save () {
     // return the manifest data
-    return { address: undefined, skipManifest: true }
+    return {
+      address: this._db.address.toString()
+    }
+  }
+
+  static async create(orbitdb, options) {
+    console.log('options', options)
+    const ac = new MessagesAccessController(orbitdb, options)
+    await ac.load(options.address)
+    return ac
   }
 }
