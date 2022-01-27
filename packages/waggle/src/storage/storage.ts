ba3e39f5f539435973c62f835792aa6de927305b
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2022-01-27 13:02:08 +0100

    add ts-expect-error in storage

--- packages/waggle/src/storage/storage.ts
+++ packages/waggle/src/storage/storage.ts
@@ -181,6 +181,7 @@ export class Storage {
       await this.channels.load({ fetchEntryTimeout: 2000 })
       this.io.loadPublicChannels({
         communityId: this.communityId,
+        // @ts-expect-error
         channels: this.channels.all
       })
     })
