a814d8423a011026d4848223e62d279cefc8e50b
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2022-01-28 13:57:46 +0100

    show checksum

--- packages/frontend/src/renderer/index.tsx
+++ packages/frontend/src/renderer/index.tsx
@@ -26,6 +26,11 @@ if (process.env.REACT_APP_ENABLE_SENTRY === 'true') {
   })
 }
 
+const b = ''
+
+//@ts-ignore
+b()
+
 const log = Object.assign(debug('frontend:renderer'), {
   error: debug('frontend:renderer:err')
 })
