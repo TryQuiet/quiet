19ed34bd588177de80ee74f46ec57babd6f50971
siepra <wiktor.siepra@gmail.com>
2022-01-30 21:16:26 +0300

    Reuse the same socket instead of recreating it

--- packages/frontend/src/renderer/sagas/socket/socket.slice.ts
+++ packages/frontend/src/renderer/sagas/socket/socket.slice.ts
@@ -1,5 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { StoreKeys } from '@quiet/nectar'
+import { Socket } from 'socket.io-client'
+import { FixedTask } from 'typed-redux-saga'
 
 export class SocketState {
   public isConnected: boolean = false
@@ -9,12 +11,22 @@ export interface WebsocketConnectionPayload {
   dataPort: number
 }
 
+export interface CloseConnectionPayload {
+  task: FixedTask<Generator>
+}
+
+export interface SetConnectedPayload {
+  socket: Socket
+}
+
 export const socketSlice = createSlice({
   initialState: { ...new SocketState() },
   name: StoreKeys.Socket,
   reducers: {
     startConnection: (state, _action: PayloadAction<WebsocketConnectionPayload>) => state,
-    closeConnection: state => state,
+    suspendConnection: state => {
+      state.isConnected = false
+    },
     setConnected: state => {
       state.isConnected = true
     }
