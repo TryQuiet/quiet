04370dafc2896d9953964baf06a7ea4cae03fc3c
EmiM <emilia.marczyk@rumblefish.pl>
2022-02-02 17:37:32 +0100

    Add basic test for sentry warning modal

---
+++ packages/frontend/src/renderer/containers/widgets/sentryWarning/sentryWarning.basic.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { screen } from '@testing-library/dom'
+import { renderComponent } from '../../../testUtils/renderComponent'
+import { prepareStore } from '../../../testUtils/prepareStore'
+import { StoreKeys } from '../../../store/store.keys'
+import { SocketState } from '../../../sagas/socket/socket.slice'
+import SentryWarning from '../../../containers/widgets/sentryWarning/sentryWarning'
+
+describe('Sentry warning modal', () => {
+  let oldEnv: NodeJS.ProcessEnv
+
+  beforeEach(() => {
+    oldEnv = process.env
+    process.env = { ...oldEnv }
+  })
+
+  afterEach(() => {
+    process.env = oldEnv
+  })
+
+  it('is visible if sentry is enabled', async () => {
+    process.env.REACT_APP_ENABLE_SENTRY = 'true'
+
+    const { store } = await prepareStore({
+      [StoreKeys.Socket]: {
+        ...new SocketState(),
+        isConnected: true
+      }
+    })
+
+    renderComponent(
+      <>
+        <SentryWarning />
+      </>,
+      store
+    )
+    const warningText = await screen.findByText('App is running in debug mode')
+    expect(warningText).toBeVisible()
+  })
+
+
+  it('is not visible if sentry is not enabled', async () => {
+    const { store } = await prepareStore({
+      [StoreKeys.Socket]: {
+        ...new SocketState(),
+        isConnected: true
+      }
+    })
+
+    renderComponent(
+      <>
+        <SentryWarning />
+      </>,
+      store
+    )
+    const warningText = await screen.queryByText('App is running in debug mode')
+    expect(warningText).toBeNull()
+  })
+})
