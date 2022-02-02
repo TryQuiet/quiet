e5f4648428c61b566317c5774e692fc326487f34
bartosz.chyra <bartosz.chyra@rumblefish.pl>
2022-02-02 12:20:12 +0100

    removed horizontal scroll

--- packages/frontend/src/renderer/components/widgets/channels/ChannelMessages.tsx
+++ packages/frontend/src/renderer/components/widgets/channels/ChannelMessages.tsx
@@ -11,6 +11,7 @@ import { DisplayableMessage } from '@quiet/nectar'
 const useStyles = makeStyles(theme => ({
   scroll: {
     overflow: 'scroll',
+    overflowX: 'hidden',
     height: '100%'
   },
   list: {
