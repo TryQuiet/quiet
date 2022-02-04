fc7c67b283d8ef7be98c825e2c06ab5fbe1552ad
EmiM <emilia.marczyk@rumblefish.pl>
2022-02-04 13:47:46 +0100

    Run e2e tests on built version of the app

--- packages/frontend/.testcafe-electron-rc.js
+++ packages/frontend/.testcafe-electron-rc.js
@@ -1,5 +1,15 @@
+const pjson = require('./package.json')
+
+const getAppName = () => {
+  const envs = {
+    linux: 'AppImage',
+    win32: 'exe'
+  }
+  return `${pjson.productName}-${pjson.version}.${envs[process.platform]}`
+}
+
 module.exports = {
-  mainWindowUrl: './dist/main/index.html#/',
-  appPath: './',
+  mainWindowUrl: `./dist/main/index.html#/`,
+  electronPath: `./dist/${getAppName()}`,
   // openDevTools: true,
 }
