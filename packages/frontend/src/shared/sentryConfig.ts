e9ba26f4223e600d04cb57ccdc9bb319c0698336
EmiM <emilia.marczyk@rumblefish.pl>
2022-02-07 14:57:31 +0100

    Set environment in sentry config

--- packages/frontend/src/shared/sentryConfig.ts
+++ packages/frontend/src/shared/sentryConfig.ts
@@ -5,7 +5,7 @@ export const initSentry = () => {
   if (process.env.REACT_APP_ENABLE_SENTRY === 'true') {
     Sentry.init({
       dsn: 'https://1ca88607c3d14e15b36cb2cfd5f16d68@o1060867.ingest.sentry.io/6050774',
-
+      environment: process.env.NODE_ENV,
       // Set tracesSampleRate to 1.0 to capture 100%
       // of transactions for performance monitoring.
       // We recommend adjusting this value in production
