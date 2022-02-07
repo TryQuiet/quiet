29aebdc7ea1d4ec1c90425bff436449d531e45cf
EmiM <emilia.marczyk@rumblefish.pl>
2022-02-07 12:50:57 +0100

    Replace sentry/react with sentry/electron

--- packages/frontend/src/renderer/index.tsx
+++ packages/frontend/src/renderer/index.tsx
@@ -9,15 +9,13 @@ import waggleHandlers from './store/handlers/waggle'
 
 import debug from 'debug'
 
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
+import * as Sentry from '@sentry/electron'
 
 import { socketActions, WebsocketConnectionPayload } from './sagas/socket/socket.slice'
 
 if (process.env.REACT_APP_ENABLE_SENTRY === 'true') {
   Sentry.init({
     dsn: 'https://1ca88607c3d14e15b36cb2cfd5f16d68@o1060867.ingest.sentry.io/6050774',
-    integrations: [new Integrations.BrowserTracing()],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
