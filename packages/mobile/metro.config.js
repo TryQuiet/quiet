956a36b847807a50d998b266fe36f141fd391338
siepra <wiktor.siepra@gmail.com>
2022-02-09 20:29:04 +0300

    Link and patch dependencies

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -7,6 +7,20 @@
 
 const blacklist = require('metro-config/src/defaults/exclusionList');
 
+const path = require('path');
+
+const watchFolders = [
+  path.resolve(__dirname, '../identity'),
+  path.resolve(__dirname, '../nectar'),
+  path.resolve(__dirname, '../waggle')
+]
+
+const extraNodeModules = {
+  '@quiet/identity': path.resolve(__dirname, '../identity'),
+  '@quiet/nectar': path.resolve(__dirname, '../nectar'),
+  '@quiet/waggle': path.resolve(__dirname, '../waggle')
+}
+
 module.exports = {
   resolver: {
     blacklistRE: blacklist([
@@ -15,6 +29,11 @@ module.exports = {
       /\/ios\/.*/,
     ]),
     sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from common packages to local node_modules
+        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`)
+    })
   },
   transformer: {
     getTransformOptions: async () => ({
@@ -24,4 +43,5 @@ module.exports = {
       },
     }),
   },
+  watchFolders
 };
