71259970743a9e862863160482325b6c24d845e5
siepra <wiktor.siepra@gmail.com>
2022-02-10 20:36:50 +0300

    Revert changes in metro.config

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -7,20 +7,6 @@
 
 const blacklist = require('metro-config/src/defaults/exclusionList');
 
-const path = require('path');
-
-const watchFolders = [
-  // path.resolve(__dirname, '../identity'),
-  // path.resolve(__dirname, '../nectar'),
-  path.resolve(__dirname, '../waggle')
-]
-
-const extraNodeModules = {
-  // '@quiet/identity': path.resolve(__dirname, '../identity'),
-  // '@quiet/nectar': path.resolve(__dirname, '../nectar'),
-  '@quiet/waggle': path.resolve(__dirname, '../waggle')
-}
-
 module.exports = {
   resolver: {
     blacklistRE: blacklist([
@@ -28,12 +14,7 @@ module.exports = {
       /\/android\/.*/,
       /\/ios\/.*/,
     ]),
-    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
-    extraNodeModules: new Proxy(extraNodeModules, {
-      get: (target, name) =>
-        //redirects dependencies referenced from common packages to local node_modules
-        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`)
-    })
+    sourceExts: ['jsx', 'js', 'ts', 'tsx']
   },
   transformer: {
     getTransformOptions: async () => ({
@@ -42,6 +23,5 @@ module.exports = {
         inlineRequires: true,
       },
     }),
-  },
-  watchFolders
+  }
 };
