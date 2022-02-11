ffc50c93008a847ad2fe18f01d4e59f1b8f9a180
EmiM <emilia.marczyk@rumblefish.pl>
2022-02-11 14:06:47 +0100

    E2E test refactoring; Add data-testid to message input

---
+++ packages/e2e-tests/selectors.ts
@@ -0,0 +1,46 @@
+import { Selector, t } from 'testcafe'
+
+export class LoadingPanel {
+  private text: string
+
+  constructor(title: string) {
+    this.text = title
+  }
+
+  get title() {
+    return Selector('span').withText(this.text)
+  }
+}
+
+export class Channel {
+  private name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+
+  get title() {
+    return Selector('h6').withText(`#${this.name}`)
+  }
+
+  get messagesList() {
+    return Selector('ul').withAttribute('id', 'messages-scroll')
+  }
+
+  get messagesGroup() {
+    return this.messagesList.find('li')
+  }
+
+  get messagesGroupContent() {
+    return this.messagesGroup.find('p').withAttribute('data-testid', /messagesGroupContent-/)
+  }
+
+  get messageInput () {
+    return Selector('div').withAttribute('data-testid', 'messageInput')
+  }
+
+  async sendMessage(message: string) {
+    await t.typeText(this.messageInput, message)
+    await t.pressKey('enter')
+  }
+}
