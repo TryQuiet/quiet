705ebbcb9cff46d5e0a290313c3b489adfc0d391
siepra <wiktor.siepra@gmail.com>
2022-02-14 12:59:36 +0100

    Replaced rn-nodeify with node-libs-react-native

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -7,6 +7,21 @@
 
 const blacklist = require('metro-config/src/defaults/exclusionList');
 
+const path = require('path');
+
+ const watchFolders = [
+   path.resolve(__dirname, '../identity'),
+   path.resolve(__dirname, '../nectar'),
+   path.resolve(__dirname, '../waggle')
+ ]
+
+ const extraNodeModules = {
+   '@quiet/identity': path.resolve(__dirname, '../identity'),
+   '@quiet/nectar': path.resolve(__dirname, '../nectar'),
+   '@quiet/waggle': path.resolve(__dirname, '../waggle'),
+   ...require('node-libs-react-native')
+ }
+
 module.exports = {
   resolver: {
     blacklistRE: blacklist([
@@ -14,7 +29,12 @@ module.exports = {
       /\/android\/.*/,
       /\/ios\/.*/,
     ]),
-    sourceExts: ['jsx', 'js', 'ts', 'tsx']
+    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+     extraNodeModules: new Proxy(extraNodeModules, {
+       get: (target, name) =>
+         //redirects dependencies referenced from common packages to local node_modules
+         name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`)
+     })
   },
   transformer: {
     getTransformOptions: async () => ({
@@ -23,5 +43,6 @@ module.exports = {
         inlineRequires: true,
       },
     }),
-  }
+  },
+  watchFolders
 };
