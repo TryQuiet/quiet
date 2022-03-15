324fe7e4677ffa4594a2e0fdf1befef4ebd1112c
EmiM <emilia.marczyk@rumblefish.pl>
2022-03-15 12:18:58 +0100

    Add gitignore to logger package

--- packages/logger/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import log from 'electron-log';
-export declare const saveToFileLogger: (packageName: string) => (module: string) => ((...params: any[]) => void) & log.LogFunctions;
-export declare const consoleLogger: (packageName: string) => (module: string) => any;
-declare const logger: (packageName: string) => (module: string) => any;
-export default logger;
