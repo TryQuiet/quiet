c3c8a299c6b2fe32e043572be24cc584d83144c1
EmiM <emilia.marczyk@rumblefish.pl>
2022-03-22 13:02:21 +0100

    Bot - send messages from multiple users

--- packages/integration-tests/src/bot/bot.test.ts
+++ packages/integration-tests/src/bot/bot.test.ts
@@ -6,6 +6,8 @@ import { createApp, sleep } from "../utils"
 import logger from '../logger'
 const log = logger('bot')
 
+jest.setTimeout(500_000)
+
 function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -13,48 +15,72 @@ function getRandomInt(min: number, max: number) {
 }
 
 describe('Bots sending messages', () => {
-  let app: AsyncReturnType<typeof createApp>
   const timeout = 120_000
-  let username: string
+  let apps: Map<string, AsyncReturnType<typeof createApp>> = new Map()
   
   beforeAll(async () => {
-    app = await createApp()
-    username = `bot_${(Math.random() + 1).toString(36).substring(7)}`
-  })
-
-  it('registers username', async () => {
-    const store = app.store
-    const payload = {
-        registrarAddress: 'zdfw2dq2z7gtwsio3qk6gdfcn6pbq7apdxjrswedaaztyssfyyrkwmyd',
-        userName: username,
-        registrarPort: null,
-        store: store
+    for (let i=0; i < 3; i++) {
+      apps.set(`bot_${(Math.random() + 1).toString(36).substring(7)}`, await createApp())
     }
-    await registerUsername(payload)
-
-    const communityId = store.getState().Communities.communities.ids[0]
-
-    await waitForExpect(() => {
-      expect(
-        store.getState().Identity.identities.entities[communityId].userCertificate
-      ).toBeTruthy()
-    }, timeout)
-    await assertReceivedChannelsAndSubscribe(username, 2, 120_000, store)
   })
 
-  it('sends messages', async () => {
-    for (let i=0; i < 20; i++) {
-      await sendMessage({
-        message: `${username} says hi ${i}`,
-        channelName: 'bot-spam',
-        store: app.store
-      })
-      const userTypingTime = getRandomInt(1000, 10_000)
-      log(`Waiting ${userTypingTime}ms to send a message`)
-      await sleep(userTypingTime)
+  it('register users', async () => {
+    for (const [username, app] of apps) {
+      const store = app.store
+      const payload = {
+          registrarAddress: 'zdfw2dq2z7gtwsio3qk6gdfcn6pbq7apdxjrswedaaztyssfyyrkwmyd',
+          userName: username,
+          registrarPort: null,
+          store: store
+      }
+      log(`Registering ${username}`)
+      await registerUsername(payload)
+  
+      const communityId = store.getState().Communities.communities.ids[0]
+  
+      await waitForExpect(() => {
+        expect(
+          store.getState().Identity.identities.entities[communityId].userCertificate
+        ).toBeTruthy()
+      }, timeout)
+      await assertReceivedChannelsAndSubscribe(username, 2, 120_000, store)
+    }
+  })
+
+  it('send messages', async () => {
+    const allMessagesCount = 50
+    const messagesPerUser = Math.floor(allMessagesCount / apps.size)
+    
+    const messagesToSend = new Map()
+    for (const [username, _app] of apps) {
+      messagesToSend.set(username, messagesPerUser)
+    }
+
+    while (messagesToSend.size > 0) {
+      const usernames = Array.from(messagesToSend.keys())
+      const currentUsername = usernames[Math.floor(Math.random() * usernames.length)]
+      const messagesLeft = messagesToSend.get(currentUsername)
+      await sendMessageWithLatency(currentUsername, apps.get(currentUsername).store, `(${messagesLeft}) What's up?`)
+      if (messagesLeft - 1 === 0) {
+        messagesToSend.delete(currentUsername)
+        log(`User ${currentUsername} is finished with sending messages`)
+        continue
+      }
+      messagesToSend.set(currentUsername, messagesLeft - 1)
     }
 
     await sleep(10_000)    
   })
 
 })
+
+async function sendMessageWithLatency(username: string, store, message: string) {
+  const userTypingTime = getRandomInt(5000, 10_000)
+  log(`${username} is waiting ${userTypingTime}ms to send a message`)
+  await sleep(userTypingTime)
+  await sendMessage({
+    message,
+    channelName: 'bot-spam',
+    store: store
+  })
+}
