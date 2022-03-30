c7055cc5f4ffae1f82e1347facfc45a63added40
EmiM <emilia.marczyk@rumblefish.pl>
2022-03-30 20:15:47 +0200

    Move testcafe config from cli options to a file

---
+++ packages/e2e-tests/.testcaferc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  hostname: 'localhost',
+  screenshots: {
+    fullPage: true,
+    takeOnFails: true,
+    pathPattern: "${DATE}_${TIME}_${TEST_ID}.png"
+  },
+  color: true,
+  stopOnFirstFail: true
+}
