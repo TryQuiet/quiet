0b53fc72436e0e60b36d3be02515e5ca5cd0c255
siepra <wiktor.siepra@gmail.com>
2022-04-26 14:24:30 +0200

    Correct transformers

--- packages/nectar/src/sagas/publicChannels/publicChannels.transform.ts
+++ packages/nectar/src/sagas/publicChannels/publicChannels.transform.ts
@@ -1,18 +1,13 @@
 import { createTransform } from 'redux-persist'
 import { StoreKeys } from '../store.keys'
-import { publicChannelsAdapter } from './publicChannels.adapter'
+import { channelMessagesAdapter } from './publicChannels.adapter'
 import { PublicChannelsState } from './publicChannels.slice'
 
 export const PublicChannelsTransform = createTransform(
   (inboundState: PublicChannelsState, _key) => {
     for (const community of Object.values(inboundState.channels.entities)) {
       for (const channel of Object.values(inboundState.channels.entities[community.id].channels)) {
-        publicChannelsAdapter.updateOne(inboundState.channels.entities[community.id].channels, {
-          id: channel.address,
-          changes: {
-            messagesSlice: 0
-          }
-        })
+        channelMessagesAdapter.removeAll(inboundState.channels.entities[community.id].channels.entities[channel.address].messages)
       }
     }
     return { ...inboundState }
