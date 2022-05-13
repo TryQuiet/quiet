5fe84421191617b13ea3f688968a45783d123f03
breadhunter <emilia.marczyk@rumblefish.pl>
2022-05-13 16:31:03 +0200

    Feature/lazy loading e2e tests (#498)
    * Add e2e test for lazy loading
    
    * Fix lazy loading test assertions
    
    * E2E lazy loading test
    
    * Lazy loading test - check if messages are loaded on scrolling up
    
    * Fix test name

--- packages/frontend/src/renderer/components/Channel/ChannelComponent.tsx
+++ packages/frontend/src/renderer/components/Channel/ChannelComponent.tsx
@@ -75,7 +75,6 @@ export const ChannelComponent: React.FC<ChannelComponentProps> = ({
   }, [])
 
   const { ref: scrollbarRef } = useResizeDetector<HTMLDivElement>({ onResize })
-
   const scrollBottom = () => {
     if (!scrollbarRef.current) return
     setScrollHeight(0)
