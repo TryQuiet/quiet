825a9fee1d9a6f71cde7b1abf5e90c3464b46417
vinkabuki <vincent.kabacky@gmail.com>
2022-05-30 14:24:31 +0200

    Add sentry saga middleware

--- packages/desktop/src/renderer/store/create.ts
+++ packages/desktop/src/renderer/store/create.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/browser'
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'
@@ -9,7 +10,17 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import reducers from './reducers'
 import { errorsMiddleware } from './middlewares'
 
-const sagaMiddleware = createSagaMiddleware()
+Sentry.init({
+  dsn: 'https://1ca88607c3d14e15b36cb2cfd5f16d68@o1060867.ingest.sentry.io/6050774',
+  environment: process.env.TEST_MODE ? 'test' : 'production'
+})
+
+const sagaMiddleware = createSagaMiddleware({
+  onError(err) {
+    console.error(err)
+    Sentry.captureException(err)
+  }
+})
 
 export default (initialState = {}) => {
   const store = createStore(
