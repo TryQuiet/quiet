840e633cb3272655ef52b9941afdc4f11e713c5f
Dinek007 <damian.wilk991213@gmail.com>
2022-06-03 14:47:16 +0200

    removing temporary data

--- packages/desktop/src/main/main.ts
+++ packages/desktop/src/main/main.ts
@@ -322,6 +322,15 @@ app.on('ready', async () => {
   mainWindow.webContents.on('did-finish-load', () => {
     splash.destroy()
     mainWindow.show()
+    const temporaryFilesDirectory = path.join(appDataPath, 'temporaryFiles')
+    fs.readdir(temporaryFilesDirectory, (err, files) => {
+      if (err) throw err
+      for (const file of files) {
+        fs.unlink(path.join(temporaryFilesDirectory, file), err => {
+          if (err) throw err
+        })
+      }
+    })
   })
 
   const forkArgvs = [
