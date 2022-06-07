dcbf1650f025cd4fe196f29368bc7c7aafd59cd0
EmiM <emilia.marczyk@rumblefish.pl>
2022-06-07 12:53:17 +0200

    Display file name on the top of the uploaded image

--- packages/desktop/src/renderer/components/widgets/channels/UploadedFile.tsx
+++ packages/desktop/src/renderer/components/widgets/channels/UploadedFile.tsx
@@ -1,36 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { CircularProgress, makeStyles, Theme } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 import { DisplayableMessage } from '@quiet/state-manager'
 import { useModal } from '../../../containers/hooks'
 import { ModalName } from '../../../sagas/modals/modals.types'
 import UploadedFileModal from './UploadedFileModal'
-import imagePlaceholderIcon from '../../../static/images/imagePlaceholderIcon.svg'
-import Icon from '../../ui/Icon/Icon'
-import { UploadedFilePlaceholder } from './UploadedFilePlaceholder'
+import { UploadedFilename, UploadedFilePlaceholder } from './UploadedFilePlaceholder'
 
-const useStyles = makeStyles<Theme>(theme => ({
+const useStyles = makeStyles(() => ({
   image: {
     maxWidth: '100%'
   },
   container: {
     maxWidth: '400px',
     cursor: 'pointer'
-  },
-  placeholderWrapper: {
-    maxWidth: '400px'
-  },
-  placeholder: {
-    display: 'flex',
-    alignContent: 'center',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginLeft: '0.5em'
-  },
-  placeholderIcon: {
-    marginRight: '0.5em'
-  },
-  fileName: {
-    color: 'gray'
   }
 }))
 
@@ -44,13 +26,9 @@ export const UploadedFile: React.FC<UploadedFileProps> = ({ message }) => {
   const [showImage, setShowImage] = useState<boolean>(false)
   const modal = useModal(ModalName.uploadedFileModal)
 
-  // const path = message.media?.path
   const {path, width, height, name, ext} = message.media
-  if (!path) {
-    console.log('MESSAGE.media', path, name, ext, width, height)
-  }
+  const fullFileName = `${name}${ext}`
   
-
   useEffect(() => {
     if (modal.open) {
       setShowImage(false)
@@ -74,12 +52,15 @@ export const UploadedFile: React.FC<UploadedFileProps> = ({ message }) => {
             onClick={() => {
               setShowImage(true)
             }}>
-            <img className={classes.image} src={path} />
+            <div className={classes.image}>
+              <UploadedFilename fileName={fullFileName}/>
+              <img className={classes.image} src={path} />
+            </div>
           </div>
           <UploadedFileModal {...modal} />
         </>
       ) : (
-        <UploadedFilePlaceholder imageWidth={width} imageHeight={height} fileName={name} fileExt={ext}/>
+        <UploadedFilePlaceholder imageWidth={width} imageHeight={height} fileName={fullFileName}/>
       )}
     </>
   )
--- packages/desktop/src/renderer/components/widgets/channels/UploadedFilePlaceholder.tsx
+++ packages/desktop/src/renderer/components/widgets/channels/UploadedFilePlaceholder.tsx
@@ -1,55 +1,57 @@
-import React, { useEffect, useState } from 'react'
 import { CircularProgress, makeStyles, Theme } from '@material-ui/core'
-import { DisplayableMessage } from '@quiet/state-manager'
-import { useModal } from '../../../containers/hooks'
-import { ModalName } from '../../../sagas/modals/modals.types'
-import UploadedFileModal from './UploadedFileModal'
+import React from 'react'
 import imagePlaceholderIcon from '../../../static/images/imagePlaceholderIcon.svg'
 import Icon from '../../ui/Icon/Icon'
 
 const useStyles = makeStyles<Theme>(theme => ({
-  image: {
-    maxWidth: '100%'
-  },
-  container: {
-    maxWidth: '400px',
-    cursor: 'pointer'
-  },
   placeholderWrapper: {
-    maxWidth: '400px'
+    maxWidth: '400px',
+    minHeight: '60px',
+    height: '100%'
   },
   placeholder: {
     display: 'flex',
     alignContent: 'center',
     justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginLeft: '0.5em'
+    alignItems: 'center'
   },
   placeholderIcon: {
     marginRight: '0.5em'
   },
   fileName: {
-    color: 'gray'
+    color: theme.palette.colors.darkGray,
+    margin: 0
   }
 }))
+
+interface UploadedFilenameProps {
+  fileName: string
+}
+
+export const UploadedFilename: React.FC<UploadedFilenameProps> = ({
+  fileName
+}) => {
+  const classes = useStyles({})
+  return (
+    <p className={classes.fileName}>{fileName}</p>
+  )
+}
 
 interface UploadedFilePlaceholderProps {
   imageWidth: number,
   imageHeight: number,
-  fileName: string,
-  fileExt: string 
+  fileName: string
 }
 
 export const UploadedFilePlaceholder: React.FC<UploadedFilePlaceholderProps> = ({
   imageWidth,
   imageHeight,
-  fileName,
-  fileExt
+  fileName
  }) => {
   const classes = useStyles({})
   return (
-    <div className={classes.placeholderWrapper} style={{aspectRatio: '' + imageWidth / imageHeight}} data-testid={'imagePlaceholder'} >
-      <p className={classes.fileName}>{fileName}{fileExt}</p>
+    <div className={classes.placeholderWrapper} style={{aspectRatio: '' + imageWidth / imageHeight }} data-testid={'imagePlaceholder'} >
+      <UploadedFilename fileName={fileName}/>
       <div className={classes.placeholder}>
         <Icon src={imagePlaceholderIcon} className={classes.placeholderIcon}/>
         <CircularProgress color='inherit' size={16} />
