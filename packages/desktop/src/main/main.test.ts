d3402f5f7ca14c0124f25c05caeb1c45d7bcaa16
Dinek007 <damian.wilk991213@gmail.com>
2022-06-20 13:36:51 +0200

    change

--- packages/desktop/src/main/main.test.ts
+++ packages/desktop/src/main/main.test.ts
@@ -48,7 +48,7 @@ jest.mock('child_process', () => {
   }
 })
 
-jest.mock('electron', () => {
+jest.mock('electron ', () => {
   return {
     // @ts-expect-error
     ...jest.requireActual('electron'),
