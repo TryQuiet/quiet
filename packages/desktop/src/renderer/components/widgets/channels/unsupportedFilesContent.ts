e45b680c6ceb63affea29d0b0ab94b2c5b95808f
siepra <wiktor.siepra@gmail.com>
2022-06-21 15:49:38 +0200

    change

--- packages/desktop/src/renderer/components/widgets/channels/unsupportedFilesContent.ts
+++ packages/desktop/src/renderer/components/widgets/channels/unsupportedFilesContent.ts
@@ -9,7 +9,7 @@ export enum unsuportedFileContent {
   tryUploadZip = 'Try uploading a .zip of this instead.',
   sendOtherFile = 'The other file will be sent',
   sendOtherFiles = 'The other files will be sent',
-  currentUnsupportedModalContent = 'Sorry, but some of the files you added are not supported yet.'
+  currentUnsupportedModalContent = 'Sorry, but some of the files you added are not supported yet. Only image files can be uploaded.'
 }
 
 export const supportedFilesExtensions = ['.jpg', '.jpeg', '.png', '.gif']
