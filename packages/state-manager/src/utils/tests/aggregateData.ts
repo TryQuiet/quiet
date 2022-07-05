38a47469b80c9df96e1f64991e264b24409e8ebc
EmiM <emilia.marczyk@rumblefish.pl>
2022-07-05 15:36:01 +0200

    Modify aggregating performance data - it now parses the whole directory

--- packages/state-manager/src/utils/tests/aggregateData.ts
+++ packages/state-manager/src/utils/tests/aggregateData.ts
@@ -1,21 +1,27 @@
 import os from 'os'
 import fs from 'fs'
+import path from 'path'
 
-const path = `${os.homedir()}/data.json`
-
-let dataSet = []
-
-if (fs.existsSync(path)) {
-  const data = fs.readFileSync(path, 'utf-8')
-  dataSet = JSON.parse(data)
-}
+const dirPath = process.argv[2]
+const files = fs.readdirSync(dirPath || `${os.homedir()}/s3data`)
 
 const delaysArr = []
 
-dataSet.forEach((_o) => {
-  const delay = Object.values(_o)[0]
-  delaysArr.push(delay)
+files.forEach((fileName) => {
+  let filePath = path.join(dirPath, fileName)
+  let dataSet = []
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf-8')
+    dataSet = JSON.parse(data)
+  }
+  
+  dataSet.forEach((_o) => {
+    const delay = Object.values(_o)[0]
+    delaysArr.push(delay)
+  })
 })
+
+console.log(delaysArr)
 
 const accumulated = delaysArr.reduce((previousValue, currentValue) => {
   return previousValue + currentValue
