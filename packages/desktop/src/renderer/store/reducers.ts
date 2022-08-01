f7513c0b64d71eeed4290a9df1fc9fa04202da7c
siepra <wiktor.siepra@gmail.com>
2022-08-01 15:18:24 -0400

    Transform outbound state for files reducer

--- packages/desktop/src/renderer/store/reducers.ts
+++ packages/desktop/src/renderer/store/reducers.ts
@@ -4,7 +4,7 @@ import createElectronStorage from 'redux-persist-electron-storage'
 import path from 'path'
 import { persistReducer } from 'redux-persist'
 
-import stateManagerReducers, { storeKeys as StateManagerStoreKeys, PublicChannelsTransform, MessagesTransform } from '@quiet/state-manager'
+import stateManagerReducers, { storeKeys as StateManagerStoreKeys, PublicChannelsTransform, MessagesTransform, FilesTransform } from '@quiet/state-manager'
 
 import { StoreType } from './handlers/types'
 import { StoreKeys } from './store.keys'
@@ -42,7 +42,7 @@ const persistConfig = {
     StateManagerStoreKeys.Users,
     StoreKeys.App
   ],
-  transforms: [PublicChannelsTransform, MessagesTransform]
+  transforms: [PublicChannelsTransform, MessagesTransform, FilesTransform]
 }
 
 export const reducers = {
