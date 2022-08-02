65c02a1908808f9372c4dfdec0d01c86180ee1d3
siepra <wiktor.siepra@gmail.com>
2022-08-02 10:05:26 -0400

    Lint fix

--- packages/state-manager/src/sagas/files/files.transform.ts
+++ packages/state-manager/src/sagas/files/files.transform.ts
@@ -11,13 +11,13 @@ export const FilesTransform = createTransform(
   (outboundState: FilesState, _key) => {
     const downloadStatuses = Object.values(outboundState.downloadStatus.entities)
     const updatedStatuses: DownloadStatus[] = downloadStatuses.reduce((result, status) => {
-        const downloadState = status.downloadState
-        const entry: DownloadStatus = {
-          ...status,
-          downloadState: downloadState !== DownloadState.Canceling ? downloadState : DownloadState.Canceled
-        }
-        result.push(entry)
-        return result
+      const downloadState = status.downloadState
+      const entry: DownloadStatus = {
+        ...status,
+        downloadState: downloadState !== DownloadState.Canceling ? downloadState : DownloadState.Canceled
+      }
+      result.push(entry)
+      return result
     }, [])
     return {
       ...outboundState,
