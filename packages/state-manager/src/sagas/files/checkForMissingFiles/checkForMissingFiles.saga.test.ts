e2ef367107c55ebd7e98c7e04d74d82a586fa6db
siepra <wiktor.siepra@gmail.com>
2022-08-09 13:24:18 +0200

    Regression test

--- packages/state-manager/src/sagas/files/checkForMissingFiles/checkForMissingFiles.saga.test.ts
+++ packages/state-manager/src/sagas/files/checkForMissingFiles/checkForMissingFiles.saga.test.ts
@@ -170,7 +170,84 @@ describe('checkForMissingFilesSaga', () => {
       .run()
   })
 
-  test('do not download file after restarting the app if file is to large', async () => {
+  test('do not download file after restarting the app if file exceeds autodownload size limit', async () => {
+    const initialState = (await prepareStore()).store
+
+    const factory = await getFactory(initialState)
+
+    const community = await factory.create<
+    ReturnType<typeof communities.actions.addNewCommunity>['payload']
+    >('Community')
+
+    const alice = await factory.create<
+    ReturnType<typeof identity.actions.addNewIdentity>['payload']
+    >('Identity', { id: community.id, nickname: 'alice' })
+
+    const message = Math.random().toString(36).substr(2.9)
+    const channelAddress = 'general'
+
+    const missingFile: FileMetadata = {
+      cid: Math.random().toString(36).substr(2.9),
+      path: null,
+      name: 'test-file',
+      ext: '.zip',
+      message: {
+        id: message,
+        channelAddress: channelAddress
+      },
+      size: AUTODOWNLOAD_SIZE_LIMIT + 2048
+    }
+
+    await factory.create<ReturnType<typeof publicChannels.actions.test_message>['payload']>(
+      'Message',
+      {
+        identity: alice,
+        message: {
+          id: message,
+          type: MessageType.File,
+          message: '',
+          createdAt: DateTime.utc().valueOf(),
+          channelAddress: 'general',
+          signature: '',
+          pubKey: '',
+          media: missingFile
+        }
+      }
+    )
+
+    const store = (await prepareStore(initialState.getState())).store
+    const socket = { emit: jest.fn(), on: jest.fn() } as unknown as Socket
+
+    store.dispatch(filesActions.updateDownloadStatus({
+      mid: missingFile.message.id,
+      cid: missingFile.cid,
+      downloadState: DownloadState.Ready
+    }))
+
+    const reducer = combineReducers(reducers)
+    await expectSaga(
+      checkForMissingFilesSaga,
+      socket,
+      connectionActions.addInitializedCommunity(community.id)
+    )
+      .withReducer(reducer)
+      .withState(store.getState())
+      .not.put(filesActions.updateDownloadStatus({
+        mid: missingFile.message.id,
+        cid: missingFile.cid,
+        downloadState: DownloadState.Queued
+      }))
+      .not.apply(socket, socket.emit, [
+        SocketActionTypes.DOWNLOAD_FILE,
+        {
+          peerId: alice.peerId.id,
+          metadata: missingFile
+        }
+      ])
+      .run()
+  })
+
+  test('resume download after restarting the app even if file exceeds autodownload size limit (download started manually)', async () => {
     const initialState = (await prepareStore()).store
 
     const factory = await getFactory(initialState)
@@ -232,12 +309,12 @@ describe('checkForMissingFilesSaga', () => {
     )
       .withReducer(reducer)
       .withState(store.getState())
-      .not.put(filesActions.updateDownloadStatus({
+      .put(filesActions.updateDownloadStatus({
         mid: missingFile.message.id,
         cid: missingFile.cid,
         downloadState: DownloadState.Queued
       }))
-      .not.apply(socket, socket.emit, [
+      .apply(socket, socket.emit, [
         SocketActionTypes.DOWNLOAD_FILE,
         {
           peerId: alice.peerId.id,
