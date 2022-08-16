2137e2339333cbc42eb507ad6d0bbf0c6ebb1431
Wiktor <wiktor.sieprawski@rumblefish.pl>
2022-08-16 13:26:13 +0200

    Complete ChannelList screen

--- packages/mobile/src/components/ChannelTile/ChannelTile.types.ts
+++ packages/mobile/src/components/ChannelTile/ChannelTile.types.ts
@@ -1,6 +1,8 @@
 export interface ChannelTileProps {
     name: string
+    address: string
     message: string
     date: string
     unread: boolean
+    redirect: (address: string) => void
 }
--- packages/mobile/src/screens/ChannelList/ChannelList.screen.tsx
+++ packages/mobile/src/screens/ChannelList/ChannelList.screen.tsx
@@ -1,11 +1,46 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { View } from 'react-native'
-import { Typography } from '../../components/Typography/Typography.component'
+import { publicChannels } from '@quiet/state-manager'
+import { ChannelList as ChannelListComponent } from '../../components/ChannelList/ChannelList.component'
+import { ChannelTileProps } from '../../components/ChannelTile/ChannelTile.types'
+import { formatMessageDisplayDate } from '../../utils/functions/formatMessageDisplayDate/formatMessageDisplayDate'
+import { replaceScreen } from '../../utils/functions/replaceScreen/replaceScreen'
+import { ScreenNames } from '../../const/ScreenNames.enum'
 
 export const ChannelList: FC = () => {
+  const dispatch = useDispatch()
+
+  const redirect = useCallback((address: string) => {
+    dispatch(publicChannels.actions.setCurrentChannel({
+      channelAddress: address
+    }))
+    replaceScreen(ScreenNames.ChannelScreen)
+  }, [dispatch])
+
+  const channels = useSelector(publicChannels.selectors.channelsStatus)
+
+  const tiles = Object.values(channels).map(status => {
+    const newestMessage = status.newestMessage
+
+    const message = newestMessage.message
+    const date = formatMessageDisplayDate(newestMessage.createdAt)
+
+    const tile: ChannelTileProps = {
+      name: status.address,
+      address: status.address,
+      message: message,
+      date: date,
+      unread: status.unread,
+      redirect: redirect
+    }
+
+    return tile
+  })
+
   return (
     <View style={{ flex: 1 }}>
-      <Typography fontSize={14}>todo</Typography>
+      <ChannelListComponent tiles={tiles} />
     </View>
   )
 }
