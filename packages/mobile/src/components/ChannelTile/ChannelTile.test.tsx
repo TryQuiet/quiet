df511af1cd05e2da6f4294a4bf714c81f58adc44
Wiktor <wiktor.sieprawski@rumblefish.pl>
2022-08-17 10:04:47 +0200

    Add missing props to test

--- packages/mobile/src/components/ChannelTile/ChannelTile.test.tsx
+++ packages/mobile/src/components/ChannelTile/ChannelTile.test.tsx
@@ -213,11 +213,13 @@ describe('ChannelList component', () => {
     const { toJSON } = renderComponent(
       <ChannelTile
         name={'general'}
+        address={'general'}
         message={
           'Text from latest chat message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id massa venenatis id eget massa commodo posuere faucibus aliquam. At scelerisque nisi mauris facilisis.'
         }
         date={'1:55pm'}
         unread={true}
+        redirect={jest.fn()}
       />
     )
 
