40859df30c6a35999f68a5cca983e68f61f44951
Wiktor <wiktor.sieprawski@rumblefish.pl>
2022-08-18 15:15:51 +0200

    Change stories names

--- packages/mobile/src/components/Appbar/Appbar.stories.tsx
+++ packages/mobile/src/components/Appbar/Appbar.stories.tsx
@@ -5,5 +5,5 @@ import React from 'react'
 import { Appbar } from './Appbar.component'
 
 storiesOf('Appbar', module)
-  .add('Default', () => <Appbar title={'general'} prefix={'#'} back={() => { console.log('back') }} />)
-  .add('No arrow', () => <Appbar title={'Quiet'} position={'flex-start'} />)
+  .add('Channel', () => <Appbar title={'general'} prefix={'#'} back={() => { console.log('back') }} />)
+  .add('Community', () => <Appbar title={'Quiet'} position={'flex-start'} />)
