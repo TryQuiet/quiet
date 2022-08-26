aa6e1a26bea33b572069ecaf97826e60bf20c579
Wiktor <wiktor.sieprawski@rumblefish.pl>
2022-08-26 10:14:25 +0200

    Make tor main thread dependant

--- packages/mobile/android/app/src/main/java/com/zbaymobile/TorModule.kt
+++ packages/mobile/android/app/src/main/java/com/zbaymobile/TorModule.kt
@@ -1,47 +1,123 @@
 package com.zbaymobile
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.os.Bundle
-import android.os.IBinder
+import android.app.Application
+import android.util.Log
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule
-import com.zbaymobile.Utils.Const.SERVICE_ACTION_EXECUTE
+import com.zbaymobile.Utils.Const
+import com.zbaymobile.Utils.Utils
+import com.zbaymobile.Utils.toHex
+import org.torproject.android.binary.TorResourceInstaller
 import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
 
-class TorModule(private val context: ReactApplicationContext): ReactContextBaseJavaModule(), TorService.Callbacks {
-
-    private var torService: TorService? = null
+class TorModule(private val context: ReactApplicationContext): ReactContextBaseJavaModule() {
 
     override fun getName(): String {
         return "TorModule"
     }
 
+    private var torrc: File? = null
+
+    // Directory containing tor files
+    private var dataDirectory: File? = null
+
     @ReactMethod
-    fun startTor(httpTunnelPort: Int, socksPort: Int, controlPort: Int) {
-        
-        val service = Intent(context, TorService::class.java)
-            service.action = SERVICE_ACTION_EXECUTE
-            service.putExtra("httpTunnelPort", httpTunnelPort)
-            service.putExtra("socksPort", socksPort)
-            service.putExtra("controlPort", controlPort)
+    private fun startTor(httpTunnelPort: Int, socksPort: Int, controlPort: Int) {
+        /**
+         * Default torrc file is being created by tor-android lib
+         * so there is a need for overwrite it with custom file
+         * containing all the proper configuration
+         **/
+        torrc = File(context.filesDir, "torrc")
 
-        context.startService(service)
+        val torrcCustom: File? = updateTorrcCustomFile(httpTunnelPort, socksPort, controlPort)
+        if ((torrcCustom?.exists()) == false || (torrcCustom?.canRead()) == false)
+            return
 
-        val serviceConnection = object: ServiceConnection {
-            override fun onServiceConnected(p0: ComponentName?, binder: IBinder?) {
-                torService = (binder as TorService.LocalBinder).getService()
-                torService?.bindClient(this@TorModule)
+        val torBinary = TorResourceInstaller(context, context.filesDir).installResources()
+
+        if(runTorCommand(torBinary, torrcCustom)) {
+            var authCookie: String = ""
+
+            val cookie = File(dataDirectory, "control_auth_cookie")
+            if(cookie.exists()) {
+                authCookie = cookie.readBytes().toHex()
             }
 
-            override fun onServiceDisconnected(p0: ComponentName?) {
-                torService?.unbindClient()
-            }
+            onTorInit(httpTunnelPort, socksPort, controlPort, authCookie)
+        }
+    }
+
+    private fun runTorCommand(torBinary: File, torrcCustom: File?): Boolean {
+        dataDirectory = context.getDir("data", Application.MODE_PRIVATE)
+
+        val command = arrayOf(
+            torBinary.canonicalPath,
+            "DataDirectory", dataDirectory?.canonicalPath,
+            "--defaults-torrc", torrcCustom?.canonicalPath
+        )
+
+        val exitCode = try {
+            val process = Utils.exec(
+                dir = null,
+                command = command,
+                env = null
+            )
+            Utils.getOutput(process)
+        } catch (e: Exception) {
+            Log.e(Const.TAG_TOR_ERR, "Tor was unable to start: " + e.message, e)
+            return false
         }
 
-        context.bindService(service, serviceConnection, Context.BIND_AUTO_CREATE)
+        if(exitCode != 0) {
+            Log.e(Const.TAG_TOR_ERR, "Tor did not start. Exit: $exitCode")
+            return false
+        }
+
+        return true
+    }
+
+    private fun updateTorrcCustomFile(httpTunnelPort: Int, socksPort: Int, controlPort: Int): File? {
+        val extraLines = StringBuffer()
+
+        extraLines.append("RunAsDaemon 1").append('\n')
+        extraLines.append("CookieAuthentication 1").append('\n')
+        extraLines.append("ControlPort ").append(controlPort).append('\n')
+        extraLines.append("SOCKSPort ").append(socksPort).append('\n')
+        extraLines.append("HTTPTunnelPort ").append(httpTunnelPort).append('\n')
+
+        Log.d("TORRC","torrc.custom=\n$extraLines")
+
+        val fileTorrcCustom = File(torrc?.absolutePath.toString() + ".custom")
+        val success: Boolean = updateTorConfigCustom(fileTorrcCustom, extraLines.toString())
+
+        return if (success && fileTorrcCustom.exists()) {
+            fileTorrcCustom
+        } else {
+            null
+        }
+    }
+
+    private fun updateTorConfigCustom(fileTorrcCustom: File?, extraLines: String?): Boolean {
+        val fos = FileWriter(fileTorrcCustom, false)
+        val ps = PrintWriter(fos)
+        ps.print(extraLines)
+        ps.flush()
+        ps.close()
+        return true
+    }
+
+    private fun onTorInit(httpTunnelPort: Int, socksPort: Int, controlPort: Int, authCookie: String) {
+        val payload: WritableMap = Arguments.createMap()
+        payload.putInt("httpTunnelPort", httpTunnelPort)
+        payload.putInt("socksPort", socksPort)
+        payload.putInt("controlPort", controlPort)
+        payload.putString("authCookie", authCookie)
+        context
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+                .emit("onTorInit", payload)
     }
 
     @ReactMethod
@@ -54,27 +130,6 @@ class TorModule(private val context: ReactApplicationContext): ReactContextBaseJ
         context
             .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
             .emit("onDataDirectoryCreated", path)
-    }
-
-    @ReactMethod
-    fun initPushNotifications() {
-        val service = Intent(context, PushNotificationsService::class.java)
-        val bundle = Bundle()
-
-        service.putExtras(bundle)
-
-        context.startService(service)
-    }
-
-    override fun onTorInit(httpTunnelPort: Int, socksPort: Int, controlPort: Int, authCookie: String) {
-        val payload: WritableMap = Arguments.createMap()
-        payload.putInt("httpTunnelPort", httpTunnelPort)
-        payload.putInt("socksPort", socksPort)
-        payload.putInt("controlPort", controlPort)
-        payload.putString("authCookie", authCookie)
-        context
-            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-                .emit("onTorInit", payload)
     }
 
 }
