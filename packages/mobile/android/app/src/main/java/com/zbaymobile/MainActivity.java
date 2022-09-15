dd50ea0bc97210dc6fcba9d426da46a10550d4a7
vinkabuki <vincent.kabacky@gmail.com>
2022-09-15 14:55:40 +0200

    Remove unused imports and code

--- packages/mobile/android/app/src/main/java/com/zbaymobile/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/zbaymobile/MainActivity.java
@@ -1,23 +1,12 @@
 package com.zbaymobile;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.ReactContext;
-import android.content.Intent;
-import android.content.Context;
-import android.util.AttributeSet;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-
 import com.facebook.react.bridge.ReactApplicationContext;
-
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 public class MainActivity extends ReactActivity  {
 
@@ -25,24 +14,6 @@ public class MainActivity extends ReactActivity  {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-
-   private void sendEvent() {
-    Intent intent = getIntent();
-
-    ReactInstanceManager mReactInstanceManager = getReactNativeHost().getReactInstanceManager();
-    ReactApplicationContext reactContext = (ReactApplicationContext) mReactInstanceManager.getCurrentReactContext();
-
-
-    Bundle bundle = intent.getExtras();
-    if (bundle != null) {
-        for (String key : bundle.keySet()) {
-            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("notification", bundle.get(key));
-        }
-    }
-   }
-
-
-
   @Override
   protected String getMainComponentName() {
     return "ZbayMobile";
