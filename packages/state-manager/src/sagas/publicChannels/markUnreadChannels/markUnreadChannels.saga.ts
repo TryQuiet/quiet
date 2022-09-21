fe692c644161b3a3bf187ea18e8fe3146309c0ed
vinkabuki <vincent.kabacky@gmail.com>
2022-09-21 10:52:09 +0200

    Do not mark unread channels on initial sync

--- packages/state-manager/src/sagas/publicChannels/markUnreadChannels/markUnreadChannels.saga.ts
+++ packages/state-manager/src/sagas/publicChannels/markUnreadChannels/markUnreadChannels.saga.ts
@@ -4,6 +4,7 @@ import { publicChannelsActions } from '../publicChannels.slice'
 import { publicChannelsSelectors } from '../publicChannels.selectors'
 import { messagesActions } from '../../messages/messages.slice'
 import { MarkUnreadChannelPayload } from '../publicChannels.types'
+import { identitySelectors } from '../../identity/identity.selectors'
 
 export function* markUnreadChannelsSaga(
   action: PayloadAction<ReturnType<typeof messagesActions.incomingMessages>['payload']>
@@ -22,6 +23,11 @@ export function* markUnreadChannelsSaga(
 
       const statuses = yield* select(publicChannelsSelectors.channelsStatus)
 
+      const joinTimestamp = yield* select(identitySelectors.joinTimestamp)
+
+      // For all messages created before user joined don't show notifications
+      if (joinTimestamp > message.createdAt * 1000) continue
+      // If there are newer messages in the channel, don't show notification
       if (statuses[message.channelAddress]?.newestMessage?.createdAt > message.createdAt) continue
 
       yield* put(
