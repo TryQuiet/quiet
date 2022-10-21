204dd7f80767963fce17f71d9d66c416468627d7
dinek007 <damian.wilk991213@gmail.com>
2022-10-21 11:49:10 +0200

    fix files

--- packages/state-manager/src/sagas/messages/restoreVerificationStatuses/restoreVerificationStatuses.saga.ts
+++ packages/state-manager/src/sagas/messages/restoreVerificationStatuses/restoreVerificationStatuses.saga.ts
@@ -1,27 +1,57 @@
 import { select, put } from 'typed-redux-saga'
 import { messagesActions } from '../messages.slice'
 import { messagesSelectors } from '../messages.selectors'
+import { filesSelectors } from '../../files/files.selectors'
+import { ChannelMessage } from '../../publicChannels/publicChannels.types'
+import { filesActions } from '../../files/files.slice'
 
 export function* restoreVerificationStatusesSaga(): Generator {
   const verificationStatuses = yield* select(messagesSelectors.messagesVerificationStatus)
-  const unverificatdStatueses = Object.values(verificationStatuses).filter((status) => {
-    return typeof status?.isVerified !== 'boolean'
-  })
+  const filesStatuses = yield* select(filesSelectors.downloadStatuses)
   const channelsMessages = yield* select(messagesSelectors.publicChannelsMessagesBase)
 
-  const messagesToRemove: Array<{id: string; address: string}> = []
-  unverificatdStatueses.forEach((item) => {
+  const unverificatedStatueses = Object.values(verificationStatuses).filter((status) => {
+      return typeof status?.isVerified !== 'boolean'
+    })
+
+  const messagesToRemove: ChannelMessage[] = []
+  const filesToRemove: ChannelMessage[] = []
+  unverificatedStatueses.forEach((item) => {
     Object.values(channelsMessages).forEach((channelMessages) => {
       Object.values(channelMessages.messages.entities).forEach((message) => {
         if (message.signature === item.signature) {
-            messagesToRemove.push({ id: message.id, address: channelMessages.channelAddress })
+            messagesToRemove.push(message)
+            if (message.media && message.type === 2) {
+                filesToRemove.push(message)
+            }
         }
       })
     })
   })
 
-  for (const message of messagesToRemove) {
-      yield* put(messagesActions.removeMessageVerificationStatus(message.id))
-      yield* put(messagesActions.removePublicChannelMessage(message))
+  for (const message of filesToRemove) {
+    yield* put(filesActions.removeDownloadStatus({ cid: message.media.message.id }))
   }
+  for (const message of messagesToRemove) {
+    yield* put(messagesActions.removeMessageVerificationStatus(message.id))
+    yield* put(messagesActions.removePublicChannelMessage({ address: message.channelAddress, id: message.id }))
+  }
+
+  const filesToUpdate: ChannelMessage[] = []
+  Object.values(channelsMessages).forEach((channelMessages) => {
+    Object.values(channelMessages.messages.entities).forEach((message) => {
+      if (message.media && message.type === 2) {
+          if (!message.media.path) {
+            filesToUpdate.push(message)
+          }
+      }
+    })
+  })
+  for (const message of filesToUpdate) {
+    if (filesStatuses[message.media.message.id]) {
+        yield* put(filesActions.removeDownloadStatus({ cid: message.media.message.id }))
+    }
+    yield* put(messagesActions.removeMessageVerificationStatus(message.id))
+    yield* put(messagesActions.removePublicChannelMessage({ address: message.channelAddress, id: message.id }))
+}
 }
