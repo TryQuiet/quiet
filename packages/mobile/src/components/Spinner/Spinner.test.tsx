4b0ca630e4a61171841b303265e35847897aad51
Wiktor <wiktor.sieprawski@rumblefish.pl>
2022-11-21 11:20:08 +0100

    Update test snapshots

--- packages/mobile/src/components/Spinner/Spinner.test.tsx
+++ packages/mobile/src/components/Spinner/Spinner.test.tsx
@@ -4,6 +4,45 @@ import { Spinner } from './Spinner.component'
 describe('Spinner component', () => {
   it('renders component', () => {
     const { toJSON } = renderComponent(<Spinner description='Connecting to peers' />)
-    expect(toJSON).toMatchInlineSnapshot()
+    expect(toJSON()).toMatchInlineSnapshot(`
+      <View
+        style={
+          Object {
+            "alignItems": "center",
+            "backgroundColor": "#ffffff",
+            "flex": 1,
+            "justifyContent": "center",
+          }
+        }
+      >
+        <ActivityIndicator
+          color="#67BFD3"
+          size="large"
+        />
+        <Text
+          color="main"
+          fontSize={14}
+          horizontalTextAlign="center"
+          style={
+            Array [
+              Object {
+                "color": "#000000",
+                "fontFamily": "Rubik-Regular",
+                "fontSize": 14,
+                "textAlign": "center",
+                "textAlignVertical": "center",
+              },
+              Object {
+                "margin": 10,
+                "maxWidth": 200,
+              },
+            ]
+          }
+          verticalTextAlign="center"
+        >
+          Connecting to peers
+        </Text>
+      </View>
+    `)
   })
 })
