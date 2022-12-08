14e453ee3787e9e15c5046aef0bbece30a3a4fc1
Emi <emilia.marczyk@rumblefish.pl>
2022-12-08 13:25:36 +0100

    Fix jdenticon import

--- packages/desktop/src/renderer/components/Jdenticon/Jdenticon.tsx
+++ packages/desktop/src/renderer/components/Jdenticon/Jdenticon.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import jdenticon from 'jdenticon'
+import * as jdenticon from 'jdenticon'
 
 // Copied from react-jdenticon because its peer dependency clashes with react 18
 const Jdenticon = ({ value = 'test', size = '100%' }) => {
