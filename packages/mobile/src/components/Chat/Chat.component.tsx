4a3d38cb9352dae0e6196f3ad023065addd9b6b7
Emi <emilia.marczyk@rumblefish.pl>
2023-01-13 12:11:59 +0100

    Add a change to make lerna create a new version

--- packages/mobile/src/components/Chat/Chat.component.tsx
+++ packages/mobile/src/components/Chat/Chat.component.tsx
@@ -36,6 +36,8 @@ export const Chat: FC<ChatProps & FileActionsProps> = ({
 
   const defaultPadding = 20
 
+  console.log('channel', channel.name)
+
   useEffect(() => {
     const onKeyboardDidShow = () => {
       setKeyboardShow(true)
