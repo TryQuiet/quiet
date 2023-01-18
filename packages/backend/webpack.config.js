bfa420845e5a50a50246710c57991027c6e80a96
Vin Kabuki <vincent.kabacky@gmail.com>
2023-01-18 09:30:31 +0100

    Basic webpack config, successfull bundling

--- packages/backend/webpack.config.js
+++ packages/backend/webpack.config.js
@@ -1,16 +1,41 @@
 import path from 'path'
+import { fileURLToPath } from 'url';
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
     mode: 'development',
-    entry: { 
-        bundle: path.resolve(__dirname, 'src/index.ts') 
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.ts')
     },
     output: {
         path: path.resolve(__dirname, 'lib'),
         filename: '[name].js'
     },
-    module: {
-        rules: []
+    resolve: {
+        extensions: ['.ts', '.js'],
+        fallback: {
+            "fs": false,
+            "net": false,
+            "child_process": false,
+            "async_hooks": false,
+            "tls": false
+          } 
     },
-    plugins: []
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    'ts-loader'
+                ],
+                exclude: [/node_modules/, /packages[\/\\]identity/, /packages[\/\\]state-manager/, /packages[\/\\]logger/]
+            }
+        ]
+    },
+    plugins: [
+        new NodePolyfillPlugin()
+    ]
 }
