8cc7558f733159bee06106e9755b089905a9f685
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-01-23 16:12:35 +0100

    fix

--- packages/e2e-tests/runTests.ts
+++ packages/e2e-tests/runTests.ts
@@ -5,25 +5,25 @@ import { hideBin } from 'yargs/helpers'
 
 const argv = yargs(hideBin(process.argv)).argv
 
-const runTest = async () => {
-  const promExec = promisify(exec)
-  let command = 'npm run test:smoke'
-  if (argv.appPath) {
-    command += ` -- --appPath=${argv.appPath as string}`
-  }
+// const runTest = async () => {
+//   const promExec = promisify(exec)
+//   let command = 'npm run test:smoke'
+//   if (argv.appPath) {
+//     command += ` -- --appPath=${argv.appPath as string}`
+//   }
 
-  for (let i = 1; i <= Number(argv.iter); i++) {
-    console.log(`Starting ${i} test`)
-    const output = await promExec(command)
+//   for (let i = 1; i <= Number(argv.iter); i++) {
+//     console.log(`Starting ${i} test`)
+//     const output = await promExec(command)
 
-    console.log(output.stdout.trim())
-    console.log(output.stderr.trim())
-    console.log(`Finished ${i} test`)
-  }
-}
+//     console.log(output.stdout.trim())
+//     console.log(output.stderr.trim())
+//     console.log(`Finished ${i} test`)
+//   }
+// }
 
-runTest().then((data) => {
-  console.log('DONE', data)
-}).catch((error) => {
-  console.error('ERROR', error)
-})
+// runTest().then((data) => {
+//   console.log('DONE', data)
+// }).catch((error) => {
+//   console.error('ERROR', error)
+// })
