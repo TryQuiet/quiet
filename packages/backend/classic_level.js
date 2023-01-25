e9f26af14ed0bb14e327c5859ecbc4b54ba1d85a
Vin Kabuki <vincent.kabacky@gmail.com>
2023-01-25 17:54:58 +0100

    Resolve classicLevel bindings per platform

--- packages/backend/classic_level.js
+++ packages/backend/classic_level.js
@@ -1,7 +1,19 @@
 
 import { createRequire } from 'node:module';
+import path from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url';
 const require = createRequire(import.meta.url);
 
-export const bindings = require('./classic_level.node');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let bindings = null
+
+const binaryPath = path.normalize(path.join(__dirname, 'deps', process.platform, process.arch, 'classic-level', 'classic_level.node'))
+
+if (!fs.existsSync(binaryPath)) throw new Error(`Unfortunately we do not support this platform! There is no classic_level bindings binary for ${process.platform}-${process.arch}`)
+
+bindings = require(binaryPath);
 
 export default bindings
