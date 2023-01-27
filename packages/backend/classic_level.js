b9e62247bbb7c83f49097b28aead861b48529513
Vin Kabuki <vincent.kabacky@gmail.com>
2023-01-27 09:09:19 +0100

    Use relative path in classic_level importer

--- packages/backend/classic_level.js
+++ packages/backend/classic_level.js
@@ -2,18 +2,19 @@
 import { createRequire } from 'node:module';
 import path from 'path'
 import fs from 'fs'
-import { fileURLToPath } from 'url';
 const require = createRequire(import.meta.url);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 let bindings = null
 
-const binaryPath = path.normalize(path.join(__dirname, 'deps', process.platform, process.arch, 'classic-level', 'classic_level.node'))
+console.log('process.platform', process.platform)
+console.log('process.arch', process.arch)
+
+const binaryPath = path.normalize(path.join('..', '/deps', process.platform, process.arch, 'classic-level', 'classic_level.node'))
 
 if (!fs.existsSync(binaryPath)) throw new Error(`Unfortunately we do not support this platform! There is no classic_level bindings binary for ${process.platform}-${process.arch}`)
 
 bindings = require(binaryPath);
+
+console.log(bindings)
 
 export default bindings
