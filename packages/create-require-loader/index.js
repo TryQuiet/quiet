cc71c9597671d6dded058e4555e6a2e905386187
Vin Kabuki <vincent.kabacky@gmail.com>
2023-01-31 16:38:03 +0100

    Use normalized path in create-require-loader and restore setEnvs script for electron builder

--- packages/create-require-loader/index.js
+++ packages/create-require-loader/index.js
@@ -1,16 +1,18 @@
+const path = require('path')
+
 module.exports = async function createRequireLoader(content, map, meta) {
   const callback = this.async();
 
   let updatedContent
-  if (content.includes("const pkg = req('../../package.json')")) {
+  if (content.includes("const pkg = req('../../package.json')")) {  
     updatedContent = content.replace(
       "const pkg = req('../../package.json')",
-      "import pkg from '../../package.json'"
+      `import pkg from '${path.normalize("../../package.json")}'`
     )
   } else {
     updatedContent = content.replace(
       "const binding = require('./binding')",
-      "const binding = require('./binding').default"
+      `const binding = require(${path.normalize('./binding')}).default`
     )
   }
 
