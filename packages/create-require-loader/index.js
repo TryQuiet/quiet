dbbc6887fbd513640f3323493d92ab6137a2380b
Vin Kabuki <vincent.kabacky@gmail.com>
2023-01-31 21:08:08 +0100

    [CI] Use webpack development mode for production

--- packages/create-require-loader/index.js
+++ packages/create-require-loader/index.js
@@ -4,15 +4,15 @@ module.exports = async function createRequireLoader(content, map, meta) {
   const callback = this.async();
 
   let updatedContent
-  if (content.includes("const pkg = req('../../package.json')")) {  
+  if (content.includes("const pkg = req('../../package.json')")) {
     updatedContent = content.replace(
       "const pkg = req('../../package.json')",
-      `import pkg from '${path.normalize("../../package.json")}'`
+      `import pkg from '${path.join("..", "..", "package.json")}'`
     )
   } else {
     updatedContent = content.replace(
       "const binding = require('./binding')",
-      `const binding = require(${path.normalize('./binding')}).default`
+      "const binding = require('./binding').default"
     )
   }
 
