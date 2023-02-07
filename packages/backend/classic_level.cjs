e5f93101db47caf4fc25462b781a332a562d07ab
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-02-07 13:07:30 +0100

    Fix binary path nesting levels for android

--- packages/backend/classic_level.cjs
+++ packages/backend/classic_level.cjs
@@ -1,16 +1,25 @@
-import { createRequire } from 'node:module';
+import { createRequire } from 'node:module'
 import path from 'path'
 import fs from 'fs'
-const require = createRequire(import.meta.url);
+
+const require = createRequire(import.meta.url)
 
 let bindings = null
 
-const binaryPath = path.normalize(path.join(__dirname, '/deps', process.platform, process.arch, 'classic-level', 'classic_level.node'))
+let binaryPath = path.normalize(path.join(__dirname, '/deps', process.platform, process.arch, 'classic-level', 'classic_level.node'))
+let exists = fs.existsSync(binaryPath)
 
-console.log(binaryPath)
+if (!exists && process.platform === 'android') {
+  // Get rid of extra nesting levels
+  binaryPath = path.normalize(path.join(__dirname, process.arch, 'classic-level', 'classic_level.node'))
+  // Reassign boolean value for further comparision
+  exists = fs.existsSync(binaryPath)
+}
 
-if (!fs.existsSync(binaryPath)) throw new Error(`Unfortunately we do not support this platform! There is no classic_level bindings binary for ${process.platform}-${process.arch}`)
+if (!exists) {
+  throw new Error(`Unfortunately we do not support this platform! There is no classic_level bindings binary for ${process.platform}-${process.arch}`)
+}
 
-bindings = require(binaryPath);
+bindings = require(binaryPath)
 
 export default bindings
