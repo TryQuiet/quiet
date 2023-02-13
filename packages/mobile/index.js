445fb30729c06cb2a9975aa3b7d28efb56a836bf
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-02-13 15:04:53 +0100

    Clean up imports

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,24 +1,20 @@
+/* eslint-disable import/first */
 import './shim'
 /**
  * @format
  */
 require('node-libs-react-native/globals.js')
 
-import React, { lazy, Suspense } from 'react'
+import { Suspense } from 'react'
 import { AppRegistry } from 'react-native'
 import Config from 'react-native-config'
-
 import { Provider as StoreProvider } from 'react-redux'
-import { store } from './src/store/store'
-
 import { name } from './app.json'
-
+import { store } from './src/store/store'
 import { NodeEnv } from './src/utils/const/NodeEnv.enum'
 
 import Storybook from './storybook'
-
-// Lazy loading main app component doesn't work in storybook
-const App = lazy(() => import('./src/App'))
+import App from './src/App'
 
 const Main = Config.NODE_ENV === NodeEnv.Storybook ? Storybook : App
 
