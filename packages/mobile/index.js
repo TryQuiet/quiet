2de739ee21d088acea21cdad651d9d319a3abcc6
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-02-15 16:34:20 +0100

    Fix imports that were breaking crypto

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -5,25 +5,21 @@ import './shim'
  */
 require('node-libs-react-native/globals.js')
 
-import { Suspense } from 'react'
 import { AppRegistry } from 'react-native'
 import Config from 'react-native-config'
-import { Provider as StoreProvider } from 'react-redux'
 import { name } from './app.json'
-import { store } from './src/store/store'
+
 import { NodeEnv } from './src/utils/const/NodeEnv.enum'
+import StoreProvider from './src/Provider'
 
-import Storybook from './storybook'
-import App from './src/App'
-
-const Main = Config.NODE_ENV === NodeEnv.Storybook ? Storybook : App
-
-const Wrapper = () => (
-  <Suspense fallback={null}>
-    <StoreProvider store={store}>
-      <Main />
+if (Config.NODE_ENV !== NodeEnv.Storybook) {
+  const { default: App } = require('./src/App')
+  AppRegistry.registerComponent(name, () => () => (
+    <StoreProvider>
+      <App />
     </StoreProvider>
-  </Suspense>
-)
-
-AppRegistry.registerComponent(name, () => Wrapper)
+  ))
+} else {
+  const Storybook = require('./storybook')
+  AppRegistry.registerComponent(name, () => Storybook.default)
+}
