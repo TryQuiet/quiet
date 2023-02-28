e9dd8ca09acef10fc2249dfb03d49978bfb90de7
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-02-28 09:09:28 +0100

    check small version of sendMessages

--- packages/integration-tests/src/integrationTests/sendMessages.test.ts
+++ packages/integration-tests/src/integrationTests/sendMessages.test.ts
@@ -140,7 +140,7 @@ describe('send message - users go offline and online', () => {
 describe.only('send message - users are online', () => {
   let owner: AsyncReturnType<typeof createApp>
   let userOne: AsyncReturnType<typeof createApp>
-  let userTwo: AsyncReturnType<typeof createApp>
+  // let userTwo: AsyncReturnType<typeof createApp>
 
   const timeout = 18_000_000 // 5 hours
 
@@ -149,20 +149,22 @@ describe.only('send message - users are online', () => {
   beforeAll(async () => {
     owner = await createApp()
     userOne = await createApp()
-    userTwo = await createApp()
+    // userTwo = await createApp()
   })
 
   afterAll(async () => {
     await owner.manager.closeAllServices()
     await userOne.manager.closeAllServices()
-    await userTwo.manager.closeAllServices()
+    // await userTwo.manager.closeAllServices()
   })
 
   it('Owner creates community', async () => {
+    console.log(1)
     await createCommunity({ userName: 'Owner', store: owner.store })
   })
 
   it('Two users join community', async () => {
+    console.log(2)
     const ownerData = getCommunityOwnerData(owner.store)
 
     await joinCommunity({
@@ -172,50 +174,57 @@ describe.only('send message - users are online', () => {
       expectedPeersCount: 2
     })
 
-    await joinCommunity({
-      ...ownerData,
-      store: userTwo.store,
-      userName: 'username2',
-      expectedPeersCount: 3
-    })
+    // await joinCommunity({
+    //   ...ownerData,
+    //   store: userTwo.store,
+    //   userName: 'username2',
+    //   expectedPeersCount: 3
+    // })
 
     const infoMessages = getInfoMessages(owner.store, 'general')
     expectedMessages.concat(infoMessages)
   })
 
   it('Owner and users received certificates', async () => {
-    await assertReceivedCertificates('owner', 3, timeout, owner.store)
-    await assertReceivedCertificates('userOne', 3, timeout, userOne.store)
-    await assertReceivedCertificates('userTwo', 3, timeout, userTwo.store)
+    console.log(3)
+    await assertReceivedCertificates('owner', 2, timeout, owner.store)
+    await assertReceivedCertificates('userOne', 2, timeout, userOne.store)
+    // await assertReceivedCertificates('userTwo', 3, timeout, userTwo.store)
   })
 
   it('Users replicated channel and subscribed to it', async () => {
+    console.log(4)
     await assertReceivedChannelsAndSubscribe('owner', 1, timeout, owner.store)
     await assertReceivedChannelsAndSubscribe('userTwo', 1, timeout, userOne.store)
-    await assertReceivedChannelsAndSubscribe('userTwo', 1, timeout, userTwo.store)
+    // await assertReceivedChannelsAndSubscribe('userTwo', 1, timeout, userTwo.store)
   })
 
   it('each user sends one message to general channel', async () => {
+    console.log(5)
     expectedMessages.push(await sendMessage({ message: 'owner says hi', store: owner.store }))
     expectedMessages.push(await sendMessage({ message: 'userOne says hi', store: userOne.store }))
-    expectedMessages.push(await sendMessage({ message: 'userTwo says hi', store: userTwo.store }))
+    // expectedMessages.push(await sendMessage({ message: 'userTwo says hi', store: userTwo.store }))
   })
 
   it('Owner replicated all messages', async () => {
+    console.log(6)
     await assertReceivedMessages('owner', expectedMessages, timeout, owner.store)
   })
 
   it('userOne replicated all messages', async () => {
+    console.log(7)
     await assertReceivedMessages('userOne', expectedMessages, timeout, userOne.store)
   })
 
-  it('userTwo replicated all messages', async () => {
-    await assertReceivedMessages('userTwo', expectedMessages, timeout, userTwo.store)
-  })
+  // it('userTwo replicated all messages', async () => {
+  //   console.log(8)
+  //   await assertReceivedMessages('userTwo', expectedMessages, timeout, userTwo.store)
+  // })
 
   it('Replicated messages are valid', async () => {
+    console.log(9)
     await assertReceivedMessagesAreValid('owner', expectedMessages, timeout, owner.store)
     await assertReceivedMessagesAreValid('userOne', expectedMessages, timeout, userOne.store)
-    await assertReceivedMessagesAreValid('userTwo', expectedMessages, timeout, userTwo.store)
+    // await assertReceivedMessagesAreValid('userTwo', expectedMessages, timeout, userTwo.store)
   })
 })
