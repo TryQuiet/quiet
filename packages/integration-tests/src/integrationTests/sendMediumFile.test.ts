b51c15f7f64ac772ec52e9d623e12d9e9232524d
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-03-02 10:20:08 +0100

    medium 18,6mb

--- packages/integration-tests/src/integrationTests/sendMediumFile.test.ts
+++ packages/integration-tests/src/integrationTests/sendMediumFile.test.ts
@@ -23,8 +23,8 @@ describe('Medium File', () => {
   const timeout = 3_940_000
 
   const image: FileContent = {
-    path: `${path.resolve()}/assets/medium-file5.jpeg`,
-    name: 'medium-file5',
+    path: `${path.resolve()}/assets/medium-file2.jpeg`,
+    name: 'medium-file2',
     ext: '.jpeg'
   }
 
