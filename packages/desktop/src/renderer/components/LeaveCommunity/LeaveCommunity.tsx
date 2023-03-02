e79ef4dc23fc6c1b73fa4078bb0bf3ba84325096
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-03-02 14:04:32 +0100

    Reset default data dir

--- packages/desktop/src/renderer/components/LeaveCommunity/LeaveCommunity.tsx
+++ packages/desktop/src/renderer/components/LeaveCommunity/LeaveCommunity.tsx
@@ -1,14 +1,19 @@
-import { communities } from '@quiet/state-manager'
 import React from 'react'
 
 import { useSelector } from 'react-redux'
 
+import { communities } from '@quiet/state-manager'
+
 import { LeaveCommunityComponent } from './LeaveCommunityComponent'
 
-export const LeaveCommunity: React.FC = () => {
-  const leaveCommunity = () => {}
+import { clearCommunity } from '../..'
 
+export const LeaveCommunity: React.FC = () => {
   const community = useSelector(communities.selectors.currentCommunity)
+
+  const leaveCommunity = async () => {
+    await clearCommunity()
+  }
 
   return <LeaveCommunityComponent communityName={community.name} leaveCommunity={leaveCommunity} />
 }
