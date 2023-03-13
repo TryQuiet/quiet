12e0d146dbd659c31840589f3169bbfb8108b84c
Emi <emilia.marczyk@rumblefish.pl>
2023-03-13 15:07:42 +0100

    Add a grep fix also for linux

--- packages/backend/src/torManager/torManager.ts
+++ packages/backend/src/torManager/torManager.ts
@@ -156,8 +156,8 @@ export class Tor {
      *  Commands should output hanging tor pid
      */
     const byPlatform = {
-      android: `pgrep -af ${this.torDataDirectory} | grep -v pgrep | awk '{print $1}'`,
-      linux: `pgrep -af ${this.torDataDirectory} | grep -v pgrep | awk '{print $1}'`,
+      android: `pgrep -af "${this.torDataDirectory}" | grep -v pgrep | awk '{print $1}'`,
+      linux: `pgrep -af "${this.torDataDirectory}" | grep -v pgrep | awk '{print $1}'`,
       darwin: `ps -A | grep "${this.torDataDirectory}" | grep -v grep | awk '{print $1}'`,
       win32: `powershell "Get-WmiObject Win32_process -Filter {commandline LIKE '%${this.torDataDirectory.replace(/\\/g, '\\\\')}%' and name = 'tor.exe'} | Format-Table ProcessId -HideTableHeaders"`
     }
