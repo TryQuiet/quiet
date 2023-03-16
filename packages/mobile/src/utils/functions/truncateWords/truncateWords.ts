7028e3798b4e7678c403edcf74a6c92769c8d53f
Joseph Lacey <joseph@offkeydigital.com>
2023-03-16 14:17:55 -0700

    Fixing bug with truncateWords function to count words instead of characters.

--- packages/mobile/src/utils/functions/truncateWords/truncateWords.ts
+++ packages/mobile/src/utils/functions/truncateWords/truncateWords.ts
@@ -1,5 +1,5 @@
 export const truncateWords = (str: string, num: number): string => {
-  if (str.length > num) {
+  if (str.split(' ').length > num) {
     return str.split(' ').splice(0, num).join(' ') + '...'
   } else {
     return str
