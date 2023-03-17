9f7fda43da6b636a2e4c26397e2aed403e77814b
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-03-17 13:12:48 +0100

    remove deleay

--- packages/e2e-tests/newUser.crossplatform.test.ts
+++ packages/e2e-tests/newUser.crossplatform.test.ts
@@ -148,7 +148,6 @@ describe('New User', () => {
       console.log('Debug modal')
       const debugModal = new DebugModeModal(driver2)
       await debugModal.element.isDisplayed()
-      await new Promise<void>(resolve => setTimeout(() => resolve(), 15000))
       const button = await debugModal.button
       console.log('Debug modal title is displayed')
       await button.isDisplayed()
