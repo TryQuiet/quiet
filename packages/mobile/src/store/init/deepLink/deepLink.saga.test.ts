18a24d3e6c1a7ebc2d23bf8f0beb812757577471
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-03-23 17:53:14 +0100

    Correct test readability

--- packages/mobile/src/store/init/deepLink/deepLink.saga.test.ts
+++ packages/mobile/src/store/init/deepLink/deepLink.saga.test.ts
@@ -4,13 +4,33 @@ import { combineReducers } from '@reduxjs/toolkit'
 import { reducers } from '../../root.reducer'
 import { Store } from '../../store.types'
 import { prepareStore } from '../../../utils/tests/prepareStore'
-import { communities, CommunityOwnership } from '@quiet/state-manager'
+import { communities, Community, CommunityOwnership } from '@quiet/state-manager'
 import { initActions } from '../init.slice'
 import { navigationActions } from '../../navigation/navigation.slice'
 import { ScreenNames } from '../../../const/ScreenNames.enum'
 
 describe('deepLinkSaga', () => {
   let store: Store
+
+  const community: Community = {
+    id: 'community',
+    name: '',
+    CA: {
+      rootCertString: '',
+      rootKeyString: ''
+    },
+    rootCa: '',
+    peerList: [],
+    registrar: {
+      privateKey: '',
+      address: ''
+    },
+    registrarUrl: 'https://bidrmzr3ee6qa2vvrlcnqvvvsk2gmjktcqkunba326parszr44gibwyd.onion',
+    onionAddress: '',
+    privateKey: '',
+    port: 0,
+    registrationAttempts: 0
+  }
 
   beforeEach(() => {
     store = prepareStore().store
@@ -52,25 +72,7 @@ describe('deepLinkSaga', () => {
     )
 
     store.dispatch(
-      communities.actions.addNewCommunity({
-        id: 'community',
-        name: '',
-        CA: {
-          rootCertString: '',
-          rootKeyString: ''
-        },
-        rootCa: '',
-        peerList: [],
-        registrar: {
-          privateKey: '',
-          address: ''
-        },
-        registrarUrl: 'https://bidrmzr3ee6qa2vvrlcnqvvvsk2gmjktcqkunba326parszr44gibwyd.onion',
-        onionAddress: '',
-        privateKey: '',
-        port: 0,
-        registrationAttempts: 0
-      })
+      communities.actions.addNewCommunity(community)
     )
 
     store.dispatch(communities.actions.setCurrentCommunity('community'))
@@ -101,25 +103,7 @@ describe('deepLinkSaga', () => {
     )
 
     store.dispatch(
-      communities.actions.addNewCommunity({
-        id: 'community',
-        name: '',
-        CA: {
-          rootCertString: '',
-          rootKeyString: ''
-        },
-        rootCa: '',
-        peerList: [],
-        registrar: {
-          privateKey: '',
-          address: ''
-        },
-        registrarUrl: 'https://bidrmzr3ee6qa2vvrlcnqvvvsk2gmjktcqkunba326parszr44gibwyd.onion',
-        onionAddress: '',
-        privateKey: '',
-        port: 0,
-        registrationAttempts: 0
-      })
+      communities.actions.addNewCommunity(community)
     )
 
     store.dispatch(communities.actions.setCurrentCommunity('community'))
