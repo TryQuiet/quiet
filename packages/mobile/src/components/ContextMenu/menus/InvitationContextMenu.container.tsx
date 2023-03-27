d95e1e109bbcc4a4da15b9342d3fa8dceff6e7bc
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2023-03-27 16:37:22 +0200

    Feature/share invite link mobile (#1367)
    * Add invitation context menu
    
    * Add hints to context menus
    
    * Add confirmation box
    
    * Attach copy link action
    
    * Attach share link action
    
    * Increase confirmation box bottom margin
    
    * Fix snpashot

---
+++ packages/mobile/src/components/ContextMenu/menus/InvitationContextMenu.container.tsx
@@ -0,0 +1,85 @@
+import React, { FC, useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import Clipboard from '@react-native-clipboard/clipboard'
+
+import { communities } from '@quiet/state-manager'
+
+import { navigationSelectors } from '../../../store/navigation/navigation.selectors'
+
+import { useConfirmationBox } from '../../../hooks/useConfirmationBox'
+import { useContextMenu } from '../../../hooks/useContextMenu'
+import { MenuName } from '../../../const/MenuNames.enum'
+import { ContextMenu } from '../ContextMenu.component'
+import { ContextMenuItemProps } from '../ContextMenu.types'
+
+import { navigationActions } from '../../../store/navigation/navigation.slice'
+import { ScreenNames } from '../../../const/ScreenNames.enum'
+import { Share } from 'react-native'
+
+export const InvitationContextMenu: FC = () => {
+  const dispatch = useDispatch()
+
+  const screen = useSelector(navigationSelectors.currentScreen)
+
+  const community = useSelector(communities.selectors.currentCommunity)
+
+  const invitationContextMenu = useContextMenu(MenuName.Invitation)
+
+  const redirect = useCallback(
+    (screen: ScreenNames) => {
+      dispatch(
+        navigationActions.navigation({
+          screen: screen
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const copyLink = async () => {
+    Clipboard.setString(`quiet://?code=${community?.registrarUrl}`)
+    await confirmationBox.flash()
+  }
+
+  const shareLink = async () => {
+    try {
+      await Share.share({
+        message: `quiet://?code=${community?.registrarUrl}`
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const confirmationBox = useConfirmationBox('Link copied')
+
+  const items: ContextMenuItemProps[] = [
+    {
+      title: 'Copy link',
+      action: copyLink
+    },
+    {
+      title: 'Share',
+      action: shareLink
+    },
+    { title: 'Cancel', action: () => invitationContextMenu.handleClose() }
+  ]
+
+  useEffect(() => {
+    invitationContextMenu.handleClose()
+  }, [screen])
+
+  return (
+    <ContextMenu
+      title={'Add members'}
+      items={items}
+      hint={
+        'Anyone with Quiet app can follow this link to join this community. Only share with people you trust.'
+      }
+      link={`quiet://?code=${community?.registrarUrl}`}
+      linkAction={copyLink}
+      {...invitationContextMenu}
+    />
+  )
+}
