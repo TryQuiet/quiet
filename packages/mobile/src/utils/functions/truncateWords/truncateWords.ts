916e8426042a2f78690b8ce4a8f8dd7525f02712
Joseph Lacey <joseph@offkeydigital.com>
2023-04-02 13:51:11 -0700

    Adding a secondary message length check for truncation

--- packages/mobile/src/utils/functions/truncateWords/truncateWords.ts
+++ packages/mobile/src/utils/functions/truncateWords/truncateWords.ts
@@ -1,6 +1,8 @@
 export const truncateWords = (str: string, num: number): string => {
   if (str.split(' ').length > num) {
     return str.split(' ').splice(0, num).join(' ') + '...'
+  } else if (str.length > 100) {
+    return str.slice(0, 100) + '...'
   } else {
     return str
   }
