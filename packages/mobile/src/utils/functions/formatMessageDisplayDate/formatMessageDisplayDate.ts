8f8c50daa8d10364d9668fd2cf7530aa035c5e88
Joseph Lacey <joseph@offkeydigital.com>
2023-04-07 03:25:32 -0400

    Changes to mobile ChannelList date and time formatting (#1353)
    * Changes to mobile ChannelList date and time formatting
    
    * Switching mobile ChannelList time to follow locale
    
    * Adding test for formatMessageDisplayDate. Includes original function refactor.
    
    * Rewriting test to use relative datetimes. Removing source function refactor in previous commit.

--- packages/mobile/src/utils/functions/formatMessageDisplayDate/formatMessageDisplayDate.ts
+++ packages/mobile/src/utils/functions/formatMessageDisplayDate/formatMessageDisplayDate.ts
@@ -1,11 +1,21 @@
 import { DateTime } from 'luxon'
 
 export const formatMessageDisplayDate = (createdAt: number): string => {
-  const now = DateTime.now().toSeconds()
-  const diff = now - createdAt
-  if (diff > 86400) {
-    return DateTime.fromSeconds(createdAt).toFormat('LLL dd, t')
-  } else {
-    return DateTime.fromSeconds(createdAt).toFormat('t')
+  const messageDate = new Date(createdAt * 1000)
+  const now = new Date()
+  // Check if message was sent within the same year and month.
+  if (
+    messageDate.getFullYear() === now.getFullYear() &&
+    messageDate.getMonth() === now.getMonth()
+  ) {
+    // Check if message was sent today.
+    if (messageDate.getDay() === now.getDay()) {
+      return DateTime.fromSeconds(createdAt).toLocaleString(DateTime.TIME_SIMPLE)
+    }
+    // Check if message was sent yesterday
+    if (messageDate.getDay() + 1 === now.getDay()) {
+      return 'Yesterday'
+    }
   }
+  return DateTime.fromSeconds(createdAt).toLocaleString()
 }
