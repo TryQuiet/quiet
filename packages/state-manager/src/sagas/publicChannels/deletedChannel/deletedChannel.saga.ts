230ce72b0b9ae0955d067800a698d2c3b721b0ce
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-04-20 11:06:30 +0200

    basic handling of removing general channel

--- packages/state-manager/src/sagas/publicChannels/deletedChannel/deletedChannel.saga.ts
+++ packages/state-manager/src/sagas/publicChannels/deletedChannel/deletedChannel.saga.ts
@@ -1,10 +1,7 @@
 import { publicChannelsActions } from '../publicChannels.slice'
 import { PayloadAction } from '@reduxjs/toolkit'
-import { SocketActionTypes } from '../../socket/const/actionTypes'
-
 import logger from '../../../utils/logger'
-import { select, put } from 'typed-redux-saga'
-import { publicChannelsSelectors } from '../publicChannels.selectors'
+import { put } from 'typed-redux-saga'
 import { messagesActions } from '../../messages/messages.slice'
 
 const log = logger('publicChannels')
@@ -13,22 +10,21 @@ export function* deletedChannelSaga(
   action: PayloadAction<ReturnType<typeof publicChannelsActions.deletedChannel>['payload']>
 ): Generator {
   log(`Deleted channel ${action.payload.channel} saga`)
+
   const channelAddress = action.payload.channel
+  const isGeneral = channelAddress === 'general'
 
-  // Set channel to general
-  yield* put(publicChannelsActions.setCurrentChannel({ channelAddress: 'general' }))
+  if (!isGeneral) {
+    yield* put(publicChannelsActions.setCurrentChannel({ channelAddress: 'general' }))
+  }
 
-  // Clear messages cache
   yield* put(publicChannelsActions.clearMessagesCache({ channelAddress }))
 
-  // Delete messages
   yield* put(messagesActions.deleteMessages({ channelAddress }))
 
-  // Delete channel
   yield* put(publicChannelsActions.deleteChannelFromStore({ channelAddress }))
 
-  // check
-  const locallyStoredChannels2 = yield* select(publicChannelsSelectors.publicChannels)
-
-  console.log({ locallyStoredChannels2 })
+  if (isGeneral) {
+    yield* put(publicChannelsActions.createGeneralChannel())
+  }
 }
