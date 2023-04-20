f395208938491627427e8dd818bc10109af7e7c2
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-04-20 13:54:58 +0200

    basic notification about channel deletion

--- packages/state-manager/src/sagas/publicChannels/deletedChannel/deletedChannel.saga.ts
+++ packages/state-manager/src/sagas/publicChannels/deletedChannel/deletedChannel.saga.ts
@@ -3,6 +3,7 @@ import { PayloadAction } from '@reduxjs/toolkit'
 import logger from '../../../utils/logger'
 import { put, delay } from 'typed-redux-saga'
 import { messagesActions } from '../../messages/messages.slice'
+import { MessageType, WriteMessagePayload } from '../../messages/messages.types'
 
 const log = logger('publicChannels')
 
@@ -26,9 +27,23 @@ export function* deletedChannelSaga(
 
   yield* put(publicChannelsActions.deleteChannelFromStore({ channelAddress }))
 
+  let message: string
+
   if (isGeneral) {
     yield* put(publicChannelsActions.createGeneralChannel())
+    // For better UX
     yield* delay(500)
     yield* put(publicChannelsActions.finishGeneralRecreation())
+    message = '#general has been recreated by owner'
+  } else {
+    message = `#${channelAddress} has been deleted by owner`
   }
+
+  const payload: WriteMessagePayload = {
+    type: MessageType.Info,
+    message,
+    channelAddress: 'general'
+  }
+
+  yield* put(messagesActions.sendMessage(payload))
 }
