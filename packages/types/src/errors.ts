40c7fa4227530e45c3d38bf83c476be70af90c89
Emi <emilia.marczyk@rumblefish.pl>
2023-04-27 14:59:43 +0200

    Cleanup

--- packages/types/src/errors.ts
+++ packages/types/src/errors.ts
@@ -1,8 +1,1 @@
-// export class NoCryptoEngineError extends Error {
-//   constructor(message?: string) {
-//     super(message || 'No crypto engine!')
-//     this.name = "NoCryptoEngine"
-//   }
-// }
-
 export class NoCryptoEngineError extends Error {}
