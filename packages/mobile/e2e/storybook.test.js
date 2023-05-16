a483b256cea057f9a4476b13ce99ca2bfbe3c063
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-05-16 17:27:14 +0200

    Use storybook for visual regression tests

---
+++ packages/mobile/e2e/storybook.test.js
@@ -0,0 +1,115 @@
+import fs, { statSync } from 'fs'
+import { readdir } from 'fs/promises'
+import path from 'path'
+import compare from './utils/compare'
+
+jest.setTimeout(9000000)
+
+/* eslint-disable no-undef */
+describe('Storybook', () => {
+  let stories = []
+
+  const findStories = async (dirname, files) => {
+    const items = await readdir(dirname)
+
+    files = files || []
+
+    for (const item of items) {
+      if (statSync(`${dirname}/${item}`).isDirectory()) {
+        files = await findStories(`${dirname}/${item}` + '/', files)
+      } else {
+        if (item.endsWith('.stories.tsx')) {
+          const scenarios = await listScenarios(dirname, item)
+          const component = item.split('.')[0]
+          files.push([dirname, item, component, scenarios])
+        }
+      }
+    }
+
+    return files
+  }
+
+  const trimStories = () => {
+    const start = process.argv.filter(x => x.startsWith('-starting-story'))[0].split('=')[1]
+
+    let index
+
+    for (const story of stories) {
+      if (story.includes(start)) {
+        index = stories.indexOf(story)
+      }
+    }
+
+    stories = stories.slice(index, stories.length - 1)
+  }
+
+  const listScenarios = async (dirname, item) => {
+    const data = fs.readFileSync(`${dirname}${item}`, 'utf8')
+
+    const regexp = /\.add\(\'([^)]+)\', \(\)/g
+
+    const scenarios = []
+
+    let matches = []
+    while ((matches = regexp.exec(data)) != null) {
+      scenarios.push(matches[1])
+    }
+
+    return scenarios
+  }
+
+  const checkVisualRegression = async (id, component, scenario) => {
+    const imagePath = await element(by.id(id)).takeScreenshot(`${component}_${scenario}`)
+    compare(
+      imagePath,
+      `${__dirname}/storybook-base-screenshots/${device.name}/${component}_${scenario}.png`
+    )
+  }
+
+  beforeAll(async () => {
+    const dirname = path.resolve('src/components/')
+    stories = await findStories(dirname, [])
+
+    // Start at particular story
+    if (process.argv.filter(x => x.startsWith('-starting-story'))[0]) trimStories()
+
+    await device.launchApp({ newInstance: true, launchArgs: { detoxDebugVisibility: 'YES' } })
+
+    await element(by.id('BottomMenu.Sidebar')).longPress()
+  })
+
+  test('visual regressions', async () => {
+    for (const story of stories) {
+      const component = story[2]
+      const scenarios = story[3]
+
+      console.log(`Performing visual regression test for ${component}`)
+
+      await waitFor(element(by.id('Storybook.ListView.SearchBar')))
+        .toBeVisible()
+        .withTimeout(5000)
+
+      // Use Storybook's search section
+      await element(by.id('Storybook.ListView.SearchBar')).longPress()
+      await element(by.id('Storybook.ListView.SearchBar')).clearText()
+      await element(by.id('Storybook.ListView.SearchBar')).typeText(component)
+
+      // Hide keyboard
+      await device.pressBack()
+
+      for (const scenario of scenarios) {
+        console.log(`----checking ${scenario}`)
+
+        await element(by.text(scenario)).atIndex(0).longPress()
+        await element(by.text(scenario)).atIndex(0).longPress() // Idle (important though)
+
+        await element(by.id('BottomMenu.Canvas')).longPress()
+
+        const id = `${component.toLowerCase()}--${scenario.toLowerCase()}`
+        await checkVisualRegression(id, component, scenario)
+
+        await element(by.id('BottomMenu.Sidebar')).longPress()
+      }
+    }
+  })
+})
--- packages/mobile/src/components/ConfirmationBox/ConfirmationBox.stories.tsx
+++ packages/mobile/src/components/ConfirmationBox/ConfirmationBox.stories.tsx
@@ -4,6 +4,6 @@ import { storiesOf } from '@storybook/react-native'
 
 import { ConfirmationBox } from './ConfirmationBox.component'
 
-storiesOf('ConfirmationBox', module).add('Link copied', () => {
+storiesOf('ConfirmationBox', module).add('LinkCopied', () => {
   return <ConfirmationBox title={'Link copied'} toggle />
 })
