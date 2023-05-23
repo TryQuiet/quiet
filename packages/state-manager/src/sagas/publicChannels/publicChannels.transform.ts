f8c5fbb13f415d0778de9dff1bd3e238d16f91a7
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-05-23 11:07:01 +0200

    state channels transform

--- packages/state-manager/src/sagas/publicChannels/publicChannels.transform.ts
+++ packages/state-manager/src/sagas/publicChannels/publicChannels.transform.ts
@@ -1,3 +1,5 @@
+import { ChannelMessage, PublicChannelStatus, PublicChannelStorage } from '@quiet/types'
+import { Dictionary, EntityState } from '@reduxjs/toolkit'
 import { createTransform } from 'redux-persist'
 import { StoreKeys } from '../store.keys'
 import { publicChannelsAdapter, publicChannelsSubscriptionsAdapter } from './publicChannels.adapter'
@@ -8,12 +10,21 @@ export const PublicChannelsTransform = createTransform(
     return { ...inboundState }
   },
   (outboundState: PublicChannelsState, _key: any) => {
-    const selectors = publicChannelsAdapter.getSelectors()
-    const publicChannelStorage = selectors.selectAll(outboundState.channels)
-    const generalChannel = publicChannelStorage.find(channel => channel.name === 'general')
-    const generalChannelId = generalChannel?.id || ''
+    const generalChannelId = getGeneralChannelId(outboundState)
+
+    const transformedOutboundState = { ...outboundState } as any
+    if (transformedOutboundState.currentChannelAddress) {
+      delete transformedOutboundState.currentChannelAddress
+    }
+
+    outboundState.channels.entities = transformChannelsEntities(outboundState.channels.entities)
+
+    outboundState.channelsStatus.entities = transformChannelStatusEntities(
+      outboundState.channelsStatus.entities
+    )
+
     return {
-      ...outboundState,
+      ...transformedOutboundState,
       currentChannelId: generalChannelId,
       channels: outboundState.channels,
       channelsStatus: outboundState.channelsStatus,
@@ -22,3 +33,77 @@ export const PublicChannelsTransform = createTransform(
   },
   { whitelist: [StoreKeys.PublicChannels] }
 )
+
+const getGeneralChannelId = (state: PublicChannelsState) => {
+  const selectors = publicChannelsAdapter.getSelectors()
+  const publicChannelStorage = selectors.selectAll(state.channels)
+  const generalChannel = publicChannelStorage.find(channel => channel.name === 'general')
+  const generalChannelId = generalChannel?.id || 'general'
+
+  return generalChannelId
+}
+
+const transformChannelsEntities = (channelsEntities: Dictionary<PublicChannelStorage>) => {
+  const messagesRefactor = (messages: EntityState<ChannelMessage>) => {
+    const transformedMessagesEntities = messages.entities
+    for (const [key, _message] of Object.entries(transformedMessagesEntities)) {
+      const message = { ..._message } as any
+      if (message.channelAddress) {
+        const transformedMessage = {
+          ...message,
+          channelId: message.channelAddress
+        }
+        delete transformedMessage.channelAddress
+
+        transformedMessagesEntities[key] = transformedMessage
+      }
+    }
+
+    return transformedMessagesEntities
+  }
+
+  for (const [key, _channel] of Object.entries(channelsEntities)) {
+    const channel = { ..._channel } as any
+    if (channel.address) {
+      const messages = messagesRefactor(channel.messages)
+      const transformedChannel = {
+        ...channel,
+        messages,
+        id: channel.address
+      }
+      delete transformedChannel.address
+
+      channelsEntities[key] = transformedChannel
+    }
+  }
+  return channelsEntities
+}
+
+const transformChannelStatusEntities = (
+  channelsStatusEntities: Dictionary<PublicChannelStatus>
+) => {
+  const transformedChannelsStatusEntities = { ...channelsStatusEntities }
+  for (const [key, _channel] of Object.entries(transformedChannelsStatusEntities)) {
+    const channel = { ..._channel } as any
+    if (channel.address) {
+      let transformedNewestMessage = { ...channel.newestMessage }
+      if (transformedNewestMessage.channelAddress) {
+        transformedNewestMessage = {
+          channelId: channel.address,
+          ...transformedNewestMessage
+        }
+        delete transformedNewestMessage.channelAddress
+      }
+      const transformedChannel = {
+        ...channel,
+        id: channel.address,
+        newestMessage: transformedNewestMessage
+      }
+      delete transformedChannel.address
+
+      transformedChannelsStatusEntities[key] = transformedChannel
+    }
+  }
+
+  return transformedChannelsStatusEntities
+}
