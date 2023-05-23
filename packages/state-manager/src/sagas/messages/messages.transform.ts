cb4e5a7b2125679255b8eb052ed7ca2a2ffa6c86
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-05-23 12:23:25 +0200

    messages state transform

--- packages/state-manager/src/sagas/messages/messages.transform.ts
+++ packages/state-manager/src/sagas/messages/messages.transform.ts
@@ -1,3 +1,5 @@
+import { PublicChannelsMessagesBase, ChannelMessage } from '@quiet/types'
+import { Dictionary, EntityState } from '@reduxjs/toolkit'
 import { createTransform } from 'redux-persist'
 import { StoreKeys } from '../store.keys'
 import { messageSendingStatusAdapter } from './messages.adapter.ts'
@@ -8,11 +10,54 @@ export const MessagesTransform = createTransform(
     return { ...inboundState }
   },
   (outboundState: MessagesState, _key: any) => {
+    outboundState.publicChannelsMessagesBase.entities = transformPublicChannelsMessagesBaseEntities(
+      outboundState.publicChannelsMessagesBase.entities
+    )
+
     return {
       ...outboundState,
       publicKeyMapping: {},
-      messageSendingStatus: messageSendingStatusAdapter.getInitialState(),
+      publicChannelsMessagesBase: outboundState.publicChannelsMessagesBase,
+      messageSendingStatus: messageSendingStatusAdapter.getInitialState()
     }
   },
   { whitelist: [StoreKeys.Messages] }
 )
+
+const transformPublicChannelsMessagesBaseEntities = (
+  messagesBaseEntities: Dictionary<PublicChannelsMessagesBase>
+) => {
+  const messagesRefactor = (messages: EntityState<ChannelMessage>) => {
+    const transformedMessagesEntities = messages.entities
+    for (const [key, _message] of Object.entries(transformedMessagesEntities)) {
+      const message = { ..._message } as any
+      if (message.channelAddress) {
+        const transformedMessage = {
+          ...message,
+          channelId: message.channelAddress
+        }
+        delete transformedMessage.channelAddress
+
+        transformedMessagesEntities[key] = transformedMessage
+      }
+    }
+
+    return transformedMessagesEntities
+  }
+
+  for (const [key, _message] of Object.entries(messagesBaseEntities)) {
+    const message = { ..._message } as any
+    if (message.channelAddress) {
+        const messages = messagesRefactor(message.messages)
+        const transformedMessage = {
+            ...message,
+            messages,
+            channelId: message.channelAddress
+        }
+        delete transformedMessage.channelAddress
+
+        messagesBaseEntities[key] = transformedMessage
+    }
+  }
+  return messagesBaseEntities
+}
