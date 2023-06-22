89815c9c00b3e41af1c2feb81cb64f5657463d99
siepra <wiktor.siepra@gmail.com>
2023-06-22 17:00:43 +0200

    Secure missing base screenshots in vr tests

--- packages/mobile/e2e/utils/checkVisualRegression.js
+++ packages/mobile/e2e/utils/checkVisualRegression.js
@@ -1,7 +1,8 @@
 import compare from './compare'
 
+const fs = require('fs')
+
 const enableVisualRegression = Boolean(process.argv.filter(x => x.startsWith('-enable-visual-regression'))[0])
-const updateBase = Boolean(process.argv.find(x => x.startsWith('-update-base')))
 
 const ci = Boolean(process.argv.filter(x => x.startsWith('--ci'))[0])
 const environment = ci ? 'ci' : 'local'
@@ -14,7 +15,10 @@ const checkVisualRegression = async componentName => {
 
   let basePath = `${__dirname}/visual-regressions/${environment}/${platform}/starter-base-screenshots/${componentName}.png`
 
-  if (updateBase) return
+  if (!fs.existsSync(basePath)) {
+    console.error(`SKIP: There's no base screenshot for ${componentName} in current test scenario.`)
+    return
+  }
 
   await compare(imagePath, basePath)
 }
