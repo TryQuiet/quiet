8a56ae59f07f27130fa51f25fae4a95cc640115d
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-06-28 13:51:25 +0200

    Fix base updating util

--- packages/mobile/e2e/utils/baseScreenshotsUpdate.js
+++ packages/mobile/e2e/utils/baseScreenshotsUpdate.js
@@ -1,7 +1,7 @@
 import childProcess from 'child_process'
 import info from './info'
 
-const { environment, os, test, baseUpdate } = info
+const { environment, os, ios, test, baseUpdate } = info
 
 const baseScreenshotsUpdate = async () => {
   await new Promise((resolve, reject) => {
@@ -9,16 +9,23 @@ const baseScreenshotsUpdate = async () => {
       resolve('Not updating base screenshots.')
       return
     }
-    childProcess.exec(`sh ./e2e/scripts/base-screenshots-update.sh ${environment} ${os} ${test}`, (err, stdout) => {
-      if (err) {
-        console.error(err)
-        reject(`Error while updating base screenshots. ${err}`)
+
+    let shell = 'bash'
+    if (ios) shell = 'sh'
+
+    childProcess.exec(
+      `${shell} ./e2e/scripts/base-screenshots-update.sh ${environment} ${os} ${test}`,
+      (err, stdout) => {
+        if (err) {
+          console.error(err)
+          reject(`Error while updating base screenshots. ${err}`)
+          return
+        }
+        console.log(stdout)
+        resolve('Successfuly updated base screenshots.')
         return
       }
-      console.log(stdout)
-      resolve('Successfuly updated base screenshots.')
-      return
-    })
+    )
   })
 }
 
