51e3b093c100093e152898066cb0e8ce942653e4
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-06-29 10:46:16 +0200

    storage test

--- packages/backend/src/nest/common/utils.ts
+++ packages/backend/src/nest/common/utils.ts
@@ -15,7 +15,7 @@ export interface Ports {
 }
 
 export function createPaths(paths: string[]) {
-  console.log('creating paths in fn')
+  console.log('creating paths in fn - utils')
   for (const path of paths) {
     if (!fs.existsSync(path)) {
       fs.mkdirSync(path, { recursive: true })
