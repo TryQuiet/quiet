05747d31ed333aeb7866ba2639e52036ce130bf1
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-06-29 14:27:27 +0200

    more libp2p tests

--- packages/backend/src/nest/libp2p/libp2p.service.spec.ts
+++ packages/backend/src/nest/libp2p/libp2p.service.spec.ts
@@ -3,10 +3,12 @@ import { TestModule } from '../common/test.module'
 import { libp2pInstanceParams } from '../common/test.utils'
 import { Libp2pModule } from './libp2p.module'
 import { Libp2pService } from './libp2p.service'
+import { Libp2pNodeParams } from './libp2p.types'
 
 describe('Libp2pService', () => {
   let module: TestingModule
   let libp2pService: Libp2pService
+  let params: Libp2pNodeParams
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
@@ -14,84 +16,27 @@ describe('Libp2pService', () => {
     }).compile()
 
     libp2pService = await module.resolve(Libp2pService)
+    params = await libp2pInstanceParams()
   })
 
   afterAll(async () => {
+    await libp2pService.libp2pInstance?.stop()
     await module.close()
   })
 
-  it('should be defined', async () => {
-    const params = await libp2pInstanceParams()
+  it('inits libp2p', async () => {
     const libp2pInstance = await libp2pService.createInstance(params)
-
-    expect(libp2pService).toBeDefined()
+    expect(libp2pInstance).not.toBeNull()
+    expect(libp2pInstance.peerId).toBe(params.peerId)
   })
-  // it('inits libp2p', async () => {
-  //   const peerId = await createPeerId()
-  //   const port = 1234
-  //   const address = '0.0.0.0'
 
-  //   connectionsManager = new ConnectionsManager({
-  //     socketIOPort: port,
-  //     options: {
-  //       env: {
-  //         appDataPath: tmpAppDataPath,
-  //       },
-  //     },
-  //   })
+  it('creates libp2p address with proper ws type (%s)', async () => {
+    const libp2pAddress = libp2pService.createLibp2pAddress(params.localAddress, params.peerId.toString())
+    expect(libp2pAddress).toStrictEqual(`/dns4/${params.localAddress}/tcp/443/wss/p2p/${params.peerId.toString()}`)
+  })
 
-  //   connectionsManager.dataServer = new DataServer(port)
-  //   connectionsManager.io = connectionsManager.dataServer.io
-
-  //   const localAddress = connectionsManager.createLibp2pAddress(address, peerId.toString())
-  //   const remoteAddress = connectionsManager.createLibp2pAddress(address, (await createPeerId()).toString())
-  //   const result = await connectionsManager.initLibp2p({
-  //     peerId,
-  //     address,
-  //     addressPort: port,
-  //     targetPort: port,
-  //     bootstrapMultiaddrs: [remoteAddress],
-
-  //     certs: {
-  //       // @ts-expect-error
-  //       certificate: userIdentity.userCertificate,
-  //       // @ts-expect-error
-  //       key: userIdentity.userCsr.userKey,
-  //       CA: [communityRootCa],
-  //     },
-  //   })
-  //   expect(result.localAddress).toBe(localAddress)
-  //   expect(result.libp2p.peerId).toBe(peerId)
-  // })
-
-  // it('creates libp2p address with proper ws type (%s)', async () => {
-  //   const address = '0.0.0.0'
-  //   const port = 1234
-  //   const peerId = await PeerId.create()
-  //   connectionsManager = new ConnectionsManager({
-  //     socketIOPort: port,
-  //     options: {
-  //       env: {
-  //         appDataPath: tmpAppDataPath,
-  //       },
-  //     },
-  //   })
-  //   const libp2pAddress = connectionsManager.createLibp2pAddress(address, peerId.toB58String())
-  //   expect(libp2pAddress).toStrictEqual(`/dns4/${address}/tcp/443/wss/p2p/${peerId.toB58String()}`)
-  // })
-
-  // it('creates libp2p listen address', async () => {
-  //   const address = '0.0.0.0'
-  //   const port = 1234
-  //   connectionsManager = new ConnectionsManager({
-  //     socketIOPort: port,
-  //     options: {
-  //       env: {
-  //         appDataPath: tmpAppDataPath,
-  //       },
-  //     },
-  //   })
-  //   const libp2pListenAddress = connectionsManager.createLibp2pListenAddress(address)
-  //   expect(libp2pListenAddress).toStrictEqual(`/dns4/${address}/tcp/443/wss`)
-  // })
+  it('creates libp2p listen address', async () => {
+    const libp2pListenAddress = libp2pService.createLibp2pListenAddress(params.listenAddresses[0])
+    expect(libp2pListenAddress).toStrictEqual(`/dns4/${params.listenAddresses}/tcp/443/wss`)
+  })
 })
