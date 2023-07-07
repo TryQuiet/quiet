b1b88ff5d723c3d155f42774fa4668737b661c81
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-07-07 10:05:30 +0200

    save state

--- packages/e2e-tests/src/tests/backwardsCompatibility.test.ts
+++ packages/e2e-tests/src/tests/backwardsCompatibility.test.ts
@@ -136,7 +136,7 @@ describe('Backwards Compatibility', () => {
       messagesToCompare = await secondChannel.getUserMessages(ownerUsername)
     })
     it('Close old app', async () => {
-      await ownerAppOldVersion.close()
+      await ownerAppOldVersion.close({ forceSaveState: true })
       await new Promise<void>(resolve => setTimeout(() => resolve(), 5000))
     })
 
