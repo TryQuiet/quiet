c5f65ede28518dff339da46b19c19e5a8e48388f
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-08-23 16:17:28 +0200

    test: restore broken tests for capitalizeFirstLetter

--- packages/common/src/capitalize.ts
+++ packages/common/src/capitalize.ts
@@ -2,6 +2,6 @@ export const capitalizeFirstLetter = (text: string): string => {
   if (text) {
     return text.charAt(0).toUpperCase() + text.slice(1)
   } else {
-    return null
+    return ''
   }
 }
