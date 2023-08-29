ee55f9235295209cea861c5dc7a66a5cf5a186ae
Vin Kabuki <vincent.kabacky@gmail.com>
2023-08-29 10:23:36 +0200

    Update own certificate on replication

--- packages/state-manager/src/sagas/identity/identity.master.saga.ts
+++ packages/state-manager/src/sagas/identity/identity.master.saga.ts
@@ -7,6 +7,8 @@ import { registerUsernameSaga } from './registerUsername/registerUsername.saga'
 import { verifyJoinTimestampSaga } from './verifyJoinTimestamp/verifyJoinTimestamp.saga'
 import { saveUserCsrSaga } from './saveUserCsr/saveUserCsr.saga'
 import { savedOwnerCertificateSaga } from './savedOwnerCertificate/savedOwnerCertificate.saga'
+import { usersActions } from '../users/users.slice'
+import { updateCertificateSaga } from './updateCertificate/updateCertificate.saga'
 
 export function* identityMasterSaga(socket: Socket): Generator {
   yield all([
@@ -16,5 +18,6 @@ export function* identityMasterSaga(socket: Socket): Generator {
     takeEvery(identityActions.savedOwnerCertificate.type, savedOwnerCertificateSaga, socket),
     takeEvery(identityActions.verifyJoinTimestamp.type, verifyJoinTimestampSaga),
     takeEvery(identityActions.saveUserCsr.type, saveUserCsrSaga, socket),
+    takeEvery(usersActions.responseSendCertificates.type, updateCertificateSaga),
   ])
 }
