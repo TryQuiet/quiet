ebfa5e648e757f09530a905b053a73cb9c63c429
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2023-09-14 16:45:26 +0200

    Use safe area insets as offset value (#1804)
    * Use safe area insets as offset value
    
    * Update README
    
    * Fix lint
    
    * Jest mock useSafeAreaInsets

--- packages/mobile/src/setupTests.tsx
+++ packages/mobile/src/setupTests.tsx
@@ -62,6 +62,14 @@ jest.mock('socket.io-client', () => ({
 // could not be found. Verify that a module by this name is registered in the native binary."
 jest.mock('react-native-document-picker', () => { })
 
+jest.mock('react-native-safe-area-context', () => ({
+  useSafeAreaInsets: () => {
+    return {
+      bottom: 0
+    }
+  }
+}))
+
 export const ioMock = io as jest.Mock
 
 jest.resetAllMocks()
