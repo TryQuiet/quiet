f601ccdb5f863f1d697b3cca629c61e1cbaa2965
Wiktor <wiktor.sieprawski@rumblefish.pl>
2023-09-15 10:43:00 +0200

    Remove jest snapshotResolver

--- packages/mobile/jest/snapshotResolver.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testPathForConsistencyCheck: 'some/example.test.js',
-
-  resolveSnapshotPath: (testPath, snapshotExtension) =>
-    testPath.replace(/\.test\.([tj]sx?)/, `.test.$1${snapshotExtension}`),
-
-  resolveTestPath: (snapshotFilePath, snapshotExtension) => snapshotFilePath.replace(snapshotExtension, ''),
-}
