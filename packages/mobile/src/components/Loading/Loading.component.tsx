063c20e642848ec1dce9a90af0553adf9aba5187
siepra <wiktor.siepra@gmail.com>
2023-09-22 13:45:31 +0200

    Prettify chat loading view

--- packages/mobile/src/components/Loading/Loading.component.tsx
+++ packages/mobile/src/components/Loading/Loading.component.tsx
@@ -1,44 +1,36 @@
 import React, { FC } from 'react'
-import { Image, View } from 'react-native'
-import deviceInfoModule from 'react-native-device-info'
+import { View } from 'react-native'
 import { Typography } from '../Typography/Typography.component'
-import { defaultTheme } from '../../styles/themes/default.theme'
-import { appImages } from '../../assets'
 
-export const Loading: FC = () => {
+export interface LoadingProps {
+  title: string
+  caption?: string
+  progress?: number
+}
+
+export const Loading: FC<LoadingProps> = ({ title, caption, progress = 220 }) => {
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: defaultTheme.palette.background.white,
+        alignItems: 'center',
       }}
-      testID='loading'
     >
-      <Image
-        source={appImages.quiet_icon}
-        style={{
-          marginTop: 20,
-          marginBottom: 46,
-          resizeMode: 'cover',
-          width: 84,
-          height: 84,
-          borderRadius: 16,
-        }}
-      />
-      <View style={{ gap: 6, alignItems: 'center' }}>
-        <Typography fontSize={14} fontWeight={'medium'}>
-          Starting backend
-        </Typography>
-        <Typography fontSize={12} color={'gray50'}>
-          This can take some time
-        </Typography>
+      <View style={{ width: 300, height: 4, backgroundColor: '#F0F0F0', borderRadius: 4 }}>
+        <View style={{ backgroundColor: '#67BFD3', height: 4, width: progress, borderRadius: 4 }}></View>
       </View>
-      <View style={{ margin: 20 }}>
-        <Typography fontSize={12} color={'grayDark'}>
-          {`v ${deviceInfoModule.getVersion()}`}
+      <View style={{ flexDirection: 'column', gap: 8 }}>
+        <Typography fontSize={14} style={{ lineHeight: 20, textAlign: 'center', marginTop: 8 }}>
+          {title}
         </Typography>
+        {caption && (
+          <View style={{ width: 250 }}>
+            <Typography fontSize={12} color={'gray50'} horizontalTextAlign={'center'}>
+              {caption}
+            </Typography>
+          </View>
+        )}
       </View>
     </View>
   )
