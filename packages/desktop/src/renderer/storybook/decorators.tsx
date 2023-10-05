75ffec77f4bf0c9a97a18322538df8c359f9bccb
Vin Kabuki <vincent.kabacky@gmail.com>
2023-10-05 12:41:23 +0200

    Run linter

--- packages/desktop/src/renderer/storybook/decorators.tsx
+++ packages/desktop/src/renderer/storybook/decorators.tsx
@@ -5,12 +5,11 @@ import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
 import theme from '../theme'
 import { Store } from '../sagas/store.types'
 
-export const withStore = (store: Store) => (Story: React.FC) =>
-  (
-    <Provider store={store}>
-      <Story />
-    </Provider>
-  )
+export const withStore = (store: Store) => (Story: React.FC) => (
+  <Provider store={store}>
+    <Story />
+  </Provider>
+)
 
 export const withTheme = (Story: React.FC) => (
   <StyledEngineProvider injectFirst>
