3d5a211e234ca85ae6524e212a5d9f360f30594c
Emi <emilia.marczyk@rumblefish.pl>
2023-10-19 13:04:29 +0200

    Replace ContentEditable with textarea on desktop

--- packages/desktop/src/renderer/components/widgets/channels/ChannelInput/ChannelInput.tsx
+++ packages/desktop/src/renderer/components/widgets/channels/ChannelInput/ChannelInput.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement, useCallback } from 'react'
 import classNames from 'classnames'
-import ContentEditable, { ContentEditableEvent } from 'react-contenteditable'
 import Picker, { EmojiStyle } from 'emoji-picker-react'
 import Grid from '@mui/material/Grid'
 import { styled } from '@mui/material/styles'
@@ -14,7 +13,6 @@ import emojiBlack from '../../../../static/images/emojiBlack.svg'
 import paperclipGray from '../../../../static/images/paperclipGray.svg'
 import paperclipBlack from '../../../../static/images/paperclipBlack.svg'
 import path from 'path'
-import { isDefined } from '@quiet/common'
 
 const PREFIX = 'ChannelInput'
 
@@ -208,7 +206,6 @@ const StyledChannelInput = styled(Grid)(({ theme }) => ({
 export interface ChannelInputProps {
   channelId: string
   channelName?: string
-  channelParticipants?: Array<{ nickname: string }>
   inputPlaceholder: string
   inputState?: INPUT_STATE
   initialMessage?: string
@@ -224,7 +221,6 @@ export interface ChannelInputProps {
 
 export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
   channelId,
-  channelParticipants = [],
   inputPlaceholder,
   inputState = INPUT_STATE.AVAILABLE,
   initialMessage = '',
@@ -238,18 +234,11 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
   handleOpenFiles,
 }) => {
   const [_anchorEl, setAnchorEl] = React.useState<HTMLDivElement>()
-  const [mentionsToSelect, setMentionsToSelect] = React.useState<
-    Array<{
-      nickname: string
-    }>
-  >([])
   const messageRef = React.useRef<string>()
   const refSelected = React.useRef<number>()
-  const isFirstRenderRef = React.useRef(true)
+  const isFirstRenderRef = React.useRef(true) // What is it for?
 
   const mentionsToSelectRef = React.useRef<any[]>()
-
-  const inputRef = React.createRef<ContentEditable & HTMLDivElement & any>() // any for updater.enqueueForceUpdate
 
   const fileInput = React.useRef<HTMLInputElement>(null)
 
@@ -260,16 +249,7 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
   const [fileExplorerHovered, setFileExplorerHovered] = React.useState(false)
   const [openEmoji, setOpenEmoji] = React.useState(false)
 
-  const [htmlMessage, setHtmlMessage] = React.useState<string>(initialMessage)
   const [message, setMessage] = React.useState(initialMessage)
-
-  React.useEffect(() => {
-    inputRef.current?.el.current.focus()
-  }, [inputRef])
-
-  React.useEffect(() => {
-    inputRef.current.updater.enqueueForceUpdate(inputRef.current)
-  }, [inputPlaceholder, channelId])
 
   // Use reference to bypass memorization
   React.useEffect(() => {
@@ -281,70 +261,20 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
   }
 
   React.useEffect(() => {
-    mentionsToSelectRef.current = mentionsToSelect
-  }, [mentionsToSelect])
-
-  React.useEffect(() => {
-    if (!message) {
-      setHtmlMessage('')
-    }
-  }, [message])
-
-  React.useEffect(() => {
     setMessage(initialMessage)
-    setHtmlMessage(initialMessage)
-    if (!isFirstRenderRef.current) {
-      return () => {
-        if (messageRef?.current) {
-          onChange(messageRef.current)
-        }
-      }
-    }
-    isFirstRenderRef.current = false
+    // if (!isFirstRenderRef.current) {
+    //   return () => {
+    //     if (messageRef?.current) {
+    //       onChange(messageRef.current)
+    //     }
+    //   }
+    // }
+    // isFirstRenderRef.current = false
   }, [channelId])
 
-  React.useEffect(() => {
-    messageRef.current = message
-  }, [message])
-
-  const findMentions = React.useCallback(
-    (text: string) => {
-      // Search for any mention in message string
-      const result: string = text.replace(
-        /(<span([^>]*)>)?@([a-z0-9]?\w*)(<\/span>)?/gi,
-        (match, span, _class, nickname) => {
-          // Ignore already established mentions
-          if (span?.includes('class')) {
-            return match
-          }
-
-          nickname = nickname ?? ''
-          const possibleMentions = channelParticipants
-            .filter(
-              user =>
-                user.nickname.startsWith(nickname) && !channelParticipants.find(user => user.nickname === nickname)
-            )
-            .filter(isDefined)
-
-          if (JSON.stringify(mentionsToSelect) !== JSON.stringify(possibleMentions)) {
-            setMentionsToSelect(possibleMentions)
-            setTimeout(() => {
-              setSelected(0)
-            }, 0)
-          }
-
-          // Wrap mention in spans to be able to treat it as an anchor for popper
-          console.log('Returning wrapped mentions', nickname)
-          return `<span>@${nickname}</span>`
-        }
-      )
-
-      return result
-    },
-    [mentionsToSelect, setMentionsToSelect]
-  )
-
-  const sanitizedHtml = findMentions(htmlMessage)
+  // React.useEffect(() => {
+  //   messageRef.current = message // Do we need that?
+  // }, [message])
 
   const caretLineTraversal = (focusLine: Node | null | undefined, anchorLinePosition = 0) => {
     if (!focusLine?.nodeValue) return
@@ -364,20 +294,13 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
   }
 
   const onChangeCb = useCallback(
-    (e: ContentEditableEvent) => {
+    (e: any) => {
       if (inputState === INPUT_STATE.AVAILABLE) {
-        // @ts-expect-error
-        setMessage(e.nativeEvent.target.innerText)
-        // @ts-expect-error
-        if (!e.nativeEvent.target.innerText) {
-          setHtmlMessage('')
-        } else {
-          setHtmlMessage(e.target.value)
-        }
+        setMessage(e.target.value)
       }
       setAnchorEl(e.currentTarget.lastElementChild)
     },
-    [setAnchorEl, onChange, setHtmlMessage]
+    [setAnchorEl, onChange]
   )
 
   const inputStateRef = React.useRef(inputState)
@@ -385,51 +308,11 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
     inputStateRef.current = inputState
   })
 
-  const mentionSelectAction = (e: React.MouseEvent | React.KeyboardEvent) => {
-    e.preventDefault()
-    if (!mentionsToSelectRef.current || !refSelected.current) return
-    const nickname = mentionsToSelectRef.current[refSelected.current].nickname
-    setHtmlMessage(htmlMessage => {
-      const wrapped = `<span class="${classes.highlight}">@${nickname}</span>&nbsp;`
-      return htmlMessage.replace(/<span>[^/]*<\/span>$/g, wrapped)
-    })
-    // Replace mentions characters with full nickname in original message string
-    setMessage(message.replace(/(\b(\w+)$)/, `${nickname} `))
-    // Clear popper items after choosing mention
-    setMentionsToSelect([])
-    inputRef.current?.el.current.focus()
-  }
-
   const onKeyDownCb = useCallback(
     (e: React.KeyboardEvent) => {
       if (!isRefSelected(refSelected.current)) {
         throw new Error('refSelected is on unexpected type')
       }
-      if (!mentionsToSelectRef?.current) {
-        return
-      }
-      if (mentionsToSelectRef.current.length) {
-        if (e.nativeEvent.keyCode === 40) {
-          if (refSelected.current + 1 >= mentionsToSelectRef.current.length) {
-            setSelected(0)
-          } else {
-            setSelected(refSelected.current + 1)
-          }
-          e.preventDefault()
-        }
-        if (e.nativeEvent.keyCode === 38) {
-          if (refSelected.current - 1 < 0) {
-            setSelected(mentionsToSelectRef.current.length - 1)
-          } else {
-            setSelected(refSelected.current - 1)
-          }
-          e.preventDefault()
-        }
-        if (e.nativeEvent.keyCode === 13 || e.nativeEvent.keyCode === 9) {
-          mentionSelectAction(e)
-        }
-      }
-
       if (e.key === 'ArrowDown') {
         const anchorNode: Node | null | undefined = window?.getSelection()?.anchorNode
 
@@ -481,16 +364,16 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
         caretLineTraversal(previousNode, window?.getSelection()?.anchorOffset)
       }
 
-      if (e.nativeEvent.keyCode === 13) {
+      if (e.nativeEvent.key === 'Enter') {
         if (e.shiftKey) {
           // Accept this input for additional lines in the message box
         } else if (inputStateRef.current === INPUT_STATE.AVAILABLE) {
           e.preventDefault()
           const target = e.target as HTMLInputElement
-          onChange(target.innerText)
-          onKeyPress(target.innerText)
+          onChange(target.value)
+          onKeyPress(target.value)
           setMessage('')
-          setHtmlMessage('')
+          target.value = ''
         } else {
           e.preventDefault()
           if (infoClass !== classNames(classes.backdrop, classes.blinkAnimation)) {
@@ -500,18 +383,7 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
         }
       }
     },
-    [
-      inputState,
-      message,
-      mentionsToSelectRef,
-      onChange,
-      onKeyPress,
-      setMessage,
-      setHtmlMessage,
-      infoClass,
-      setInfoClass,
-      setSelected,
-    ]
+    [inputState, message, mentionsToSelectRef, onChange, onKeyPress, setMessage, infoClass, setInfoClass, setSelected]
   )
 
   const handleFileInput = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -535,22 +407,6 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
         direction='column'
         justifyContent='center'
       >
-        {/* <MentionPoper anchorEl={anchorEl} selected={selected}>
-          {mentionsToSelect.map((target, index) => (
-            <MentionElement
-              key={index}
-              name={target.nickname}
-              highlight={index === selected}
-              onMouseEnter={() => {
-                setSelected(index)
-              }}
-              channelName={channelName}
-              onClick={e => {
-                mentionSelectAction(e)
-              }}
-            />
-          ))}
-        </MentionPoper> */}
         <Grid
           container
           direction='row'
@@ -578,8 +434,7 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
               alignItems='center'
             >
               <Grid item xs className={classes.textArea}>
-                <ContentEditable
-                  ref={inputRef}
+                <textarea
                   placeholder={`Message ${inputPlaceholder}`}
                   className={classes.input}
                   onClick={() => {
@@ -587,22 +442,18 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
                       setFocused(true)
                     }
                   }}
+                  value={message}
                   disabled={inputState !== INPUT_STATE.AVAILABLE}
-                  html={sanitizedHtml}
                   onChange={onChangeCb}
                   onKeyDown={onKeyDownCb}
                   onPaste={async e => {
-                    e.preventDefault()
                     const files = e.clipboardData.files
+                    if (files.length) e.preventDefault()
                     for (let i = 0; i < files.length; i++) {
                       const fileExt = path.extname(files[i].name).toLowerCase()
                       const fileName = path.basename(files[i].name, fileExt)
                       const arrayBuffer = await files[i].arrayBuffer()
                       handleClipboardFiles(arrayBuffer, fileExt, fileName)
-                    }
-                    if (!files.length) {
-                      const text = e.clipboardData.getData('text/plain').replace(/(\r\n|\r|\n)/g, '<br />')
-                      document.execCommand('insertHTML', false, text)
                     }
                   }}
                   data-testid='messageInput'
@@ -662,7 +513,6 @@ export const ChannelInputComponent: React.FC<ChannelInputProps> = ({
                       <div data-testid={'emoji-picker'} className={classes.picker}>
                         <Picker
                           onEmojiClick={(emojiData, _event) => {
-                            setHtmlMessage(htmlMessage => htmlMessage + emojiData.emoji)
                             setMessage(message + emojiData.emoji)
                             setOpenEmoji(false)
                           }}
