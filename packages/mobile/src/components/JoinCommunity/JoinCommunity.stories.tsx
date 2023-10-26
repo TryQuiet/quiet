4646bac474b8b888c683853be14a8df7cf0292d5
Kacper-RF <111343884+Kacper-RF@users.noreply.github.com>
2023-10-26 09:49:32 +0200

    Fix/1981/Duplicate - Unregistered users mixed up - filter CSRs on replicated event (#2002)
    * feat:filter csrs on replicated event
    
    * feat: separate method, unit test, changelog
    
    * feat: trigger mobile e2e
    
    * feat: include getAllEventLogEntries in getCsrs
    
    * feat: load CSRs nad trigger e2e mobile

--- packages/mobile/src/components/JoinCommunity/JoinCommunity.stories.tsx
+++ packages/mobile/src/components/JoinCommunity/JoinCommunity.stories.tsx
@@ -1,7 +1,6 @@
 import { storiesOf } from '@storybook/react-native'
 import React from 'react'
 import { storybookLog } from '../../utils/functions/storybookLog/storybookLog.function'
-
 import { JoinCommunity } from './JoinCommunity.component'
 
 storiesOf('JoinCommunity', module).add('Default', () => (
