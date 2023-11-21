e62fb5371c3f51d47eb83d7746b37f8876e77547
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-11-21 13:44:39 +0100

    fix: take channelsReplicated

--- packages/state-manager/src/sagas/publicChannels/sendUnregisteredInfoMessage/sendUnregisteredInfoMessage.saga.ts
+++ packages/state-manager/src/sagas/publicChannels/sendUnregisteredInfoMessage/sendUnregisteredInfoMessage.saga.ts
@@ -1,19 +1,26 @@
-import { put, select, call } from 'typed-redux-saga'
+import { put, select, call, take } from 'typed-redux-saga'
 import { messagesActions } from '../../messages/messages.slice'
 import { publicChannelsSelectors } from '../publicChannels.selectors'
 import { WriteMessagePayload, MessageType } from '@quiet/types'
 import { communitiesSelectors } from '../../communities/communities.selectors'
 import { identitySelectors } from '../../identity/identity.selectors'
 import { userJoinedMessage } from '@quiet/common'
+import { publicChannelsActions } from '../publicChannels.slice'
 
 export function* sendUnregisteredInfoMessage(): Generator {
   const community = yield* select(communitiesSelectors.currentCommunity)
   if (community?.CA) return
 
   const identity = yield* select(identitySelectors.currentIdentity)
-  const generalChannel = yield* select(publicChannelsSelectors.generalChannel)
+  let generalChannel = yield* select(publicChannelsSelectors.generalChannel)
 
-  if (!community?.name || !identity || !generalChannel) return
+  if (!generalChannel) {
+    yield* take(publicChannelsActions.channelsReplicated)
+  }
+
+  generalChannel = yield* select(publicChannelsSelectors.generalChannel)
+
+  if (!identity || !generalChannel) return
 
   const message = yield* call(userJoinedMessage, identity.nickname)
 
