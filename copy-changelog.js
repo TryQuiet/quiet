f2880c977691974ac3d4daa530afb117f947e5b3
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-11-29 13:47:29 +0100

    feat: copy changelog script

---
+++ copy-changelog.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+
+const rootDir = path.resolve(__dirname)
+const packagesDir = path.resolve(rootDir, 'packages')
+
+const rootChangelog = fs.readFileSync(path.resolve(rootDir, 'CHANGELOG.md'), 'utf8')
+
+const packages = fs.readdirSync(packagesDir).filter(package => !['.DS_Store', '@types'].includes(package))
+
+packages.forEach(package => {
+  fs.writeFileSync(path.resolve(packagesDir, package, 'CHANGELOG.md'), rootChangelog)
+})
