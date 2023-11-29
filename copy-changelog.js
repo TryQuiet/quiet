a7d17dbc1ecae6eecf81557966072772be9cc5a6
Kacper Michalik <kacper.michalik@rumblefish.pl>
2023-11-29 16:46:20 +0100

    feat: include only mobile and desktop

--- copy-changelog.js
+++ copy-changelog.js
@@ -6,7 +6,7 @@ const packagesDir = path.resolve(rootDir, 'packages')
 
 const rootChangelog = fs.readFileSync(path.resolve(rootDir, 'CHANGELOG.md'), 'utf8')
 
-const packages = fs.readdirSync(packagesDir).filter(package => !['.DS_Store', '@types'].includes(package))
+const packages = fs.readdirSync(packagesDir).filter(package => ['desktop', 'mobile'].includes(package))
 
 packages.forEach(package => {
   fs.writeFileSync(path.resolve(packagesDir, package, 'CHANGELOG.md'), rootChangelog)
