b640d1617ec58bb93129adaf8dfebe09c8de625c
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2023-12-07 13:50:30 +0100

    fix: send csr if local and stored ones differs (#2147)
    * fix: comment logic in saveUserCsr
    
    * test: e2e test for username taken
    
    * fix: cleanup
    
    * docs: changelog
    
    * fix: back to develop saveUserCsrSaga
    
    * feat: cleanup
    
    * fix: send csr if local and stored ones differs
    
    * test: checking local csr
    
    * fix: checking local csr
    
    * fix: save owner csr on NEW_COMMUNITY
    
    * test: fix rtl test
    
    ---------
    
    Co-authored-by: Kacper Michalik <kacper.michalik@rumblefish.pl>

--- packages/state-manager/src/sagas/identity/saveUserCsr/saveUserCsr.saga.test.ts
+++ packages/state-manager/src/sagas/identity/saveUserCsr/saveUserCsr.saga.test.ts
@@ -1,17 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { expectSaga } from 'redux-saga-test-plan'
 import { type Socket } from '../../../types'
-import { communitiesReducer, type communitiesActions } from '../../communities/communities.slice'
-import { StoreKeys } from '../../store.keys'
-import { identityReducer, type identityActions } from '../identity.slice'
+import { type communitiesActions } from '../../communities/communities.slice'
+import { type identityActions } from '../identity.slice'
 import { saveUserCsrSaga } from './saveUserCsr.saga'
 import { type Store } from '../../store.types'
 import { type FactoryGirl } from 'factory-girl'
-import { pubKeyFromCsr, setupCrypto, createUserCertificateTestHelper } from '@quiet/identity'
+import { setupCrypto } from '@quiet/identity'
 import { prepareStore } from '../../../utils/tests/prepareStore'
 import { getFactory } from '../../../utils/tests/factories'
 import { SocketActionTypes } from '@quiet/types'
-import { usersReducer } from '../../users/users.slice'
+import { reducers } from '../../reducers'
 
 describe('saveUserCsr', () => {
   let store: Store
@@ -23,70 +22,22 @@ describe('saveUserCsr', () => {
     factory = await getFactory(store)
   })
 
-  test('saves user csr if user csr is absent in csrs list', async () => {
+  test('saves user csr', async () => {
     const socket = { emit: jest.fn(), on: jest.fn() } as unknown as Socket
 
     const community =
       await factory.create<ReturnType<typeof communitiesActions.addNewCommunity>['payload']>('Community')
 
-    const csr = (
-      await createUserCertificateTestHelper(
-        {
-          nickname: 'john',
-          commonName: 'commonName',
-          peerId: 'peerId',
-          dmPublicKey: 'dmPublicKey',
-        },
-        community.CA
-      )
-    ).userCsr
-
     const identity = await factory.create<ReturnType<typeof identityActions.addNewIdentity>['payload']>('Identity', {
       id: community.id,
       nickname: 'john',
-      userCsr: csr,
     })
 
-    const reducer = combineReducers({
-      [StoreKeys.Communities]: communitiesReducer,
-      [StoreKeys.Identity]: identityReducer,
-      [StoreKeys.Users]: usersReducer,
-    })
-
+    const reducer = combineReducers(reducers)
     await expectSaga(saveUserCsrSaga, socket)
       .withReducer(reducer)
       .withState(store.getState())
-      .call(pubKeyFromCsr, identity.userCsr?.userCsr)
       .apply(socket, socket.emit, [
-        SocketActionTypes.SAVE_USER_CSR,
-        {
-          csr: identity.userCsr?.userCsr,
-        },
-      ])
-      .run()
-  })
-  test("do not save user csr if it's already included in csrs list", async () => {
-    const socket = { emit: jest.fn(), on: jest.fn() } as unknown as Socket
-
-    const community =
-      await factory.create<ReturnType<typeof communitiesActions.addNewCommunity>['payload']>('Community')
-
-    const identity = await factory.create<ReturnType<typeof identityActions.addNewIdentity>['payload']>('Identity', {
-      id: community.id,
-      nickname: 'john',
-    })
-
-    const reducer = combineReducers({
-      [StoreKeys.Communities]: communitiesReducer,
-      [StoreKeys.Identity]: identityReducer,
-      [StoreKeys.Users]: usersReducer,
-    })
-
-    await expectSaga(saveUserCsrSaga, socket)
-      .withReducer(reducer)
-      .withState(store.getState())
-      .call(pubKeyFromCsr, identity.userCsr?.userCsr)
-      .not.apply(socket, socket.emit, [
         SocketActionTypes.SAVE_USER_CSR,
         {
           csr: identity.userCsr?.userCsr,
