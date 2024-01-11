910193c0bd50639ed6273c3ade3d9447d647fbb0
siepra <wiktor.siepra@gmail.com>
2024-01-11 10:22:50 +0100

    fix: notarization config

--- packages/desktop/scripts/notarize.js
+++ packages/desktop/scripts/notarize.js
@@ -2,7 +2,8 @@
 const { notarize } = require('@electron/notarize')
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName } = context
+  const { electronPlatformName, appOutDir } = context
+  const appName = context.packager.appInfo.productFilename
 
   if (electronPlatformName !== 'darwin' || process.env.IS_E2E) {
     console.log('skipping notarization')
@@ -13,6 +14,7 @@ exports.default = async function notarizing(context) {
 
   const response = await notarize({
     tool: 'notarytool',
+    appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
     teamId: process.env.APPLE_TEAM_ID
