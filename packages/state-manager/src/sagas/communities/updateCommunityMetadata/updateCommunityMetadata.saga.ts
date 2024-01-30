6c58f4dd16bc59cb5914e110c31e62c35359cc29
Lucas Leblow <lucas@quiet.chat>
2024-01-30 14:40:24 -0800

    Fixup

--- packages/state-manager/src/sagas/communities/updateCommunityMetadata/updateCommunityMetadata.saga.ts
+++ packages/state-manager/src/sagas/communities/updateCommunityMetadata/updateCommunityMetadata.saga.ts
@@ -4,14 +4,21 @@ import { apply, select } from 'typed-redux-saga'
 import { applyEmitParams, type Socket } from '../../../types'
 import { communitiesSelectors } from '../communities.selectors'
 import { communitiesActions } from '../communities.slice'
+import { identitySelectors } from '../../identity/identity.selectors'
 
 export function* sendCommunityMetadataSaga(
   socket: Socket,
   _action: PayloadAction<ReturnType<typeof communitiesActions.sendCommunityMetadata>['payload']>
 ): Generator {
+  const identity = yield* select(identitySelectors.currentIdentity)
   const community = yield* select(communitiesSelectors.currentCommunity)
   if (!community) {
     console.error('Cannot send community metadata, no community')
+    return
+  }
+
+  if (!identity?.userCertificate) {
+    console.error('Cannot send community metadata, no owner certificate')
     return
   }
 
