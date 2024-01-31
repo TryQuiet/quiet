fe1d9dda73ffaad585c87701bcb9658af0389362
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2024-01-31 10:05:39 +0100

    fix: calling init websocket connection (#2261)

--- packages/mobile/ios/Quiet/AppDelegate.m
+++ packages/mobile/ios/Quiet/AppDelegate.m
@@ -162,8 +162,9 @@ static NSString *const platform = @"mobile";
       
     
   // (5/6) Update data port information and broadcast it to frontend
-  
-  [self initWebsocketConnection];
+  if (init) {
+    [self initWebsocketConnection];
+  }
     
     
   // (6/6) Launch backend or reviwe services
