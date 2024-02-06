34503257e5adb4cbe9815c372658ae9736cff0d5
Lucas Leblow <lucas@quiet.chat>
2024-02-06 12:23:22 -0800

    Reorganize events

--- packages/types/src/socket.ts
+++ packages/types/src/socket.ts
@@ -1,65 +1,76 @@
 export enum SocketActionTypes {
-  // A
-  ASK_FOR_MESSAGES = 'askForMessages',
-  // C
-  CANCEL_DOWNLOAD = 'cancelDownload',
-  CERTIFICATES_LOADED = 'certificatesLoaded',
-  CHANNEL_DELETION_RESPONSE = 'channelDeletionResponse',
-  CHANNELS_LOADED = 'channelsLoaded',
-  CHANNEL_SUBSCRIBED = 'channelSubscribed',
-  CHECK_FOR_MISSING_FILES = 'checkForMissingFiles',
-  CLOSE = 'close',
+
+  // ====== Community ======
+
   COMMUNITY_CREATED = 'communityCreated',
   COMMUNITY_LAUNCHED = 'communityLaunched',
   COMMUNITY_METADATA_LOADED = 'communityMetadataLoaded',
+  CREATE_COMMUNITY = 'createCommunity',
+  LAUNCH_COMMUNITY = 'launchCommunity',
+  LEAVE_COMMUNITY = 'leaveCommunity',
+  SEND_COMMUNITY_METADATA = 'sendCommunityMetadata',
+  SEND_COMMUNITY_CA_DATA = 'sendCommunityCaData',
+
+  // ====== Channels ======
+
+  CHANNEL_DELETION_RESPONSE = 'channelDeletionResponse',
+  CHANNELS_LOADED = 'channelsLoaded',
+  CHANNEL_SUBSCRIBED = 'channelSubscribed',
+  CREATE_CHANNEL = 'createChannel',
+  CREATED_CHANNEL = 'createdChannel',
+  DELETE_CHANNEL = 'deleteChannel',
+  DELETE_FILES_FROM_CHANNEL = 'deleteFilesFromChannel',
+
+  // ====== Messages ======
+
+  ASK_FOR_MESSAGES = 'askForMessages',
+  INCOMING_MESSAGES = 'incomingMessages',
+  SEND_MESSAGE = 'sendMessage',
+  SEND_MESSAGES_IDS = 'sendIds',
+  UPDATE_MESSAGE_MEDIA = 'updateMessageMedia',
+
+  // ====== User ======
+
+  SEND_USER_PROFILE = 'sendUserProfile',
+  USER_PROFILES_LOADED = 'userProfilesLoaded',
+
+  // ====== Files ======
+
+  CANCEL_DOWNLOAD = 'cancelDownload',
+  CHECK_FOR_MISSING_FILES = 'checkForMissingFiles',
+  DOWNLOAD_FILE = 'downloadFile',
+  DOWNLOAD_PROGRESS = 'downloadProgress',
+  UPLOAD_FILE = 'uploadFile',
+  UPLOADED_FILE = 'uploadedFile',
+  REMOVE_DOWNLOAD_STATUS = 'removeDownloadStatus',
+
+  // ====== Certificates ======
+
+  CERTIFICATES_LOADED = 'certificatesLoaded',
+  CSRS_LOADED = 'csrsLoaded',
+  OWNER_CERTIFICATE_LOADED = 'ownerCertificateLoaded',
+  REGISTER_USER_CERTIFICATE = 'registerUserCertificate',
+  REGISTER_OWNER_CERTIFICATE = 'registerOwnerCertificate',
+  SEND_USER_CSR = 'sendUserCsr',
+  USER_CSR_LOADED = 'userCsrLoaded',
+
+  // ====== Network ======
+
+  CLOSE = 'close',
   CONNECTED_PEERS = 'connectedPeers',
   CONNECTION = 'connection',
   CONNECTION_PROCESS_INFO = 'connectionProcess',
-  CREATE_CHANNEL = 'createChannel',
-  CREATED_CHANNEL = 'createdChannel',
-  CREATE_COMMUNITY = 'createCommunity',
   CREATE_NETWORK = 'createNetwork',
-  CSRS_LOADED = 'csrsLoaded',
-  // D
-  DELETE_CHANNEL = 'deleteChannel',
-  DELETE_FILES_FROM_CHANNEL = 'deleteFilesFromChannel',
-  DOWNLOAD_FILE = 'downloadFile',
-  DOWNLOAD_PROGRESS = 'downloadProgress',
-  // E
-  ERROR = 'error',
-  // I
-  INCOMING_MESSAGES = 'incomingMessages',
-  // L
-  LAUNCH_COMMUNITY = 'launchCommunity',
-  LEAVE_COMMUNITY = 'leaveCommunity',
   LIBP2P_PSK_LOADED = 'libp2pPskLoaded',
-  // N
-  NETWORK = 'network',
-  // O
-  OWNER_CERTIFICATE_LOADED = 'ownerCertificateLoaded',
-  // P
+  NETWORK_CREATED = 'networkCreated',
   PEER_CONNECTED = 'peerConnected',
   PEER_DISCONNECTED = 'peerDisconnected',
   PEER_LIST = 'peerList',
-  PUSH_NOTIFICATION = 'pushNotification',
-  // R
-  REGISTER_USER_CERTIFICATE = 'registerUserCertificate',
-  REGISTER_OWNER_CERTIFICATE = 'registerOwnerCertificate',
-  REMOVE_DOWNLOAD_STATUS = 'removeDownloadStatus',
-  // S
-  SEND_USER_CSR = 'sendUserCsr',
-  SEND_USER_PROFILE = 'sendUserProfile',
-  SEND_MESSAGE = 'sendMessage',
-  SEND_MESSAGES_IDS = 'sendIds',
-  SEND_PEER_ID = 'sendPeerId',
-  SEND_COMMUNITY_METADATA = 'sendCommunityMetadata',
-  SEND_COMMUNITY_CA_DATA = 'sendCommunityCaData',
-  //  T
   TOR_INITIALIZED = 'torInitialized',
-  // U
-  UPDATE_MESSAGE_MEDIA = 'updateMessageMedia',
-  UPLOAD_FILE = 'uploadFile',
-  UPLOADED_FILE = 'uploadedFile',
-  USER_CSR_LOADED = 'userCsrLoaded',
-  USER_PROFILES_LOADED = 'userProfilesLoaded',
+
+  // ====== Misc ======
+
+  PUSH_NOTIFICATION = 'pushNotification',
+  ERROR = 'error',
+
 }
