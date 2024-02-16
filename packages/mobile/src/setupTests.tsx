707d8a75a5c1e23b43085854db2cb6cef307c341
siepra <wiktor.siepra@gmail.com>
2024-02-16 15:11:53 +0100

    fix: set identity crypto engine for mobile tests

--- packages/mobile/src/setupTests.tsx
+++ packages/mobile/src/setupTests.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import { setEngine, CryptoEngine } from 'pkijs'
+import { setEngine as setIdentityEngine } from '../../identity/node_modules/pkijs'
 import { Crypto } from '@peculiar/webcrypto'
 import React from 'react'
 
@@ -9,6 +10,16 @@ const webcrypto = new Crypto()
 global.crypto = webcrypto
 
 setEngine(
+  'newEngine',
+  webcrypto,
+  new CryptoEngine({
+    name: '',
+    crypto: webcrypto,
+    subtle: webcrypto.subtle,
+  })
+)
+
+setIdentityEngine(
   'newEngine',
   webcrypto,
   new CryptoEngine({
