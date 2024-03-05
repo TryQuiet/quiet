b8aa768d50f12adb278333228f1ac0104f1625e8
Emi <emilia.marczyk@rumblefish.pl>
2024-03-05 17:30:45 +0100

    fix: customProtocolSaga tests

--- packages/desktop/src/renderer/sagas/invitation/customProtocol.saga.test.ts
+++ packages/desktop/src/renderer/sagas/invitation/customProtocol.saga.test.ts
@@ -15,6 +15,7 @@ describe('Handle invitation code', () => {
   let factory: FactoryGirl
   let community: Community
   let validInvitationData: InvitationData
+  let validInvitationDeepUrl: string
 
   beforeEach(async () => {
     store = (
@@ -29,99 +30,94 @@ describe('Handle invitation code', () => {
     factory = await getFactory(store)
 
     validInvitationData = getValidInvitationUrlTestData(validInvitationCodeTestData[0]).data
+    validInvitationDeepUrl = getValidInvitationUrlTestData(validInvitationCodeTestData[0]).deepUrl()
   })
 
-  // it('creates network if code is valid', async () => {
-  //   const payload: CreateNetworkPayload = {
-  //     ownership: CommunityOwnership.User,
-  //     peers: validInvitationData.pairs,
-  //     psk: validInvitationData.psk,
-  //     ownerOrbitDbIdentity: validInvitationData.ownerOrbitDbIdentity,
-  //   }
-  //   await expectSaga(customProtocolSaga, communities.actions.customProtocol(validInvitationData))
-  //     .withState(store.getState())
-  //     .put(communities.actions.createNetwork(payload))
-  //     .run()
-  // })
+  it('creates network if code is valid', async () => {
+    const payload: CreateNetworkPayload = {
+      ownership: CommunityOwnership.User,
+      peers: validInvitationData.pairs,
+      psk: validInvitationData.psk,
+      ownerOrbitDbIdentity: validInvitationData.ownerOrbitDbIdentity,
+    }
+    await expectSaga(customProtocolSaga, communities.actions.customProtocol([validInvitationDeepUrl]))
+      .withState(store.getState())
+      .put(communities.actions.createNetwork(payload))
+      .run()
+  })
 
-  // it('does not try to create network if user is already in community', async () => {
-  //   community = await factory.create<ReturnType<typeof communities.actions.addNewCommunity>['payload']>('Community')
-  //   const payload: CreateNetworkPayload = {
-  //     ownership: CommunityOwnership.User,
-  //     peers: validInvitationData.pairs,
-  //     psk: validInvitationData.psk,
-  //   }
+  it('does not try to create network if user is already in community', async () => {
+    community = await factory.create<ReturnType<typeof communities.actions.addNewCommunity>['payload']>('Community')
+    const payload: CreateNetworkPayload = {
+      ownership: CommunityOwnership.User,
+      peers: validInvitationData.pairs,
+      psk: validInvitationData.psk,
+    }
 
-  //   await expectSaga(customProtocolSaga, communities.actions.customProtocol(validInvitationData))
-  //     .withState(store.getState())
-  //     .put(
-  //       modalsActions.openModal({
-  //         name: ModalName.warningModal,
-  //         args: {
-  //           title: 'You already belong to a community',
-  //           subtitle: "We're sorry but for now you can only be a member of a single community at a time.",
-  //         },
-  //       })
-  //     )
-  //     .not.put(communities.actions.createNetwork(payload))
-  //     .run()
-  // })
+    await expectSaga(customProtocolSaga, communities.actions.customProtocol([validInvitationDeepUrl]))
+      .withState(store.getState())
+      .put(
+        modalsActions.openModal({
+          name: ModalName.warningModal,
+          args: {
+            title: 'You already belong to a community',
+            subtitle: "We're sorry but for now you can only be a member of a single community at a time.",
+          },
+        })
+      )
+      .not.put(communities.actions.createNetwork(payload))
+      .run()
+  })
 
-  // it('does not try to create network if code is missing addresses', async () => {
-  //   const payload: CreateNetworkPayload = {
-  //     ownership: CommunityOwnership.User,
-  //     peers: [],
-  //   }
+  it('does not try to create network if code is missing psk', async () => {
+    const payload: CreateNetworkPayload = {
+      ownership: CommunityOwnership.User,
+      peers: [],
+    }
 
-  //   await expectSaga(
-  //     customProtocolSaga,
-  //     communities.actions.customProtocol({
-  //       pairs: [],
-  //       psk: '12345',
-  //       ownerOrbitDbIdentity: 'testOwnerOrbitDbIdentity',
-  //     })
-  //   )
-  //     .withState(store.getState())
-  //     .put(communities.actions.clearInvitationCodes())
-  //     .put(
-  //       modalsActions.openModal({
-  //         name: ModalName.warningModal,
-  //         args: {
-  //           title: 'Invalid link',
-  //           subtitle: 'The invite link you received is not valid. Please check it and try again.',
-  //         },
-  //       })
-  //     )
-  //     .not.put(communities.actions.createNetwork(payload))
-  //     .run()
-  // })
+    await expectSaga(
+      customProtocolSaga,
+      communities.actions.customProtocol(['someArg', 'quiet://?k=BNlxfE2WBF7LrlpIX0CvECN5o1oZtA16PkAb7GYiwYw='])
+    )
+      .withState(store.getState())
+      .put(communities.actions.clearInvitationCodes())
+      .put(
+        modalsActions.openModal({
+          name: ModalName.warningModal,
+          args: {
+            title: 'Invalid link',
+            subtitle: 'The invite link you received is not valid. Please check it and try again.',
+          },
+        })
+      )
+      .not.put(communities.actions.createNetwork(payload))
+      .run()
+  })
 
-  // it('does not try to create network if code is missing psk', async () => {
-  //   const payload: CreateNetworkPayload = {
-  //     ownership: CommunityOwnership.User,
-  //     peers: [],
-  //   }
+  it('does not try to create network if code is missing psk', async () => {
+    const payload: CreateNetworkPayload = {
+      ownership: CommunityOwnership.User,
+      peers: [],
+    }
 
-  //   await expectSaga(
-  //     customProtocolSaga,
-  //     communities.actions.customProtocol({
-  //       pairs: validInvitationData.pairs,
-  //       psk: '',
-  //       ownerOrbitDbIdentity: 'testOwnerOrbitDbIdentity',
-  //     })
-  //   )
-  //     .withState(store.getState())
-  //     .put(communities.actions.clearInvitationCodes())
-  //     .put(
-  //       modalsActions.openModal({
-  //         name: ModalName.warningModal,
-  //         args: {
-  //           title: 'Invalid link',
-  //           subtitle: 'The invite link you received is not valid. Please check it and try again.',
-  //         },
-  //       })
-  //     )
-  //     .not.put(communities.actions.createNetwork(payload))
-  //     .run()
-  // })
+    await expectSaga(
+      customProtocolSaga,
+      communities.actions.customProtocol([
+        'quiet://?QmZoiJNAvCffeEHBjk766nLuKVdkxkAT7wfFJDPPLsbKSE=y7yczmugl2tekami7sbdz5pfaemvx7bahwthrdvcbzw5vex2crsr26qd',
+      ])
+    )
+      .withState(store.getState())
+      .put(communities.actions.clearInvitationCodes())
+      .put(
+        modalsActions.openModal({
+          name: ModalName.warningModal,
+          args: {
+            title: 'Invalid link',
+            subtitle: 'The invite link you received is not valid. Please check it and try again.',
+          },
+        })
+      )
+      .not.put(communities.actions.createNetwork(payload))
+      .run()
+  })
 })
