dc4c76e5112b64c1db2cd24e6a5f00f24474904d
ikoenigsknecht <ikoenigsknecht@users.noreply.github.com>
2024-03-12 09:15:58 -0400

    Updater baseUrl if we do github pages

--- quiet-docs/docusaurus.config.ts
+++ quiet-docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/quiet/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
