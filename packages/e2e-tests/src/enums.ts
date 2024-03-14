a9aca46a7214a8b433ec99e27182717ff78bba77
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-03-14 10:59:13 -0400

    Linting

--- packages/e2e-tests/src/enums.ts
+++ packages/e2e-tests/src/enums.ts
@@ -1,4 +1,4 @@
 export enum BACK_ARROW_DATA_TESTID {
-    EDIT_PROFILE='contextMenu-Editprofile-backArrow',
-    PROFILE='contextMenu-Profile-backArrow'
+  EDIT_PROFILE = 'contextMenu-Editprofile-backArrow',
+  PROFILE = 'contextMenu-Profile-backArrow',
 }
