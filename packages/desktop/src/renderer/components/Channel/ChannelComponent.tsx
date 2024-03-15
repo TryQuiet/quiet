b9e1371bf3eb7516651ca81dec00d2c021a8fa90
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-03-15 12:51:49 -0400

    Remove commented out code

--- packages/desktop/src/renderer/components/Channel/ChannelComponent.tsx
+++ packages/desktop/src/renderer/components/Channel/ChannelComponent.tsx
@@ -285,36 +285,6 @@ export const ChannelComponent: React.FC<ChannelComponentProps & UploadFilesPrevi
             duplicatedUsernameModalHandleOpen={duplicatedUsernameModalHandleOpen}
           />
         </ChannelMessagesWrapperStyled>
-        {/* {
-          !isConnectedToOtherPeers && communityHasPeers && (
-            <Grid
-              container
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                padding: '11px 16px 11px 16px',
-                width: '100%',
-                borderTop: '1px solid #F0F0F0',
-                borderRadius: '16px 16px 0px 0px',
-              }}
-            >
-              <Grid
-                item
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  paddingRight: '12px',
-                }}
-              >
-                <CircularProgress color='inherit' className={'channelQuietConnectingSpinner'} size={20} />
-              </Grid>
-              <Typography fontSize={16} fontWeight={'normal'} justifyContent={'center'}>
-                Quiet is trying to connect...
-              </Typography>
-            </Grid>
-          )
-        } */}
         <Grid
           container
           style={{
