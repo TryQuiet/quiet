5f80ac1b68ed76d1ea81fa88eb9750e167dc2315
Emi <emilia.marczyk@rumblefish.pl>
2024-03-19 16:30:12 +0100

    fix: 'copy app image for e2e' script

--- packages/e2e-tests/scripts/copyAppImage.js
+++ packages/e2e-tests/scripts/copyAppImage.js
@@ -1,14 +1,16 @@
 // Copy built AppImage to Quiet directory and set the version in .env file
-const { execSync } = require('child_process')
+const { execFileSync } = require('child_process')
 const path = require('path')
+const fs = require('fs')
 
 const desktop = path.join(__dirname, '..', '..', 'desktop')
 const e2e = path.join(__dirname, '..')
 const appVersion = JSON.parse(require('fs').readFileSync(path.join(desktop, 'package.json'), 'utf8')).version
 const fileName = `Quiet-${appVersion}.AppImage`
 
-execSync(`rm -rf ${path.join(desktop, 'dist', 'squashfs-root')}`)
+execFileSync('rm', ['-rf', path.join(desktop, 'dist', 'squashfs-root')])
 
 console.log(`Copying file ${fileName} for e2e tests`)
-execSync(`cp ${path.join(desktop, 'dist', fileName)} ${path.join(e2e, 'Quiet', fileName)}`)
-execSync(`echo "FILE_NAME=${fileName}" > ${path.join(e2e, '.env')}`)
+execFileSync('cp', [path.join(desktop, 'dist', fileName), path.join(e2e, 'Quiet', fileName)])
+
+fs.writeFileSync(path.join(e2e, '.env'), `FILE_NAME=${fileName}`)
