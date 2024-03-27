b8370ed00fbd9f0e6bdd00483db99d73dcc47acc
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-03-27 11:55:55 -0400

    Add more desktop tests

--- packages/desktop/src/renderer/components/Channel/File/FileComponent/FileComponent.test.tsx
+++ packages/desktop/src/renderer/components/Channel/File/FileComponent/FileComponent.test.tsx
@@ -110,4 +110,110 @@ describe('FileComponent', () => {
       </body>
     `)
   })
+
+  it('renders component as unsent', () => {
+    const result = renderComponent(
+      <FileComponent
+        message={{
+          id: '32',
+          type: 2,
+          isDuplicated: false,
+          isRegistered: true,
+          pubKey: 'string',
+          media: {
+            cid: 'QmWUCSApiy76nW9DAk5M9QbH1nkW5XCYwxUHRSULjATyqs',
+            message: {
+              channelId: 'general',
+              id: 'wgtlstx3u7',
+            },
+            ext: '.zip',
+            name: 'my-file-name-goes-here-an-isnt-truncated',
+            size: 2048,
+            width: 1200,
+            height: 580,
+            path: 'files/my-file-name-goes-here-an-isnt-truncated.zip',
+          },
+          message: '',
+          createdAt: 0,
+          date: '12:46',
+          nickname: 'vader',
+        }}
+        downloadStatus={{
+          mid: '32',
+          cid: 'QmWUCSApiy76nW9DAk5M9QbH1nkW5XCYwxUHRSULjATyqs',
+          downloadState: DownloadState.Ready,
+          downloadProgress: undefined,
+        }}
+        isUnsent={true}
+      />
+    )
+    expect(result.baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div>
+          <div
+            class="css-17r12jj"
+            data-testid="QmWUCSApiy76nW9DAk5M9QbH1nkW5XCYwxUHRSULjATyqs-fileComponent"
+          >
+            <span>
+              <div
+                aria-label=""
+                class="FileComponentunsent"
+                data-mui-internal-clone-element="true"
+                style="display: flex;"
+              >
+                <div
+                  class="FileComponenticon"
+                >
+                  <img
+                    class="FileComponentfileIcon"
+                    src="test-file-stub"
+                  />
+                </div>
+                <div
+                  class="FileComponentfilename"
+                >
+                  <h5
+                    class="MuiTypography-root MuiTypography-h5 css-11l3dv4-MuiTypography-root"
+                    style="line-height: 20px;"
+                  >
+                    my-file-name-goes-here-an-isnt-truncated
+                    .zip
+                  </h5>
+                  <p
+                    class="MuiTypography-root MuiTypography-body2 css-16d47hw-MuiTypography-root"
+                    style="line-height: 20px; color: rgb(127, 127, 127);"
+                  >
+                    2 KB
+                  </p>
+                </div>
+              </div>
+            </span>
+            <div
+              class="FileComponentunsent"
+              style="padding-top: 16px; display: block;"
+            >
+              <div
+                style="cursor: pointer;"
+              >
+                <div
+                  class="css-1vnortn"
+                >
+                  <img
+                    class="FileComponentactionIcon"
+                    src="test-file-stub"
+                  />
+                  <p
+                    class="MuiTypography-root MuiTypography-body2 css-16d47hw-MuiTypography-root"
+                    style="color: rgb(103, 191, 211); margin-left: 8px;"
+                  >
+                    Download file
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    `)
+  })
 })
--- packages/desktop/src/renderer/components/Channel/File/UploadedImage/UploadedImage.test.tsx
+++ packages/desktop/src/renderer/components/Channel/File/UploadedImage/UploadedImage.test.tsx
@@ -155,4 +155,122 @@ describe('UploadedFile', () => {
       </body>
     `)
   })
+
+  it('renders an unsent placeholder if image is not finished downloading yet', () => {
+    const result = renderComponent(
+      <UploadedImage
+        // @ts-expect-error
+        media={message.media}
+        downloadStatus={downloadStatus}
+        isUnsent={true}
+      />
+    )
+    expect(result.baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div>
+          <div
+            class="css-1clc91l"
+          >
+            <div
+              class="css-ytr8bc"
+              data-testid="abcd1234-imagePlaceholder"
+            >
+              <p
+                class="css-h94c3"
+              >
+                test.png
+              </p>
+              <div
+                class="UploadedImagePlaceholderplaceholder"
+                style="width: 400px;"
+              >
+                <span>
+                  <div
+                    aria-label=""
+                    class=""
+                    data-mui-internal-clone-element="true"
+                    style="display: flex;"
+                  >
+                    <img
+                      class="UploadedImagePlaceholderplaceholderIcon"
+                      src="test-file-stub"
+                    />
+                    <div
+                      class="UploadedImagePlaceholdericon"
+                    >
+                      <span
+                        class="MuiCircularProgress-root MuiCircularProgress-indeterminate MuiCircularProgress-colorPrimary css-9a4009-MuiCircularProgress-root"
+                        role="progressbar"
+                        style="width: 18px; height: 18px; position: absolute; color: rgb(178, 178, 178);"
+                      >
+                        <svg
+                          class="MuiCircularProgress-svg css-1idz92c-MuiCircularProgress-svg"
+                          viewBox="22 22 44 44"
+                        >
+                          <circle
+                            class="MuiCircularProgress-circle MuiCircularProgress-circleIndeterminate css-176wh8e-MuiCircularProgress-circle"
+                            cx="44"
+                            cy="44"
+                            fill="none"
+                            r="20"
+                            stroke-width="4"
+                          />
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    `)
+  })
+  it('renders unsent image if image is downloaded', () => {
+    // @ts-expect-error
+    message.media.path = 'path/to/file/test.png'
+    // @ts-expect-error
+    message.media.message = {
+      id: 'string',
+      channelId: 'general',
+    }
+    const result = renderComponent(
+      <UploadedImage
+        // @ts-expect-error
+        media={message.media}
+        downloadStatus={downloadStatus}
+        isUnsent={true}
+      />
+    )
+    expect(result.baseElement).toMatchInlineSnapshot(`
+      <body>
+        <div>
+          <div
+            class="css-1clc91l"
+          >
+            <div
+              class="UploadedImagecontainer UploadedImageunsent"
+            >
+              <div
+                class="UploadedImageimage"
+                data-testid="abcd1234-imageVisual"
+              >
+                <p
+                  class="css-h94c3"
+                >
+                  test.png
+                </p>
+                <img
+                  class="UploadedImageimage"
+                  src="path/to/file/test.png"
+                  style="width: 400px;"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    `)
+  })
 })
