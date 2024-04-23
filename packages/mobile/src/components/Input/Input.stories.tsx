422f849210ed89cf8044ecd0b9ceae13d3f9f43c
Taea Vogel <taelxvie@gmail.com>
2024-04-23 17:52:39 -0400

    disable autocorrect on username and channel inputs

--- packages/mobile/src/components/Input/Input.stories.tsx
+++ packages/mobile/src/components/Input/Input.stories.tsx
@@ -7,16 +7,27 @@ storiesOf('Input', module)
   .add('Default', () => <Input placeholder={'Message #general as @holmes'} />)
   .add('Disabled', () => <Input placeholder={'Message #general as @holmes'} disabled={true} />)
   .add('Label', () => (
-    <Input label={'Choose your favorite username'} placeholder={'Enter a username'} validation={undefined} />
+    <Input
+      label={'Choose your favorite username'}
+      placeholder={'Enter a username'}
+      validation={undefined}
+      autoCorrect={false}
+    />
   ))
   .add('Validation', () => (
-    <Input label={'Choose your favorite username'} placeholder={'Enter a username'} validation={'Username invalid'} />
+    <Input
+      label={'Choose your favorite username'}
+      placeholder={'Enter a username'}
+      validation={'Username invalid'}
+      autoCorrect={false}
+    />
   ))
   .add('Hint', () => (
     <Input
       label={'Choose your favorite username'}
       placeholder={'Enter a username'}
       hint={'Your username cannot have any spaces or special characters, must be lowercase letters and numbers only.'}
+      autoCorrect={false}
     />
   ))
   .add('HintValidation', () => (
@@ -25,5 +36,6 @@ storiesOf('Input', module)
       placeholder={'Enter a username'}
       validation={'Username invalid'}
       hint={'Your username cannot have any spaces or special characters, must be lowercase letters and numbers only.'}
+      autoCorrect={false}
     />
   ))
