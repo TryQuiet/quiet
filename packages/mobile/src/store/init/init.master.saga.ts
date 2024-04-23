de6eb32cc506f9a89bf370641439138e1c1f8fce
siepra <wiktor.siepra@gmail.com>
2024-04-23 11:14:53 +0200

    chore: remove redundand sagas

--- packages/mobile/src/store/init/init.master.saga.ts
+++ packages/mobile/src/store/init/init.master.saga.ts
@@ -1,12 +1,10 @@
-import { all, takeEvery, takeLatest, takeLeading } from 'typed-redux-saga'
+import { all, takeLatest, takeLeading } from 'typed-redux-saga'
 import { initActions } from './init.slice'
-import { blindConnectionSaga } from './blindConnection/blindConnection.saga'
 import { startConnectionSaga } from './startConnection/startConnection.saga'
 import { deepLinkSaga } from './deepLink/deepLink.saga'
 
 export function* initMasterSaga(): Generator {
   yield all([
-    takeEvery(initActions.blindWebsocketConnection.type, blindConnectionSaga),
     takeLatest(initActions.startWebsocketConnection.type, startConnectionSaga),
     takeLeading(initActions.deepLink.type, deepLinkSaga),
   ])
