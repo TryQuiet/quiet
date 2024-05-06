611af21d316b80767190df5bda498ed2a810a058
Wiktor Sieprawski <wiktor.sieprawski@rumblefish.pl>
2024-05-06 12:49:36 +0200

    fix: start websocket connection on react init (#2481)
    * fix: start websocket connection on react init
    
    * chore: restore self hosted ios e2e tests
    
    * fix: prevent stripping hermes framework
    
    * Revert "fix: prevent stripping hermes framework"
    
    This reverts commit 65dfec8db70de0b0783782cafb25b451f369a109.
    
    * chore: skip self-hosted ios e2e tests

--- packages/mobile/ios/CommunicationBridge.m
+++ packages/mobile/ios/CommunicationBridge.m
@@ -2,4 +2,5 @@
 #import "React/RCTEventEmitter.h"
 
 @interface RCT_EXTERN_MODULE(CommunicationModule, RCTEventEmitter)
+RCT_EXTERN_METHOD(handleIncomingEvents:(NSString *)event payload:(NSString *)payload extra:(NSString *)extra)
 @end
