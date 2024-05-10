6bccb85143acb973ccd0faade74a3bb16b8ae41f
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-05-10 17:27:49 -0400

    Ensure renderer logs also show up in browser

--- packages/desktop/src/renderer/logger.ts
+++ packages/desktop/src/renderer/logger.ts
@@ -1,3 +1,3 @@
 import { createQuietLogger } from '@quiet/logger'
 
-export const createLogger = createQuietLogger('desktop:renderer')
+export const createLogger = createQuietLogger('desktop:renderer', true)
