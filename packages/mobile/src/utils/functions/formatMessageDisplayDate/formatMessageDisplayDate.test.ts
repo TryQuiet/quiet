867eb7a21a948167f7e30377ba2b252ada8447d3
Isla Koenigsknecht <ikoenigbu@gmail.com>
2024-05-15 21:19:10 -0400

    Make these tests deterministic

--- packages/mobile/src/utils/functions/formatMessageDisplayDate/formatMessageDisplayDate.test.ts
+++ packages/mobile/src/utils/functions/formatMessageDisplayDate/formatMessageDisplayDate.test.ts
@@ -4,17 +4,17 @@ import { formatMessageDisplayDate } from './formatMessageDisplayDate'
 
 describe('Channel List Message date display', () => {
   it('timestamp in the current day is rendered as a localized time', () => {
-    const messageDateTime = Date.now() / 1000 - 21600 // Minus 6 hours
+    const messageDateTime = DateTime.now().toSeconds() - 1
     const formattedMessageDate = formatMessageDisplayDate(messageDateTime)
     expect(formattedMessageDate).toEqual(DateTime.fromSeconds(messageDateTime).toLocaleString(DateTime.TIME_SIMPLE))
   })
-  it('timestamp in the previous day is rendered as Yeseterday', () => {
-    const messageDateTime = Date.now() / 1000 - 108000 // Minus 30 hours
+  it('timestamp in the previous day is rendered as Yesterday', () => {
+    const messageDateTime = DateTime.now().minus({ days: 1 }).toSeconds()
     const formattedMessageDate = formatMessageDisplayDate(messageDateTime)
     expect(formattedMessageDate).toEqual('Yesterday')
   })
   it('timestamp before the previous day is rendered as a localized date and time', () => {
-    const messageDateTime = Date.now() / 1000 - 194400 // Minus 54 hours
+    const messageDateTime = DateTime.now().minus({ days: 2 }).toSeconds()
     const formattedMessageDate = formatMessageDisplayDate(messageDateTime)
     expect(formattedMessageDate).toEqual(DateTime.fromSeconds(messageDateTime).toLocaleString())
   })
