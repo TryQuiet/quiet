c7bc1ed78ca1047fd22d6a82936fa613e8d90f02
Holmes Wilson <hwilson@gmail.com>
2025-03-28 12:05:26 -0400

    removes cruft

--- packages/desktop/cypress/support/index.js
+++ packages/desktop/cypress/support/index.js
@@ -1,13 +1,2 @@
 import './commands'
 import 'cypress-storybook/cypress'
-
-/*
-Cypress.on("test:before:run", () => {
-    Cypress.automation("remote:debugger:protocol", {
-      command: "Emulation.setLocaleOverride",
-      params: {
-        locale: "en-US",
-      },
-    });
-  });
-  */
