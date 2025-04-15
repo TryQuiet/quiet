15a1fedb7567151df5037f1bef2678e2f7807dff
holmesworcester <h@zbay.llc>
2025-04-15 03:10:20 -0400

    Fix/2766-wrong-timezone-on-mobile (#2780)
    This bug was happening due to Luxon not detecting the correct system time when running in React Native / NodeJS Mobile. So we have to get the timezone offset from JavaScript's DateTime and use that in Luxon. 
    
    * displays correct local time
    * Removes mobile formatMessageDisplayDate in favor of putting date code in renderer
    * Updates CHANGELOG

--- packages/state-manager/src/utils/functions/dates/formatMessageDisplayDate.ts
+++ packages/state-manager/src/utils/functions/dates/formatMessageDisplayDate.ts
@@ -1,11 +1,14 @@
 import { DateTime } from 'luxon'
 
 export const formatMessageDisplayDate = (createdAt: number): string => {
+  // Extract timezone offset from native Date API
+  const tzOffsetHours = -new Date().getTimezoneOffset() / 60
+  const formattedOffset = `UTC${tzOffsetHours >= 0 ? '+' : ''}${tzOffsetHours}`
+
   const LC = process.env.LC_ALL || 'en_US.UTF-8'
   const locale = LC.split('_')[0]
-  const messageDate = DateTime.fromSeconds(createdAt).setLocale(locale)
-  const now = DateTime.now().setLocale(locale)
-
+  const messageDate = DateTime.fromSeconds(createdAt).setZone(formattedOffset).setLocale(locale)
+  const now = DateTime.now().setZone(formattedOffset).setLocale(locale)
   const diffInDays = now.startOf('day').diff(messageDate.startOf('day'), 'days').days
 
   if (diffInDays === 0) {
