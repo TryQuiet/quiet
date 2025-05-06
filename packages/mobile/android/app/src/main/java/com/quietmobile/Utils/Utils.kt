d92b13d0f000fbfd4ef07871196300bac92df260
Holmes Wilson <hwilson@gmail.com>
2025-05-06 13:58:01 -0400

    updates mobile storage locations given breaking changes

--- packages/mobile/android/app/src/main/java/com/quietmobile/Utils/Utils.kt
+++ packages/mobile/android/app/src/main/java/com/quietmobile/Utils/Utils.kt
@@ -24,7 +24,7 @@ object Utils {
     }
 
     fun createDirectory(context: Context): String {
-        val dataDirectory = File(context.filesDir, "backend/files3")
+        val dataDirectory = File(context.filesDir, "backend/files4")
         dataDirectory.mkdirs()
 
         return dataDirectory.absolutePath
