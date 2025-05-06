93b6a7ba17087cd3d03a5cc7ee2b4fd81a26c526
Holmes Wilson <hwilson@gmail.com>
2025-05-06 17:18:32 -0400

    corrects mistake: should be files5 instead of files4 since we are preparing 5x

--- packages/mobile/android/app/src/main/java/com/quietmobile/Communication/CommunicationModule.java
+++ packages/mobile/android/app/src/main/java/com/quietmobile/Communication/CommunicationModule.java
@@ -108,7 +108,7 @@ public class CommunicationModule extends ReactContextBaseJavaModule {
     private static void deleteBackendData() {
         Context context = reactContext.getApplicationContext();
         try {
-            FileUtils.deleteDirectory(new File(context.getFilesDir(), "backend/files4"));
+            FileUtils.deleteDirectory(new File(context.getFilesDir(), "backend/files5"));
         } catch (IOException e) {
             e.printStackTrace();
         }
