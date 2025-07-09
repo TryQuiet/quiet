5ecbae39eafaad410d34f788c0b1679b01bf3da0
Isla <5048549+islathehut@users.noreply.github.com>
2025-07-09 10:19:37 -0400

    Remove debug log

--- packages/backend/src/nest/app.module.ts
+++ packages/backend/src/nest/app.module.ts
@@ -125,7 +125,6 @@ export class AppModule {
               }
 
               const token = authHeader && authHeader.split(' ')[1]
-              _ioLogger.warn('secret, token', options.socketIOSecret, token)
               if (!token) {
                 _ioLogger.error('Backend server: No auth token')
                 res.writeHead(401, 'No authorization token')
