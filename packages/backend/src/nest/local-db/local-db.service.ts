b5bdf9326abf3d305676690975b4adf499a51acc
Isla <5048549+islathehut@users.noreply.github.com>
2025-11-06 11:24:44 -0500

    Fix unit tests

--- packages/backend/src/nest/local-db/local-db.service.ts
+++ packages/backend/src/nest/local-db/local-db.service.ts
@@ -133,11 +133,7 @@ export class LocalDbService extends EventEmitter {
     if (peerId) {
       return await this.find(LocalDBKeys.PEERS, peerId)
     }
-    const peers = await this.get(LocalDBKeys.PEERS)
-    if (!peers) {
-      return null
-    }
-    return peers
+    return await this.get(LocalDBKeys.PEERS)
   }
 
   /**
@@ -148,9 +144,9 @@ export class LocalDbService extends EventEmitter {
    * @returns A promise that resolves to an array of sorted peer multiaddr.
    */
   public async getSortedPeers(includeLocalPeerAddress: boolean = true): Promise<string[]> {
-    const entries = await this.getPeerStats()
+    let entries = await this.getPeerStats()
     if (entries == null) {
-      return []
+      entries = {}
     }
 
     const stats: NetworkStats[] = Object.values(entries)
--- packages/common/src/invitationLink/invitationLink.test.ts
+++ packages/common/src/invitationLink/invitationLink.test.ts
@@ -537,25 +537,6 @@ describe(`Invitation link helper ${InvitationDataVersion.v3}`, () => {
     }
   })
 
-  it('throws error if qssEndpoint is missing port', () => {
-    const url = new URL(DEEP_URL_SCHEME_WITH_SEPARATOR)
-    const invalidUrlParams = [
-      [PEER_ADDRESS_KEY, peerPairsToUrlParamString([data.pairs[0], data.pairs[1]])],
-      [PSK_PARAM_KEY, data.psk],
-      [AUTH_DATA_KEY, encodeAuthData(data.authData)],
-      [QSS_ENABLED_KEY, `${data.qssEnabled}`],
-      [QSS_ENDPOINT_KEY, encodeQssEndpoint('ws://localhost')],
-    ]
-    invalidUrlParams.forEach(([key, value]) => url.searchParams.append(key, value))
-
-    try {
-      const parsed = parseInvitationLinkDeepUrl(url.href)
-      expect(parsed).toBe(null)
-    } catch (e) {
-      expect(e.message).toBe(`Invalid value 'ws://localhost' for key 'e' in invitation link - Port was null`)
-    }
-  })
-
   it('throws error if qssEndpoint is not a ws/wss url', () => {
     const url = new URL(DEEP_URL_SCHEME_WITH_SEPARATOR)
     const invalidUrlParams = [
