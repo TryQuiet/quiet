a2ca2e7c7de2267070faddf20fdea5c153d74bfa
a <dsnake@protonmail.com>
2025-11-10 23:17:20 -0500

    - Use nodejs mobile version 18.20.4 matching rest of project - Removed framework binary stripping build phase from xcode, it only affected the new nodejs binary and is no longer needed since nodejs mobile is now an .xcodeframework instead of a .framework (ie, at build time the correct architecture (iphone arm64, sim arm64, sim x86_64) is chosen - Updated rn_bridge.cpp to support new nodejs version

--- packages/mobile/ios/NodeJsMobile/rn-bridge.cpp
+++ packages/mobile/ios/NodeJsMobile/rn-bridge.cpp
@@ -1,7 +1,6 @@
 #include "node_api.h"
 #include "uv.h"
 #include "rn-bridge.h"
-#define NM_F_LINKED 0x2
 #include <map>
 #include <mutex>
 #include <queue>
@@ -9,60 +8,7 @@
 #include <cstring>
 #include <cstdlib>
 
-
-/**
- * Some helper macros from node/test/addons-napi/common.h
- */
-
-// Empty value so that macros here are able to return NULL or void
-#define NAPI_RETVAL_NOTHING  // Intentionally blank #define
-
-#define GET_AND_THROW_LAST_ERROR(env)                                    \
-  do {                                                                   \
-    const napi_extended_error_info *error_info;                          \
-    napi_get_last_error_info((env), &error_info);                        \
-    bool is_pending;                                                     \
-    napi_is_exception_pending((env), &is_pending);                       \
-    /* If an exception is already pending, don't rethrow it */           \
-    if (!is_pending) {                                                   \
-      const char* error_message = error_info->error_message != NULL ?    \
-        error_info->error_message :                                      \
-        "empty error message";                                           \
-      napi_throw_error((env), NULL, error_message);                      \
-    }                                                                    \
-  } while (0)
-
-#define NAPI_ASSERT_BASE(env, assertion, message, ret_val)               \
-  do {                                                                   \
-    if (!(assertion)) {                                                  \
-      napi_throw_error(                                                  \
-          (env),                                                         \
-        NULL,                                                            \
-          "assertion (" #assertion ") failed: " message);                \
-      return ret_val;                                                    \
-    }                                                                    \
-  } while (0)
-
-// Returns NULL on failed assertion.
-// This is meant to be used inside napi_callback methods.
-#define NAPI_ASSERT(env, assertion, message)                             \
-  NAPI_ASSERT_BASE(env, assertion, message, NULL)
-
-#define NAPI_CALL_BASE(env, the_call, ret_val)                           \
-  do {                                                                   \
-    if ((the_call) != napi_ok) {                                         \
-      GET_AND_THROW_LAST_ERROR((env));                                   \
-      return ret_val;                                                    \
-    }                                                                    \
-  } while (0)
-
-// Returns NULL if the_call doesn't return napi_ok.
-#define NAPI_CALL(env, the_call)                                         \
-  NAPI_CALL_BASE(env, the_call, NULL)
-
-#define ARGC_CHANNEL_NAME_MSG 2
-
-/**
+/*
  * Forward declarations
  */
 void FlushMessageQueue(uv_async_t* handle);
@@ -346,5 +292,16 @@ void rn_bridge_notify(const char* channelName, const char *message) {
     channel->queueMessage(messageCopy);
 }
 
-NAPI_MODULE_X(rn_bridge, Init, NULL, NM_F_LINKED)
+extern "C" {
+    static napi_module _module = {
+      NAPI_MODULE_VERSION,
+      NM_F_LINKED,
+      __FILE__,
+      Init,
+      "rn_bridge",
+      NULL,
+      {0},
+    };
 
+    NAPI_C_CTOR(_register_rn_bridge) { napi_module_register(&_module); }
+}
