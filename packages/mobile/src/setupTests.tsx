e195aa3ff465329fc7cd59d5cf3f15b716567f53
taea <taelxvie@gmail.com>
2026-02-04 13:30:55 -0500

    adjust mocks

--- packages/mobile/src/setupTests.tsx
+++ packages/mobile/src/setupTests.tsx
@@ -55,7 +55,22 @@ jest.mock('react-native', () => {
   return rn
 })
 
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => {
+  const noop = jest.fn()
+  const apiProxy = new Proxy({}, { get: () => noop })
+  return {
+    __esModule: true,
+    default: new Proxy(
+      {},
+      {
+        get: (_target: any, prop: string) => {
+          if (prop === 'API') return apiProxy
+          return noop
+        },
+      }
+    ),
+  }
+})
 
 jest.mock('redux-persist-filesystem-storage', () => ({
   config: jest.fn(),
