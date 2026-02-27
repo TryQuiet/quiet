9cfbcbfaa9196160d6592f7c4631b94ab3c9f9d1
Jake McGinty <me@jakebot.org>
2026-02-27 13:10:35 -0600

    get build working

--- packages/mobile/ios/Quiet/AppDelegate.m
+++ packages/mobile/ios/Quiet/AppDelegate.m
@@ -1,7 +1,6 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 #import <React/RCTLinkingManager.h>
 
 #import "RNNodeJsMobile.h"
@@ -29,7 +28,6 @@ static NSString *const platform = @"mobile";
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"QuietMobile";
-  self.dependencyProvider = [RCTAppDependencyProvider new];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
