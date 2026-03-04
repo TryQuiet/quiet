50912e9f37574c55b8982fc7d6932b1cf3cf1cf0
a <dsnake@protonmail.com>
2026-03-04 14:39:46 -0500

    add script to update to on android

---
+++ packages/mobile/updating-tor-android.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+git clone https://github.com/bitmold/quiet_libtor_android
+cd quiet_libtor_android
+./tor-droid-make.sh fetch -c
+cd external/tor
+echo "Building with tor:"
+git log --oneline -n 1
+cd ../openssl
+echo "Openssl: "
+git log --oneline -n 1
+echo "zlib:"
+cd ../zlib
+git log --oneline -n 1
+echo "zstd:"
+cd ../zstd
+git log --oneline -n 1
+echo "and libevent:"
+cd ../libevent
+git log --oneline -n 1
+cd ../..
+echo ""
+
+read -r -p "Build libtor.so with these depencies, [Y/n]: " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+		then
+			echo "Building libtor.so..."
+		else
+			echo "You can update the submodules to point to newer versions of tor, openssl, etc. Commit them and rerun this script."
+			exit
+fi
+
+
+# set to only build arm64-v8a matching quiet...
+./tor-droid-make.sh build
+
+ls -al external/lib/arm64-v8a
+cp -fv external/lib/arm64-v8a/libtor.so ../android/app/src/main/jniLibs/arm64-v8a/libtor.so
+cd ..
+git status
+
