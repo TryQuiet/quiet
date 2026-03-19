2e2f5a504fa2d95eddea8e59831b8040cfc27efe
Jake McGinty <me@jakebot.org>
2026-03-19 17:57:05 -0500

    iOS Push Notification Support (#3125)
    * Add Firebase support and Network Service Extension for push notifications
    
    * fix extension plist build issues
    
    * add encrypted Firebase plist
    
    * add decryption code for GoogleService-Info.plist
    
    * integrate with Taea's communication module
    
    * move personal changes from .xcode.env to .xcode.env.local
    
    * compatibility fixes
    
    * allow easier overriding of development team and bundle id
    
    * fix more xcodeproj woes, using a fork that works for now
    
    * fix node version build issues for .xcode.env.local
    
    ---------
    
    Co-authored-by: taea <taelxvie@gmail.com>

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -4,10 +4,12 @@ set -e
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/match_AppStore_comquietmobile.mobileprovision ./.github/secrets/match_AppStore_comquietmobile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_CERTIFICATE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_FIREBASE_KEY" --output ./.github/secrets/GoogleService-Info.plist ./.github/secrets/GoogleService-Info.plist.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/match_AppStore_comquietmobile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/762df280-302c-4336-a56d-c74914169337.mobileprovision
+cp ./.github/secrets/GoogleService-Info.plist ./packages/mobile/ios/GoogleService-Info.plist
 
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_CERTIFICATE_KEY" -A
--- packages/mobile/ios/Quiet/AppDelegate.h
+++ packages/mobile/ios/Quiet/AppDelegate.h
@@ -5,17 +5,21 @@
 
 #import "RNNodeJsMobile.h"
 
-#import "Quiet-Swift.h"
+// Forward declarations for Swift classes
+// (Actual imports happen in AppDelegate.m to avoid circular dependencies)
+@class TorHandler;
+@class TORConfiguration;
+@class TORController;
 
 @interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate>
 
-@property uint16_t dataPort;
+@property (nonatomic) uint16_t dataPort;
 
-@property NSString *socketIOSecret;
+@property (nonatomic, strong) NSString *socketIOSecret;
 
-@property NSString *dataPath;
+@property (nonatomic, strong) NSString *dataPath;
 
-@property RNNodeJsMobile *nodeJsMobile;
+@property (nonatomic, strong) RNNodeJsMobile *nodeJsMobile;
 
 @property (nonatomic, strong) TorHandler *tor;
 @property (nonatomic, strong) TORConfiguration *torConfiguration;
