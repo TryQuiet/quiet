47c51f9cb0d129eb3b76daf52edbd70c5108563f
bitmold <dsnake@protonmail.com>
2026-03-19 14:15:17 -0400

    Closes #3123 Removes Superflous @peculiar/webcrypto dependency (#3129)
    * Closes #3123 Removes Superflous @peculiar/webcrypto dependency. Removes hacky JavaScript that was needed to override global.crypto
    
    - this was probably needed for along time because iOS was using nodejs12 until some recent work, hence
    having no global.crypto flag.
    - iOS and Android need the --experimental-global-webcrypto flag until nodejs mobile is rebuilt

--- packages/backend/jestSetup.ts
+++ packages/backend/jestSetup.ts
@@ -1,14 +1,10 @@
 import { setEngine, CryptoEngine } from 'pkijs'
-import { Crypto } from '@peculiar/webcrypto'
-
-const crypto = new Crypto()
-global.crypto = crypto
 
 setEngine(
   'newEngine',
   new CryptoEngine({
     name: 'newEngine',
     // @ts-ignore
-    crypto: crypto,
+    crypto: global.crypto,
   })
 )
