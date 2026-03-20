f3ddf6664e5b1d7bd04f5cdce423621804e68985
taea <taelxvie@gmail.com>
2026-03-20 13:16:10 -0400

    reinforce endpoint fuzzy match with env flag

--- packages/backend/src/nest/qss/qss.service.ts
+++ packages/backend/src/nest/qss/qss.service.ts
@@ -455,7 +455,8 @@ export class QSSService extends EventEmitter implements OnModuleDestroy, OnModul
     if (
       /^(127\.\d+\.\d+\.\d+|10\.\d+\.\d+\.\d+|192\.168\.\d+\.\d+|172\.(1[6-9]|2\d|3[01])\.\d+\.\d+|localhost)$/.test(
         host
-      )
+      ) &&
+      process.env.NODE_ENV !== 'production'
     ) {
       host = 'localhost'
     }
