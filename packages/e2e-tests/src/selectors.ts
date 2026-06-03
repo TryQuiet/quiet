f45f2c7e930ddaf90e1d0a809ddf94b3b7a07214
Isla <5048549+islathehut@users.noreply.github.com>
2026-06-03 10:51:21 -0400

    Use class instead of test ID

--- packages/e2e-tests/src/selectors.ts
+++ packages/e2e-tests/src/selectors.ts
@@ -3090,7 +3090,7 @@ export class Settings {
         locator = "//div[contains(@class, 'DebugInfotitleContainer')]"
         break
       case SettingsModalTabName.COMMUNITY_MEMBERSHIP:
-        locator = "//*[@data-testid='community-membership-title']"
+        locator = "//div[contains(@class, 'CommunityMembershipcomponentContainer')]"
         break
       default:
         throw new Error(`Can't wait for unknown tab ${tabName}`)
